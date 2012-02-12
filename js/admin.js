3e1c4c5d48ab1b592267a4a9dd2e7e0bf530c84f
Eric Teubert <ericteubert@googlemail.com>
2012-02-12 21:11:49 +0100

    refactor javascript

--- js/admin.js
+++ js/admin.js
@@ -1,41 +1,34 @@
-jQuery(function($) {
+(function( $ ){
 
-	$('.podcast_show_checkbox').on('change', function() {
-		var slug = $(this).data("slug").toLowerCase();
-		var old_val = $("#tax-input-podcast_shows").val().toLowerCase().split(",");
-		var new_val = null;
-		
-		if ($(this).is(":checked")) {
-			// add show
-			old_val.push(slug);
-		} else {
-			// remove show
-			index = $.inArray(slug, old_val);
-			old_val.splice(index, 1);
-		}
-		
-		new_val = old_val.join(",");
-		
-		$("#tax-input-podcast_shows").val(new_val);
-	});
+	// Link a checkbox to a taxonomy
+	// When you (de)activate the checkbox, the referenced taxonomy will be updated.
+	$.fn.taxonomize = function( hidden_taxonomy_field ) {  
+	    return this.each(function() {
+			$(this).on('change', function() {
+				var slug = $(this).data("slug").toLowerCase();
+				var old_val = hidden_taxonomy_field.val().toLowerCase().split(",");
+				var new_val = null;
+				
+				if ($(this).is(":checked")) {
+					// add show
+					old_val.push(slug);
+				} else {
+					// remove show
+					index = $.inArray(slug, old_val);
+					old_val.splice(index, 1);
+				}
+
+				new_val = old_val.join(",");
+				hidden_taxonomy_field.val(new_val);
+			});
+		});
+	};
+
+})( jQuery );
+
+jQuery(function($) {
 	
-	$('.podcast_format_checkbox').on('change', function() {
-		var slug = $(this).data("slug").toLowerCase();
-		var old_val = $("#tax-input-podcast_file_formats").val().toLowerCase().split(",");
-		var new_val = null;
-		
-		if ($(this).is(":checked")) {
-			// add show
-			old_val.push(slug);
-		} else {
-			// remove show
-			index = $.inArray(slug, old_val);
-			old_val.splice(index, 1);
-		}
-		
-		new_val = old_val.join(",");
-		console.log(new_val);
-		$("#tax-input-podcast_file_formats").val(new_val);
-	});
+	$('.podcast_show_checkbox').taxonomize($("#tax-input-podcast_shows"));
+	$('.podcast_format_checkbox').taxonomize($("#tax-input-podcast_file_formats"));
 	
 });
