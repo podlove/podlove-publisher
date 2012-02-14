e62e213df74fa3d4a43bf7e133b24b20096aa65a
Eric Teubert <ericteubert@googlemail.com>
2012-02-14 19:40:29 +0100

    seed default formats

--- plugin.php
+++ plugin.php
@@ -9,6 +9,27 @@ function activate() {
 	Model\Feed::build();
 	Model\Format::build();
 	Model\Show::build();
+	
+	if ( ! Model\Format::has_entries() ) {
+		$default_formats = array(
+			array( 'name' => 'MP3 Audio',  'type' => 'audio', 'mime_type' => 'audio/mpeg',  'extension' => 'mp3' ),
+			array( 'name' => 'MPG Video',  'type' => 'video', 'mime_type' => 'video/mpeg',  'extension' => 'mpg' ),
+			array( 'name' => 'MP4 Audio',  'type' => 'audio', 'mime_type' => 'audio/mp4',   'extension' => 'mp4' ),
+			array( 'name' => 'MP4 Video',  'type' => 'video', 'mime_type' => 'video/mp4',   'extension' => 'mp4' ),
+			array( 'name' => 'OGG Audio',  'type' => 'audio', 'mime_type' => 'audio/ogg',   'extension' => 'ogg' ),
+			array( 'name' => 'OGG Video',  'type' => 'video', 'mime_type' => 'video/ogg',   'extension' => 'ogg' ),
+			array( 'name' => 'WebM Audio', 'type' => 'audio', 'mime_type' => 'audio/webm',  'extension' => 'webm' ),
+			array( 'name' => 'WebM Video', 'type' => 'video', 'mime_type' => 'video/webm',  'extension' => 'webm' ),
+		);
+		
+		foreach ( $default_formats as $format ) {
+			$f = new Model\Format;
+			foreach ( $format as $key => $value ) {
+				$f->{$key} = $value;
+			}
+			$f->save();
+		}
+	}
 }
 
 function deactivate() {
