9de83370618f10823538f889c30fcff472369dbc
Eric Teubert <ericteubert@googlemail.com>
2012-02-28 09:51:39 +0100

    add missing slash

--- lib/podcast_post_type.php
+++ lib/podcast_post_type.php
@@ -102,7 +102,7 @@ class Podcast_Post_Type {
 			
 			wp_register_script(
 				'podlove_admin_script',
-				WP_PLUGIN_URL . 'podlove/js/admin.js',
+				WP_PLUGIN_URL . '/podlove/js/admin.js',
 				array( 'jquery' ),
 				'1.0' 
 			);
