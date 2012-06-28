0603501179d57318bffb574b74dceb6229c6c096
Eric Teubert <ericteubert@googlemail.com>
2012-06-28 11:51:37 +0200

    simplify custom guid module
    Note to self: Had this idea in the shower. Maybe take a shower more
    than once a day?

--- lib/custom_guid.php
+++ lib/custom_guid.php
@@ -15,29 +15,9 @@ remove_filter( 'post_guid',     'wp_strip_all_tags' );
 remove_filter( 'post_guid',     'esc_url' );
 remove_filter( 'post_guid',     'wp_kses_data' );
 
-add_action( 'save_post', '\Podlove\save_custom_guid' );
-
-function save_custom_guid( $post_id ) {
-	global $wpdb;
-
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
-		return;
-
-	if ( empty( $_POST[ 'podlove_noncename' ] ) || ! wp_verify_nonce( $_POST[ 'podlove_noncename' ], \Podlove\PLUGIN_FILE ) )
-		return;
-
-	// Check permissions
-	if ( 'podcast' == $_POST['post_type'] ) {
-	  if ( ! current_user_can( 'edit_post', $post_id ) )
-		return;
-	} else {
-		return;
-	}
-
-	// set custom guid
-	$current_guid = get_post_field( 'guid', $post_id );
-	if ( false === strpos( $current_guid, 'plv-' ) ) {
+add_filter( 'post_guid', function ( $guid ) {
+	if ( false === strpos( $guid, 'plv-' ) )
 		$guid = uniqid( 'plv-', true ); 
-		$wpdb->update( $wpdb->posts, array( 'guid' => $guid ), array( 'ID' => $post_id ) );
-	}
-}
+
+	return $guid; 
+} );
