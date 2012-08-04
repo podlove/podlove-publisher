be2a5e8a032df5b4c1f10cd2c5bcb3bb268a4a9a
Eric Teubert <ericteubert@googlemail.com>
2012-08-04 10:58:33 +0200

    new module: open graph

---
+++ lib/modules/open_graph/open_graph.php
@@ -0,0 +1,75 @@
+<?php
+namespace Podlove\Modules\OpenGraph;
+
+class Open_Graph extends \Podlove\Modules\Base {
+
+		protected $module_name = 'Open Graph Integration';
+		protected $module_description = 'Adds Open Graph metadata to episodes. Useful for third party services.';
+
+		public function load() {
+			add_action( 'wp', array( $this, 'register_hooks' ) );
+		}
+
+		/**
+		 * Register hooks on episode pages only.
+		 */
+		public function register_hooks() {
+			
+			if ( ! is_single() )
+				return;
+
+			if ( 'podcast' !== get_post_type() )
+				return;
+
+			add_filter( 'language_attributes', function ( $output = '' ) {
+				return $output . ' prefix="og: http://ogp.me/ns#"';
+			} );
+
+			add_action( 'wp_head', array( $this, 'insert_open_graph_metadata' ) );
+		}
+
+		/**
+		 * Insert HTML meta tags into site head.
+		 *
+		 * @todo  caching
+		 * @todo  let user choose what's in og:description: subtitle, excerpt, ...
+		 * @todo  handle multiple releases per episode
+		 */
+		public function insert_open_graph_metadata() {
+
+			$post_id = get_the_ID();
+			if ( ! $post_id )
+				return;
+
+			$episode = \Podlove\Model\Episode::find_one_by_post_id( $post_id );
+			if ( ! $episode )
+				return;
+
+			// assumes that there is only one active release per episode
+			$release = \Podlove\Model\Release::find_one_by_where( sprintf( '`episode_id` = "%s" AND `active` = "1"', $episode->id ) );
+			if ( ! $release )
+				return;
+
+			$show = $release->show();
+
+			// determine image
+			$cover_art_url = $release->cover_art;
+			if ( ! $cover_art_url )
+				$cover_art_url = $show->cover_image;
+			
+			?>
+			<meta property="og:type" content="website" />
+			<meta property="og:site_name" content="<?php echo $show->full_title(); ?>" />
+			<meta property="og:title" content="<?php the_title(); ?>" />
+			<?php if ( $cover_art_url ): ?>
+				<meta property="og:image" content="<?php echo $cover_art_url; ?>" />
+			<?php endif ?>
+			<meta property="og:url" content="<?php the_permalink(); ?>" />
+			<?php $media_files = $release->media_files(); ?>
+			<?php foreach ( $media_files as $media_file ): ?>
+				<meta property="og:audio" content="<?php echo $media_file->get_file_url(); ?>" />
+				<meta property="og:audio:type" content="<?php echo $media_file->media_location()->media_format()->mime_type ?>" />
+			<?php endforeach ?>
+			<?php
+		}		
+}
