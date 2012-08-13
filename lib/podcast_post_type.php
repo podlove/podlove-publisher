10aee43a6547089aeee60eaafbe433bf753ceb89
Eric Teubert <ericteubert@googlemail.com>
2012-08-13 22:12:27 +0200

    apply some style to validations

--- lib/podcast_post_type.php
+++ lib/podcast_post_type.php
@@ -252,50 +252,82 @@ class Podcast_Post_Type {
 		}
 
 		?>
-		<a href="#" id="validate_everything">
-			<?php echo __( 'Validate Everything', 'podlove' ); ?>
-		</a>
-		<?php
+		<div id="validation">
 
-		foreach ( $shows as $show ) {
-			echo "<h4>" . $show->name . "</h4>";
-			$releases = $show->releases();
-			?>
+			<a href="#" id="validate_everything">
+				<?php echo __( 'Validate Everything', 'podlove' ); ?>
+			</a>
 
-			<?php foreach ( $releases as $release ): ?>
-				<div class="release">
-					<span class="slug">
-						<strong><?php echo $release->slug; ?></strong>
-					</span>
-					<span class="duration">
-						<?php echo sprintf( __( 'Duration: %s', 'podlove' ), $release->duration ); ?>
-					</span>
-					<span class="chapters">
-						<?php echo sprintf( __( 'Chapters: %s' ), strlen( $release->chapters ) > 0 ? __( 'existing', 'podlove' ) : __( '<span style="color:maroon">empty</span>', 'podlove' ) ); ?>
-					</span>
-					<span class="coverart">
-						<?php echo sprintf( __( 'Cover Art: %s' ), strlen( $release->cover_art ) > 0 ? __( 'existing', 'podlove' ) : __( '<span style="color:maroon">empty</span>', 'podlove' ) ); ?>
-					</span>
-					<div class="media_files">
-						<?php $media_files = $release->media_files(); ?>
-						<?php foreach ( $media_files as $media_file ): ?>
-							<div class="file" data-id="<?php echo $media_file->id; ?>">
-								<span class="status">
-									<?php if ( $media_file->size <= 0 ): ?>
-										<?php echo __( "<span style='color:red'>filesize missing</span>", 'podlove' ); ?>
-									<?php endif ?>
-								</span>
-								<span class="title"><?php echo $media_file->media_location()->title() ?></span>
-								<span class="url">
-									<?php echo $media_file->get_file_url(); ?>
-								</span>
+			<?php foreach ( $shows as $show ): ?>
+				<?php
+				echo "<h4>" . $show->name . "</h4>";
+				$releases = $show->releases();
+				?>
+
+				<?php foreach ( $releases as $release ): ?>
+					<?php
+					$episode = $release->episode();
+					$post_id = $episode->post_id;
+					?>
+					<div class="release">
+						<div class="slug">
+							<strong><?php echo sprintf( "%s (%s)", get_the_title( $post_id ), $release->slug ); ?></strong>
+						</div>
+						<div class="duration">
+							<?php echo sprintf( __( 'Duration: %s', 'podlove' ), ( $release->duration ) ? $release->duration : __( '<span class="warning">empty</span>', 'podlove' ) ); ?>
+						</div>
+						<div class="chapters">
+							<?php echo sprintf( __( 'Chapters: %s' ), strlen( $release->chapters ) > 0 ? __( 'existing', 'podlove' ) : __( '<span class="warning">empty</span>', 'podlove' ) ); ?>
+						</div>
+						<?php if ( $show->supports_cover_art ): ?>
+							<div class="coverart">
+								<?php echo sprintf( __( 'Cover Art: %s' ), strlen( $release->cover_art ) > 0 ? __( 'existing', 'podlove' ) : __( '<span class="warning">empty</span>', 'podlove' ) ); ?>
 							</div>
-						<?php endforeach ?>
+						<?php endif; ?>
+						<div class="media_files">
+							<?php $media_files = $release->media_files(); ?>
+							<?php foreach ( $media_files as $media_file ): ?>
+								<div class="file" data-id="<?php echo $media_file->id; ?>">
+									<span class="status">
+										<?php if ( $media_file->size <= 0 ): ?>
+											<?php echo __( "<span class=\"error\">filesize missing</span>", 'podlove' ); ?>
+										<?php endif ?>
+									</span>
+									<span class="title"><?php echo $media_file->media_location()->title() ?></span>
+									<span class="url">
+										<?php echo $media_file->get_file_url(); ?>
+									</span>
+								</div>
+							<?php endforeach ?>
+						</div>
 					</div>
-				</div>
-			<?php endforeach ?>
-			<?php
+				<?php endforeach ?>
+			<?php endforeach ?>			
+		</div>
+
+		<style type="text/css">
+		#validation h4 {
+			font-size: 20px;
 		}
+
+		#validation .release {
+			margin: 0 0 15px 0;
+		}
+
+		#validation .slug {
+			font-size: 18px;
+			margin: 0 0 5px 0;
+		}
+
+		#validation .warning {
+			color: maroon;
+		}
+
+		#validation .error {
+			color: red;
+		}
+		</style>
+		<?php
 	}
 	
 	/**
