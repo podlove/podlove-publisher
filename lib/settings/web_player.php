ad45918651b0489f9245ab6133da5f421ca2b4c8
Eric Teubert <ericteubert@googlemail.com>
2012-08-21 12:17:09 +0200

    add web player settings page

---
+++ lib/settings/web_player.php
@@ -0,0 +1,114 @@
+<?php
+namespace Podlove\Settings;
+use \Podlove\Model;
+
+class WebPlayer {
+
+	static $pagehook;
+	
+	public function __construct( $handle ) {
+		
+		WebPlayer::$pagehook = add_submenu_page(
+			/* $parent_slug*/ $handle,
+			/* $page_title */ 'Web Player',
+			/* $menu_title */ 'Web Player',
+			/* $capability */ 'administrator',
+			/* $menu_slug  */ 'podlove_webplayer_settings_handle',
+			/* $function   */ array( $this, 'page' )
+		);
+
+		register_setting( WebPlayer::$pagehook, 'podlove_webplayer_formats' );
+	}
+
+	public function page() {
+		?>
+		<div class="wrap">
+			<div id="icon-options-general" class="icon32"></div>
+			<h2><?php echo __( 'Web Player', 'podlove' ); ?></h2>
+
+			<?php echo __( 'Webplayers are able to provide various media formats depending on context. Try to provide as many as possible to maximize compatibility with all browsers.', 'podlove' ); ?>
+
+
+			<form method="post" action="options.php">
+				<?php settings_fields( WebPlayer::$pagehook ); ?>
+
+				<table class="form-table">
+					<?php $this->form_fields(); ?>
+				</table>
+				
+				<?php submit_button( __( 'Save Changes' ), 'button-primary', 'submit', TRUE ); ?>
+			</form>
+
+		</div>	
+		<?php
+	}
+
+	public function form_fields() {
+
+		$formats = array(
+			'audio' => array(
+				'mp3' => array(
+					'title'     => __( 'MP3 Audio', 'podlove' ),
+					'mime_type' => 'audio/mpeg'
+				),
+				'mp4' => array(
+					'title'     => __( 'MP4 Audio', 'podlove' ),
+					'mime_type' => 'audio/mp4'
+				),
+				'ogg' => array(
+					'title'     => __( 'OGG Audio', 'podlove' ),
+					'mime_type' => 'audio/ogg'
+				),
+			),
+			'video' => array(
+				'mp4'  => array(
+					'title'     => __( 'MP4 Video', 'podlove' ),
+					'mime_type' => 'video/mp4'
+				),
+				'ogg'  => array(
+					'title'     => __( 'OGG Video', 'podlove' ),
+					'mime_type' => 'video/ogg'
+				),
+				'webm' => array(
+					'title'     => __( 'Webm Video', 'podlove' ),
+					'mime_type' => 'video/webm'
+				),
+			)
+		);
+
+		$formats_data = get_option( 'podlove_webplayer_formats', array() );
+		$media_locations = Model\MediaLocation::all();
+
+		foreach ( $formats as $format => $extensions ) {
+			foreach ( $extensions as $extension => $extension_data ) {
+				$label = $extension_data['title'];
+				$mime_type = $extension_data['mime_type'];
+
+				$id    = sprintf( 'podlove_webplayer_formats_%s_%s'  , $format, $extension );
+				$name  = sprintf( 'podlove_webplayer_formats[%s][%s]', $format, $extension );
+				$value = ( isset( $formats_data[$format] ) && isset( $formats_data[$format][$extension] ) ) ? $formats_data[$format][$extension] : 0;
+				?>
+				<tr valign="top">
+					<th scope="row" valign="top">
+						<label for="<?php echo $id ?>"><?php echo $label; ?></label>
+					</th>
+					<td>
+						<div>
+							<select name="<?php echo $name; ?>" id="<?php echo $id; ?>">
+								<option value="0" <?php selected( 0, $value ); ?> ><?php echo __( 'Unused', 'podlove' ); ?></option>
+								<?php foreach ( $media_locations as $media_location ): ?>
+									<?php $media_format = $media_location->media_format(); ?>
+									<?php if ( $media_format && $media_format->mime_type === $mime_type ): ?>
+										<option value="<?php echo $media_location->id; ?>" <?php selected( $media_location->id, $value ); ?>><?php echo $media_location->title ?></option>
+									<?php endif ?>
+								<?php endforeach ?>
+							</select>
+						</div>
+					</td>
+				</tr>
+				<?php 
+			}
+		}
+	}
+
+}
