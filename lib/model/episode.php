90251b63a4cc05b66e48a35ab8dca2e2f3a0b022
Eric Teubert <ericteubert@googlemail.com>
2012-09-05 11:22:58 +0200

    episode: add summary and subtitle

--- lib/model/episode.php
+++ lib/model/episode.php
@@ -53,6 +53,7 @@ Episode::property( 'id', 'INT NOT NULL AUTO_INCREMENT PRIMARY KEY' );
 Episode::property( 'post_id', 'INT' );
 Episode::property( 'show_id', 'INT' );
 Episode::property( 'subtitle', 'VARCHAR(255)' );
+Episode::property( 'summary', 'TEXT' );
 Episode::property( 'active', 'INT' ); // publicized or not?
 Episode::property( 'enable', 'INT' ); // listed in podcast directories or not?
 Episode::property( 'slug', 'VARCHAR(255)' );
