2e7189cddd96979f48926fb71cddba210f7ed283
Eric Teubert <ericteubert@googlemail.com>
2012-11-03 13:46:39 +0100

    guess what? another version bump! :neckbeard:

--- podlove.php
+++ podlove.php
@@ -3,7 +3,7 @@
  * Plugin Name: Podlove Podcasting Plugin for WordPress
  * Plugin URI:  http://wordpress.org/extend/plugins/podlove-podcasting-plugin-for-wordpress/
  * Description: The one and only next generation podcast feed generator. Seriously. It's magical and sparkles a lot.
- * Version:     1.2.16a-alpha
+ * Version:     1.2.17-alpha
  * Author:      eteubert
  * Author URI:  ericteubert@googlemail.com
  * License:     MIT
