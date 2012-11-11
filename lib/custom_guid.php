526728a832802996bfb5236661b4a96529d4ace3
Eric Teubert <ericteubert@googlemail.com>
2012-11-11 12:52:41 +0100

    automatic guid regeneration

--- lib/custom_guid.php
+++ lib/custom_guid.php
@@ -18,13 +18,60 @@ class Custom_Guid {
 	}
 
 	public static function add_guid_form_element( $form_wrapper, $episode ) {
+		?>
+		<tr class="row__podlove_meta_guid">
+			<th scope="row" valign="top">
+				<label for="_podlove_meta_guid">GUID</label>
+			</th>
+			<td>
+				<div>
+					<span id="guid_preview"><?php echo get_the_guid() ?></span>
+					<a href="#" id="regenerate_guid"><?php echo __( 'regenerate', 'podlove' ) ?></a>
+				</div>
+				<span class="description">
+					<?php echo __( 'Identifier for this episode. Change it to force podcatchers to redownload media files.', 'podlove' ) ?>
+				</span>
+				<?php
+				$form_wrapper->builder->hidden( 'guid', array(
+					'label'       => __( 'GUID', 'podlove' ),
+					'default'     => get_the_guid()
+				));
+				?>
+				<script type="text/javascript">
+				jQuery(function($){
+					$("#regenerate_guid").on('click', function(e) {
+						e.preventDefault();
 
-		$form_wrapper->string( 'guid', array(
-			'label'       => __( 'GUID', 'podlove' ),
-			'description' => __( 'Identifier for this episode. Change it to force podcatchers to redownload media files.', 'podlove' ),
-			'html'        => array( 'class' => 'large-text' ),
-			'default'     => get_the_guid()
-		));
+						var data = {
+							action: 'podlove-get-new-guid',
+							post_id: jQuery("#post_ID").val()
+						};
+
+						$.ajax({
+							url: ajaxurl,
+							data: data,
+							dataType: 'json',
+							success: function(result) {
+								if (result && result.guid) {
+									$("#_podlove_meta_guid").val(result.guid);
+									$("#guid_preview").html(result.guid);
+									if ( ! $(".guid_warning").length ) {
+										$(".row__podlove_meta_guid .description")
+											.append("<br><strong class=\"guid_warning\">GUID regenerated. You still need to save the post.<br>Only regenerate if you messed up and need all clients to redownload all files!</strong>");
+									}
+								} else {
+									alert("Sorry, couln't generate new GUID.");
+								}
+							}
+						});
+
+						return false;
+					});
+				});
+				</script>
+			</td>
+		</tr>
+		<?php
 	}
 
 	public static function save_form( $post_id, $form_data ) {
