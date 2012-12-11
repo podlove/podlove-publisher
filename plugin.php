6e43945fb607e0fb9b2d8b80a0aa64a4a8cb2b66
Eric Teubert <ericteubert@googlemail.com>
2012-12-11 13:00:10 +0100

    when using the WordPress importer, don't create new GUIDs

--- plugin.php
+++ plugin.php
@@ -325,6 +325,26 @@ function clear_all_caches() {
 		wp_cache_clear_cache();
 }
 
+// Fix WordPress post meta import for our custom GUID
+// The importer inserts a post, which creates a new GUID. Then it adds the
+// post metas resulting in two GUID entries. Here we make sure to only use
+// the previous one.
+add_action( 'added_post_meta', function ( $meta_id, $post_id, $meta_key, $_meta_value ) {
+	
+	if ( $meta_key !== 'podlove_guid' )
+		return;
+
+	$metas = get_post_meta( $post_id, 'podlove_guid' );
+	if ( count( $metas ) > 1 ) {
+		foreach ( $metas as $meta ) {
+			if ( $meta !== $_meta_value ) {
+				delete_post_meta( $post_id, $meta_key, $meta );
+			}
+		}
+	}
+
+}, 10, 4 );
+
 namespace Podlove\AJAX;
 use \Podlove\Model;
 
