24839eedeb2743a10b21e892c40f13d151076607
Eric Teubert <ericteubert@googlemail.com>
2012-12-20 21:00:41 +0100

    refactor: brevity

--- lib/modules/categories/categories.php
+++ lib/modules/categories/categories.php
@@ -8,14 +8,10 @@ class Categories extends \Podlove\Modules\Base {
 	protected $module_description = 'Enable categories for episodes.';
 
 	public function load() {
-		
 		add_filter( 'podlove_post_type_args', function ( $args ) {
-
 			$args['taxonomies'][] = 'category';
-
 			return $args;		
 		} );
-
 	}
 
 }
