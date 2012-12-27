186df2df4edc910f338fe90f6c237affe1df5c78
Eric Teubert <ericteubert@googlemail.com>
2012-12-27 21:34:27 +0100

    un-default some modules: episode assistant, twitter card summary

--- plugin.php
+++ plugin.php
@@ -53,7 +53,7 @@ function activate_for_current_blog() {
 		}
 	}
 
-	$default_modules = array( 'podlove_web_player', 'episode_assistant', 'open_graph', 'twitter_summary_card' );
+	$default_modules = array( 'podlove_web_player', 'open_graph' );
 	foreach ( $default_modules as $module ) {
 		\Podlove\Modules\Base::activate( $module );
 	}
