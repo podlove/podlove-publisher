157f7b2d60e174914f41dac83a5c1f83de2d4456
Eric Teubert <ericteubert@googlemail.com>
2012-12-28 16:58:29 +0100

    allow underscores in slugs
    issue #93

--- lib/helper.php
+++ lib/helper.php
@@ -24,7 +24,7 @@ function get_setting( $name ) {
 function slugify( $text ) {
 
 	// replace non letter or digits by -
-	$text = preg_replace( '~[^\\pL\d]+~u', '-', $text );
+	$text = preg_replace( '~[^\\pL\d_]+~u', '-', $text );
 
 	// trim
 	$text = trim( $text, '-' );
