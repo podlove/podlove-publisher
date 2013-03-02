c5513e5b420b7b45beb4ce6d5773dac21b37b818
Eric Teubert <ericteubert@googlemail.com>
2013-03-02 16:20:51 +0100

    update link to github issues (new repo)

--- lib/settings/dashboard.php
+++ lib/settings/dashboard.php
@@ -32,7 +32,7 @@ class Dashboard {
 		?>
 		<ul>
 			<li>
-				<a target="_blank" href="https://github.com/eteubert/podlove/issues">Report Bugs</a>
+				<a target="_blank" href="https://github.com/podlove/podlove-publisher/issues">Report Bugs</a>
 			</li>
 			<li>
 				<a target="_blank" href="https://trello.com/board/podlove-publisher/508293f65573fa3f62004e0a">See what I'm working on</a>
