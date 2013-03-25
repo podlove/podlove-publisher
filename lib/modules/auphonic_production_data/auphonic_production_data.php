8a2bdaaf78dcb6f127008c0a1b5461879052c77c
Toby Baier <toby@isolation.de>
2013-03-25 23:53:31 +0100

    pip ajax request through wordpress for remote media bases

--- lib/modules/auphonic_production_data/auphonic_production_data.php
+++ lib/modules/auphonic_production_data/auphonic_production_data.php
@@ -9,7 +9,8 @@ class Auphonic_Production_Data extends \Podlove\Modules\Base {
 
 	public function load() {
 		add_action( 'admin_print_styles', array( $this, 'add_jquery_auphonicdata' ) );
-		add_action( 'podlove_episode_form', array( __CLASS__, 'add_media_base_url' ), 1, 2 );
+		add_action( 'podlove_episode_form', array( $this, 'add_media_base_url' ), 1, 2 );
+		add_action( 'wp_ajax_get_auphonic_data', array($this, 'get_auphonic_data_callback'));
 	}
 
 	function add_jquery_auphonicdata() {
@@ -30,4 +31,11 @@ class Auphonic_Production_Data extends \Podlove\Modules\Base {
 		</script>
 		<?php 
 	}
+
+	function get_auphonic_data_callback() {
+		$url = $_REQUEST['url'];
+		echo wp_remote_retrieve_body( wp_remote_get( $url ));
+		die(); // this is required to return a proper result
+	}
+
 }
