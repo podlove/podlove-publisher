ae55999a9413fc2f63171fda4003ec0f869a92a0
Eric Teubert <ericteubert@googlemail.com>
2013-04-13 15:26:49 +0200

    refactor: DRY up ajax functions

--- lib/ajax/ajax.php
+++ lib/ajax/ajax.php
@@ -4,11 +4,13 @@ use \Podlove\Model;
 
 class Ajax {
 
+	/**
+	 * Conventions: 
+	 * - all actions must be prefixed with "podlove-"
+	 * - hyphens in actions are substituted for underscores in methods
+	 */
 	public function __construct() {
 
-		// notes: 
-		//  - all actions must be prefixed with "podlove-"
-		//  - hyphens in actions are substituted for underscores in methods
 		$actions = array(
 			'get-new-guid',
 			'validate-file',
@@ -30,6 +32,12 @@ class Ajax {
 		die();
 	}
 
+	private function simulate_temporary_episode_slug( $slug ) {
+		add_filter( 'podlove_file_url_template', function ( $template ) use ( $slug ) {
+			return str_replace( '%episode_slug%', $slug, $template );;
+		} );
+	}
+
 	public function get_new_guid() {
 		$post_id = $_REQUEST['post_id'];
 
@@ -44,43 +52,39 @@ class Ajax {
 
 		$file = \Podlove\Model\MediaFile::find_by_id( $file_id );
 		$info = $file->curl_get_header();
+		$reachable = $info['http_code'] >= 200 && $info['http_code'] < 300;
 
-		$result = array();
-		$result['file_id']   = $file_id;
-		$result['reachable'] = ( $info['http_code'] >= 200 && $info['http_code'] < 300 );
-		$result['file_size'] = $info['download_content_length'];
-
-		$this->respond_with_json( $result );
+		$this->respond_with_json( array(
+			'file_url'	=> $file_url,
+			'reachable'	=> $reachable,
+			'file_size'	=> $info['download_content_length']
+		) );
 	}
 
 	public function validate_url() {
 		$file_url = $_REQUEST['file_url'];
 
 		$info = \Podlove\Model\MediaFile::curl_get_header_for_url( $file_url );
-
-		$result = array();
-		$result['file_url']  = $file_url;
-		$result['reachable'] = ( $info['http_code'] >= 200 && $info['http_code'] < 300 );
-		$result['file_size'] = $info['download_content_length'];
+		$reachable = $info['http_code'] >= 200 && $info['http_code'] < 300;
 
 		$validation_cache = get_option( 'podlove_migration_validation_cache', array() );
-		$validation_cache[ $file_url ] = $result['reachable'];
+		$validation_cache[ $file_url ] = $reachable;
 		update_option( 'podlove_migration_validation_cache', $validation_cache );
 
-		$this->respond_with_json( $result );
+		$this->respond_with_json( array(
+			'file_url'	=> $file_url,
+			'reachable'	=> $reachable,
+			'file_size'	=> $info['download_content_length']
+		) );
 	}
 
 	public function update_file() {
-		$file_id = $_REQUEST['file_id'];
+		$file_id = (int) $_REQUEST['file_id'];
 
 		$file = \Podlove\Model\MediaFile::find_by_id( $file_id );
 
-		if ( isset( $_REQUEST['slug'] ) ) {
-			// simulate a not-saved-yet slug
-			add_filter( 'podlove_file_url_template', function ( $template ) {
-				return str_replace( '%episode_slug%', $_REQUEST['slug'], $template );;
-			} );
-		}
+		if ( isset( $_REQUEST['slug'] ) )
+			$this->simulate_temporary_episode_slug( $_REQUEST['slug'] );
 
 		$info = $file->determine_file_size();
 		$file->save();
@@ -107,26 +111,22 @@ class Ajax {
 	}
 
 	public function create_file() {
-		$episode_id        = $_REQUEST['episode_id'];
-		$episode_asset_id  = $_REQUEST['episode_asset_id'];
+
+		$episode_id        = (int) $_REQUEST['episode_id'];
+		$episode_asset_id  = (int) $_REQUEST['episode_asset_id'];
 
 		if ( ! $episode_id || ! $episode_asset_id )
 			die();
 
-		if ( isset( $_REQUEST['slug'] ) ) {
-			// simulate a not-saved-yet slug
-			add_filter( 'podlove_file_url_template', function ( $template ) {
-				return str_replace( '%episode_slug%', $_REQUEST['slug'], $template );;
-			} );
-		}
+		if ( isset( $_REQUEST['slug'] ) )
+			$this->simulate_temporary_episode_slug( $_REQUEST['slug'] );
 
 		$file = Model\MediaFile::find_or_create_by_episode_id_and_episode_asset_id( $episode_id, $episode_asset_id );
 
-		$result = array();
-		$result['file_id']   = $file->id;
-		$result['file_size'] = $file->size;
-
-		$this->respond_with_json( $result );
+		$this->respond_with_json( array(
+			'file_id'   => $file->id,
+			'file_size' => $file->size
+		) );
 	}
 
 	public function update_asset_position() {
@@ -134,13 +134,10 @@ class Ajax {
 		$asset_id = (int)   $_REQUEST['asset_id'];
 		$position = (float) $_REQUEST['position'];
 
-		if ( $asset = Model\EpisodeAsset::find_by_id( $asset_id ) ) {
-			$asset->position = $position;
-			$asset->save();
-		}
+		Model\EpisodeAsset::find_by_id( $asset_id )
+			->update_attributes( array( 'position' => $position ) );
 
 		die();
 	}
 	
 }
-
