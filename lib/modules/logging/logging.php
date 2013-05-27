4548818a1299ae836293ad045529dbc93bafd316
Eric Teubert <eric@ericteubert.de>
2013-05-27 12:24:21 +0200

    send an email for critical errors

--- lib/modules/logging/logging.php
+++ lib/modules/logging/logging.php
@@ -1,8 +1,11 @@
 <?php 
 namespace Podlove\Modules\Logging;
-use \Podlove\Model;
-use \Podlove\Log;
-use \Podlove\Settings\Dashboard;
+
+use Monolog\Logger;
+
+use Podlove\Model;
+use Podlove\Log;
+use Podlove\Settings\Dashboard;
 
 class Logging extends \Podlove\Modules\Base {
 
@@ -25,6 +28,7 @@ class Logging extends \Podlove\Modules\Base {
 
 		$log = Log::get();
 		$log->pushHandler( new WPDBHandler( $wpdb, $log->get_log_level() ) );
+		$log->pushHandler( new WPMailHandler( get_option( 'admin_email' ), "Podlove | Critical notice for " . get_option( 'blogname' ), Logger::CRITICAL ) );
 	}
 
 	public function register_meta_box() {
@@ -33,6 +37,30 @@ class Logging extends \Podlove\Modules\Base {
 
 	public function dashoard_template() {
 		?>
+		<style type="text/css">
+		#podlove-log {
+			height: 500px;
+			overflow: auto;
+			font-family: monospace;
+			font-size: 14px;
+			line-height: 18px;
+			padding: 5px;
+		}
+
+		.log-level-200 {  }
+		.log-level-400 { color: #95002B; }
+		.log-level-550 { background: #95002B; color: #FAD4AF; }
+		.log-level-550 a { color: #F4E6AD; }
+		</style>
+
+		<script type="text/javascript">
+		jQuery(function($) {
+			$(document).ready(function() {
+				$("#podlove-log").scrollTop(1000000);
+			});
+		});
+		</script>
+		
 		<div id="podlove-log">
 		<?php foreach ( LogTable::all() as $log_entry ): ?>
 			<div class="log-entry log-level-<?php echo $log_entry->level ?>">
@@ -48,7 +76,8 @@ class Logging extends \Podlove\Modules\Base {
 					if ( isset( $data->media_file_id ) ) {
 						$media_file = Model\MediaFile::find_by_id( $data->media_file_id );
 						$episode = $media_file->episode();
-						echo sprintf( '<a href="%s">%s</a>', get_edit_post_link( $episode->post_id ), $episode->slug );
+						$asset = $media_file->episode_asset();
+						echo sprintf( '<a href="%s">%s / %s</a>', get_edit_post_link( $episode->post_id ), $episode->slug, $asset->title );
 					}
 					if ( isset( $data->episode_id ) ) {
 						$episode = Model\Episode::find_by_id( $data->episode_id );
@@ -59,28 +88,6 @@ class Logging extends \Podlove\Modules\Base {
 			</div>
 		<?php endforeach; ?>
 		</div>
-
-		<style type="text/css">
-		#podlove-log {
-			height: 500px;
-			overflow: auto;
-			font-family: monospace;
-			font-size: 14px;
-			line-height: 18px;
-			padding: 5px;
-		}
-
-		.log-level-200 {  }
-		.log-level-400 { color: #FF0000; }
-		</style>
-
-		<script type="text/javascript">
-		jQuery(function($) {
-			$(document).ready(function() {
-				$("#podlove-log").scrollTop(1000000);
-			});
-		});
-		</script>
 		<?php
 	}
 
