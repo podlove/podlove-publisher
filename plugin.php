9a2752ca26c96c231d62d669112d1999b2a8109b
Eric Teubert <eric@ericteubert.de>
2013-05-27 12:33:19 +0200

    activate logging by default

--- plugin.php
+++ plugin.php
@@ -54,7 +54,7 @@ function activate_for_current_blog() {
 	}
 
 	// set default modules
-	$default_modules = array( 'podlove_web_player', 'open_graph', 'asset_validation' );
+	$default_modules = array( 'podlove_web_player', 'open_graph', 'asset_validation', 'logging' );
 	foreach ( $default_modules as $module ) {
 		\Podlove\Modules\Base::activate( $module );
 	}
