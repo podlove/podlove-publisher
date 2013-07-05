58dfd148129d2376a263b86eefbfcfb61187dde2
Eric Teubert <eric@ericteubert.de>
2013-07-05 22:04:41 +0200

    extract css and js into separate files

--- lib/modules/auphonic/auphonic.php
+++ lib/modules/auphonic/auphonic.php
@@ -9,10 +9,12 @@ class Auphonic extends \Podlove\Modules\Base {
     protected $module_group = 'external services';
 	
     public function load() {
+
+			add_action( 'admin_print_styles', array( $this, 'admin_print_styles' ) );
     		
     		if($this->get_module_option('auphonic_api_key') == "") { } else {
     			add_action( 'podlove_episode_form_beginning', array( $this, 'auphonic_episodes' ), 10, 2 );
-				add_action( 'podlove_episode_form_beginning', array( $this, 'create_auphonic_production' ), 10, 2 );    			
+				// add_action( 'podlove_episode_form_beginning', array( $this, 'create_auphonic_production' ), 10, 2 );    			
     		}
     		
 			if( isset( $_GET["page"] ) && $_GET["page"] == "podlove_settings_modules_handle") {
@@ -93,6 +95,25 @@ class Auphonic extends \Podlove\Modules\Base {
     		
     		}
     }
+
+    public function admin_print_styles() {
+
+    	wp_register_style(
+    		'podlove_auphonic_admin_style',
+    		$this->get_module_url() . '/admin.css',
+    		false,
+    		\Podlove\get_plugin_header( 'Version' )
+    	);
+    	wp_enqueue_style('podlove_auphonic_admin_style');
+
+    	wp_register_script(
+    		'podlove_auphonic_admin_script',
+    		$this->get_module_url() . '/admin.js',
+    		array( 'jquery' ),
+    		\Podlove\get_plugin_header( 'Version' )
+    	);
+    	wp_enqueue_script('podlove_auphonic_admin_script');
+    }
     
     public function auphonic_episodes( $wrapper, $episode ) {
     	$wrapper->callback( 'import_from_auphonic_form', array(
@@ -140,274 +161,15 @@ class Auphonic extends \Podlove\Modules\Base {
     }
 
     public function auphonic_episodes_form() {
-
 		$asset_assignments = Model\AssetAssignment::get_instance();
-
 		?>
-		<script type='text/javascript'>
-		var PODLOVE = PODLOVE || {};
 
-		(function($){
-
-			PODLOVE.AuphonicImport = function () {
-
-				function get_chapters_string_from_data (data) {
-					var chapters_entry = "";
-
-					$.each(data.data.chapters, function(index, value) {
-						chapters_entry = chapters_entry + value.start + " " + value.title;
-						if (value.url == "") {
-					
-						} else {
-							chapters_entry = chapters_entry + " <" + value.url + ">";
-						}
-						chapters_entry = chapters_entry + '\n';
-					});
-
-					return chapters_entry;
-				}
-
-				function get_fields_to_update(data, chapter_asset_assignment) {
-					var fields = [
-						{ field: '#title'                 , value: data.data.metadata.title },
-						{ field: '#_podlove_meta_subtitle', value: data.data.metadata.subtitle },
-						{ field: '#_podlove_meta_summary' , value: data.data.metadata.summary },
-						{ field: '#_podlove_meta_duration', value: data.data.length_timestring },
-						{ field: '#_podlove_meta_slug'    , value: data.data.output_basename },
-						{ field: '#new-tag-post_tag'      , value: data.data.metadata.tags.join(" , ") },
-					];
-
-					if (chapter_asset_assignment == 'manual') {
-						fields.push({ field: '#_podlove_meta_chapters', value: get_chapters_string_from_data(data) });
-					}
-
-					return fields;
-				}
-
-				/**
-				 * Import and override existing fields.
-				 */
-				function do_force_import(data, chapter_asset_assignment) {
-					var fields = get_fields_to_update(data, chapter_asset_assignment);
-					$.each(fields, function (index, field) {
-						$(field.field).val(field.value);
-					});
-				}
-
-				/**
-				 * Import but do not override existing fields.
-				 */
-				function do_simple_import(data, chapter_asset_assignment) {
-					var fields = get_fields_to_update(data, chapter_asset_assignment);
-					$.each(fields, function (index, field) {
-						if ($(field.field).val() == "") {
-							$(field.field).val(field.value);
-						}
-					});
-				}
-				
-				/**
-				 * Create Auphonic production.
-				 */				
-				 function create_auphonic_production(token) {
-				 	var presetuuid = $("#create_auphonic_production_button").data('presetuuid');
-				 	var chapter_asset_assignment = "<?php echo $asset_assignments->chapters ?>";
-				 	var cover_art_asset_assignment = "<?php echo $asset_assignments->image ?>";
-				 	var module_url = "<?php echo $this->get_module_url(); ?>";
-				 	var auphonic_production_data = new Object();
-				 	var auphonic_production_metadata = new Object();
-				 	var auphonic_files = new Object();
-				 	
-				 	var raw_production_tags = $(".tagchecklist").text();
-				 	var raw_chapters = $("#_podlove_meta_chapters").val();
-				 	
-				 	var now = new Date();
-				 	var chapters = new Array();
-				 	
-				 	$("#fetch_create_production_status").html('<i class="podlove-icon-spinner rotate"></i>').show();
-				 	
-				 	if(typeof presetuuid !== undefined && presetuuid !== "") {
-				 		auphonic_production_data.preset = presetuuid;
-				 	}
-				 	
-				 	auphonic_production_data.length_timestring = $("#_podlove_meta_duration").val();
-				 	auphonic_production_data.output_basename= $("#_podlove_meta_slug").val();
-				 	auphonic_production_metadata.title = $("#title").val();
-				 	auphonic_production_metadata.subtitle = $("#_podlove_meta_subtitle").val();
-				 	auphonic_production_metadata.summary = $("#_podlove_meta_summary").val();
-				 	auphonic_production_metadata.year = now.getFullYear();
-				 	/* auphonic_production_metadata.tags = raw_production_tags.substring(2, raw_production_tags.length).split('X\u00a0'); */
-				 		
-				 	if(typeof chapter_asset_assignment !== 'undefined') {
-				 		if (chapter_asset_assignment == 'manual' && raw_chapters !== "") {
-				 			$(raw_chapters.split('\n')).each(function (index, value) {
-				 				if(value !== "\n" && value !== "") {
-									var chapter = new Object();
-									chapter.start = value.substring(0, value.indexOf(" "));
-									if(value.indexOf("<") == -1) {
-										chapter.title = value.substring(value.indexOf(" ") + 1, value.length);
-										chapter.url = "";
-									} else {
-										chapter.title = value.substring(value.indexOf(" ") + 1, value.lastIndexOf(" "));
-										chapter.url = value.substring(value.lastIndexOf(" "), value.length).substring(2, value.substring(value.lastIndexOf(" "), value.length).length - 1);
-									}
-									chapters[index] = chapter;
-									delete chapter;
-								}
-				 			});
-				 			auphonic_production_data.chapters = chapters;
-				 		}
-				 	}
-								 		
-				 	auphonic_production_data.metadata = auphonic_production_metadata;
-
-					$.post(module_url + "/create_auphonic_production.php?access_token=" + token, { data: JSON.stringify(auphonic_production_data) }).always(function(data) {
-						console.log(data);
-						var new_episode_data = data.data;						
-						$("#fetch_create_production_status")
-							.html('<i class="podlove-icon-ok"></i>')
-							.delay(250)
-							.fadeOut(500);
-						$("#new_created_episode_data")
-							.html('<i>The production was successfully created. You can edit this episode <a href="https://auphonic.com/engine/upload/edit/' + new_episode_data.uuid + '" target="_blank">here</a>.')
-							.delay(250);
-						delete new_episode_data;
-						fetch_episodes(token);
-					});
-				 }
-
-				function fetch_production_data(token) {
-					var uuid = $("#import_from_auphonic option:selected").val(),
-					    module_url = "<?php echo $this->get_module_url(); ?>",
-					    chapter_asset_assignment = "<?php echo $asset_assignments->chapters ?>";
-
-					$("#fetch_production_status").html('<i class="podlove-icon-spinner rotate"></i>').show();
-					$.getJSON(module_url + '/fetch_episode.php?uuid=' + uuid + '&access_token=' + token, function(data) {
-
-						// hide prompt label which usually is placed above the title field
-						$('#title-prompt-text').addClass('screen-reader-text');
-
-						if (document.getElementById('force_import_from_auphonic').checked) {
-							do_force_import(data, chapter_asset_assignment);
-						} else {
-							do_simple_import(data, chapter_asset_assignment);
-						}
-
-						// activate all assets if no asset is active
-						if ($(".media_file_row input[type=checkbox]:checked").length === 0) {
-							$(".media_file_row input[type=checkbox]:not(:checked)").click();
-						}
-					}).done(function() {
-						$("#fetch_production_status")
-							.html('<i class="podlove-icon-ok"></i>')
-							.delay(250)
-							.fadeOut(500);
-					});
-				}
-				
-				function fetch_episodes(token) {
-					$("#reload_episodes_status").html('<i class="podlove-icon-spinner rotate"></i>').show();				
-					var module_url = "<?php echo $this->get_module_url(); ?>";
-					var productions = $.getJSON(module_url + '/fetch_episodes.php?access_token=' + token, function(data) {
-						var production_list = new Array();
-						var auphonic_productions = data.data;
-						$("#import_from_auphonic").empty();
-						$(auphonic_productions).each(function(key, value) {				
-							var date = new Date(value.change_time);				
-							$("#import_from_auphonic").append('<option value="' + value.uuid + '">' + value.output_basename + ' (' + date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + (date.getDay() + 1)).slice(-2) + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ') [' + value.status_string + ']</option>\n');
-						});
-						
-						delete auphonic_productions;
-					}).done(function() {
-						$("#reload_episodes_status")
-							.html('<i class="podlove-icon-ok"></i>')
-							.delay(250)
-							.fadeOut(500);
-					});
-				}
-
-				$("#fetch_production_data_button").click(function () {
-					fetch_production_data($(this).data('token'));
-				});
-				
-				$("#create_auphonic_production_button").click(function () {
-					create_auphonic_production($(this).data('token'));
-				});
-				
-				$("#reload_productions_button").click(function () {
-					fetch_episodes($(this).data('token'));
-				});
-				
-				$("#open_production_button").click(function () {
-					window.open('https://auphonic.com/engine/upload/edit/' + $("#import_from_auphonic").find(":selected").val());
-				});
-				$(document).ready(function() {
-					fetch_episodes('<?php echo $this->get_module_option('auphonic_api_key') ?>');
-				});
-
-			}
-		}(jQuery));
-
-		jQuery(function($) {
-			PODLOVE.AuphonicImport();
-		});
-		</script>
-
-		<span class='description'>
-		<?php
-		/*	
-		if ( $asset_assignments->chapters == 'manual' ) {
-			echo __( "Title, subtitle, summary, tags, duration, episode media file slug and chapters will be imported from Auphonic.", 'podlove' );
-		} else {
-			echo __( "Title, subtitle, summary, tags, duration and episode media file slug will be imported from Auphonic.", 'podlove' );
-		}
-		*/
-		?>
-		</span>
-
-		<style type="text/css">
-		#auphonic-import-form, #create-auphonic-production-form {
-			line-height: 24px;
-			padding-top: 5px;
-		}
-
-		.auphonic-select-wrapper {
-			float: left;
-			margin-right: 10px;
-		}
-		
-		.auphonic-select-wrapper label {
-			display: inline-block;
-			margin-right: 5px;
-			vertical-align: baseline;
-		}
-		
-		#import_from_auphonic {
-			width: 280px;
-		}
-
-		.auphonic-button-wrapper { float: left;	}
-
-		#fetch_production_data_button, #create_auphonic_production_button {
-			padding-left: 15px;
-			margin-right: 5px;
-		}
-
-		#fetch_production_data_button > div, #create_auphonic_production_button > div {
-			display: inline-block;
-			width: 5px;
-		}
-
-		.auphonic-checkbox-wrapper {
-			float: left;
-			text-align: left;
-			margin-right: 10px;
-			vertical-align: baseline;
-		}
-
-		.auphonic-checkbox-wrapper label { vertical-align: baseline; }
-		.auphonic-checkbox-wrapper input { width: 18px; }
-		</style>
+		<input type="hidden" id="auphonic" value="1"
+			data-api-key="<?php echo $this->get_module_option('auphonic_api_key') ?>"
+			data-assignment-chapter="<?php echo $asset_assignments->chapters ?>"
+			data-assignment-image="<?php echo $asset_assignments->image ?>"
+			data-module-url="<?php echo $this->get_module_url() ?>"
+			/>
 
 		<div id="auphonic-import-form">
 		
@@ -439,28 +201,37 @@ class Auphonic extends \Podlove\Modules\Base {
 			
 			<div class="auphonic-button-wrapper" style="float: left; margin-right: 10px;">
 				<a class='button' id='reload_productions_button' class='button' data-token='<?php echo $this->get_module_option('auphonic_api_key') ?>'>
-					&#x21bb; <span id="reload_episodes_status"></span>
+					<i class="podlove-icon-repeat"></i> <span id="reload_episodes_status"></span>
 				</a>
 			</div>
 			
 			<div class="auphonic-button-wrapper" style="float: left; margin-right: 10px;">
 				<a class='button' id='open_production_button' class='button' data-token='<?php echo $this->get_module_option('auphonic_api_key') ?>'>
-					Open in Auphonic
+					<i class="podlove-icon-external-link">
+						&nbsp;Open in Auphonic
+					</i>
 				</a>
 			</div>
+			
+			<div style="clear: both"></div>
+			
+			<div style="margin: 5px 0px 0px 0px;">
+				<div class="auphonic-button-wrapper">
+					<a class='button' id='fetch_production_data_button' class='button' data-token='<?php echo $this->get_module_option('auphonic_api_key') ?>'>
+						<i class="podlove-icon-cloud-download">
+							&nbsp;Import episode data from production
+						</i>
+						<div>
+							<span id="fetch_production_status"></span>
+						</div>
+					</a>
+				</div>
 
-			<div class="auphonic-button-wrapper" style="float: left">
-				<a class='button' id='fetch_production_data_button' class='button' data-token='<?php echo $this->get_module_option('auphonic_api_key') ?>'>
-					Import from Auphonic
-					<div>
-						<span id="fetch_production_status"></span>
-					</div>
-				</a>
-			</div>
-
-			<div class="auphonic-checkbox-wrapper">
-				<input type='checkbox' id='force_import_from_auphonic'/>
-				<label for='force_import_from_auphonic' title="<?php echo __( 'Overwrite all fields, even if they are already filled out.', 'podlove' ) ?>"><?php echo __( 'Overwrite existing content', 'podlove' ) ?></label>
+				<div class="auphonic-checkbox-wrapper">
+					<input type='checkbox' id='force_import_from_auphonic'/>
+					<label for='force_import_from_auphonic' title="<?php echo __( 'Overwrite all fields, even if they are already filled out.', 'podlove' ) ?>"><?php echo __( 'Overwrite existing content', 'podlove' ) ?></label>
+				</div>
+				<div style="clear: both"></div>
 			</div>
 
 			<div style="clear: both"></div>
