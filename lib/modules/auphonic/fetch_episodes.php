2aea47e61c8aa10c14d6ecc4f1e46d0058188d43
Eric Teubert <eric@ericteubert.de>
2013-07-05 18:15:47 +0200

    fix notices

--- lib/modules/auphonic/fetch_episodes.php
+++ lib/modules/auphonic/fetch_episodes.php
@@ -1,12 +1,14 @@
 <?php
 		header('Content-type: application/json');
 		
-		$callurl = 'https://auphonic.com/api/productions.json?limit=10&bearer_token='.$_GET["access_token"];
+		$callurl = 'https://auphonic.com/api/productions.json?limit=10&bearer_token='.$_REQUEST["access_token"];
 
 		$ch = curl_init($callurl);                                                                      
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");       
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Podlove Publisher (http://podlove.org/)');  
-		curl_setopt($ch, CURLOPT_POSTFIELDS, urldecode($_POST["data"]));                                                            
+		if ( isset( $_REQUEST["data"] ) ) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, urldecode($_REQUEST["data"]));
+		}
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                     
 			'Content-type: application/json')                                                                      
 		);                                                              
