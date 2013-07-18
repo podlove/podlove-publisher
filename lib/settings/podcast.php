f55b3e5fcf5da212be14b0358073479126dfba30
Eric Teubert <eric@ericteubert.de>
2013-07-18 12:20:49 +0200

    podcast settings: move language settings up

--- lib/settings/podcast.php
+++ lib/settings/podcast.php
@@ -79,6 +79,13 @@ class Podcast {
 						'html'        => array( 'rows' => 3, 'cols' => 40, 'class' => 'autogrow' )
 					) );
 
+					$wrapper->select( 'language', array(
+						'label'       => __( 'Language', 'podlove' ),
+						'description' => __( '', 'podlove' ),
+						'default'     => get_bloginfo( 'language' ),
+						'options'  => \Podlove\Locale\locales()
+					) );
+
 					$wrapper->subheader(
 						__( 'Media', 'podlove' ),
 						__( 'The Podlove Publisher expects all your media files to be in the same <strong>Upload Location</strong>.
@@ -177,13 +184,6 @@ class Podcast {
 						                 . __( 'For placement within the older, text-based browse system, podcast feeds may list up to 3 category/subcategory pairs. (For example, "Music" counts as 1, as does "Business > Careers.") For placement within the newer browse system based on Category links, however, and for placement within the Top Podcasts and Top Episodes lists that appear in the right column of most podcast pages, only the first category listed in the feed is used.' )
 						                 . ' (<a href="http://www.apple.com/itunes/podcasts/specs.html#category" target="_blank">http://www.apple.com/itunes/podcasts/specs.html#category</a>)',
 						'options'  => \Podlove\Itunes\categories()
-					) );
-
-					$wrapper->select( 'language', array(
-						'label'       => __( 'Language', 'podlove' ),
-						'description' => __( '', 'podlove' ),
-						'default'     => get_bloginfo( 'language' ),
-						'options'  => \Podlove\Locale\locales()
 					) );
 
 					$wrapper->select( 'explicit', array(
