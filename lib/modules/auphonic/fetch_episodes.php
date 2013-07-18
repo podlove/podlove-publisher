888f7760ab6fa53b1865f58106f10c59cd0fce15
Eric Teubert <eric@ericteubert.de>
2013-07-18 08:47:58 +0200

    fix indentation

--- lib/modules/auphonic/fetch_episodes.php
+++ lib/modules/auphonic/fetch_episodes.php
@@ -1,22 +1,22 @@
 <?php
-		header('Content-type: application/json');
-		$callurl = 'https://auphonic.com/api/productions.json?limit=10&bearer_token='.$_REQUEST["access_token"];
-		$ch = curl_init($callurl);                                                                      
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");       
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Podlove Publisher (http://podlove.org/)');  
-		if ( isset( $_REQUEST["data"] ) ) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, urldecode($_REQUEST["data"]));
-		}
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                     
-			'Content-type: application/json')                                                                      
-		);                                                              
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);        
-	
-		$result = curl_exec($ch);
-		
-		if ( curl_errno( $ch ) ) {
-			header("HTTP/1.1 503 Service Temporarily Unavailable");
-			header("Status: 503 Service Temporarily Unavailable");
-		} else {
-			print_r( $result );
-		}
+header('Content-type: application/json');
+$callurl = 'https://auphonic.com/api/productions.json?limit=10&bearer_token='.$_REQUEST["access_token"];
+$ch = curl_init($callurl);                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");       
+curl_setopt($ch, CURLOPT_USERAGENT, 'Podlove Publisher (http://podlove.org/)');  
+if ( isset( $_REQUEST["data"] ) ) {
+	curl_setopt($ch, CURLOPT_POSTFIELDS, urldecode($_REQUEST["data"]));
+}
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                     
+	'Content-type: application/json')                                                                      
+);                                                              
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);        
+
+$result = curl_exec($ch);
+
+if ( curl_errno( $ch ) ) {
+	header("HTTP/1.1 503 Service Temporarily Unavailable");
+	header("Status: 503 Service Temporarily Unavailable");
+} else {
+	print_r( $result );
+}
