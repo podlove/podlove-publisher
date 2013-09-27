7a4ef8e2246ef4ec796fb66a034063b6116b424d
Eric Teubert <eric@ericteubert.de>
2013-09-27 21:47:47 +0200

    shortcode: add linking and filter by role

--- lib/modules/contributors/shortcodes.php
+++ lib/modules/contributors/shortcodes.php
@@ -22,15 +22,19 @@ class Shortcodes {
 		add_shortcode( 'podlove-contributors', array( $this, 'shortcode') );
 	}
 
+		
 	/**
 	 * Parameters:
 	 *
 	 *	style       - One of 'table', 'list'. Default: 'table'
 	 *	id          - Specify a contributor id to display a specific contributor avatar.
 	 *	avatars     - One of 'yes', 'no'. Display avatars in list views or not. Default: 'yes'
-	 *	avatar_size - Specify avatar size in pixel for single contributors. Default: 50
+	 *	avatarsize  - Specify avatar size in pixel for single contributors. Default: 50
 	 *	align       - One of 'left', 'right', 'none'. Align contributor. Default: none
 	 *	caption     - Optional caption for contributor avatars.
+	 *	linkto      - One of 'none', 'publicemail', 'www', 'adn', 'twitter', 'facebook', 'amazonwishlist'.
+	 *	              Links contributor name to the service if available. Default: 'none'
+	 *	role        - Filter lists by role. Default: 'all'
 	 * 
 	 * Examples:
 	 *
@@ -45,9 +49,11 @@ class Shortcodes {
 		$defaults = array(
 			'style' => 'table',
 			'id' => null,
-			'avatar_size' => 50,
+			'avatarsize' => 50,
 			'align' => 'none',
-			'avatars' => 'yes'
+			'avatars' => 'yes',
+			'linkto' => 'none',
+			'role' => 'all'
 		);
 		$this->settings = array_merge($defaults, $attributes);
 
@@ -73,16 +79,41 @@ class Shortcodes {
 		if ($this->settings['align'] == 'right')
 			$alignclass = 'alignright';
 
-		return '<div class="wp-caption ' . $alignclass . '" style="width: ' . $this->settings['avatar_size'] . 'px">
-				' . $contributor->getAvatar($this->settings['avatar_size']) . '
+		$avatar = $contributor->getAvatar($this->settings['avatarsize']);
+		$avatar = $this->wrapWithLink($contributor, $avatar);
+
+		return '<div class="wp-caption ' . $alignclass . '" style="width: ' . $this->settings['avatarsize'] . 'px">
+				' . $avatar . '
 			<p class="wp-caption-text">' . $this->settings['caption'] . '</p>
 		</div>';
+	}
+
+	/**
+	 * Maybe link text to named service.
+	 */
+	private function wrapWithLink($contributor, $linktext)
+	{
+		$service = $this->getService($this->settings['linkto']);
+
+		if (!$service || !$contributor->{$service['key']})
+			return $linktext;
+
+		return sprintf('<a href="%s">%s</a>',
+			sprintf($service['url_template'], $contributor->{$service['key']}),
+			$linktext
+		);
 	}
 
 	private function renderListOfContributors() {
 		// fetch contributions
 		$episode = Model\Episode::get_current();
 		$this->contributions = EpisodeContribution::all('WHERE `episode_id` = "' . $episode->id . '" ORDER BY `position` ASC');
+
+		if ($this->settings['role'] != 'all') {
+			$this->contributions = array_filter($this->contributions, function($c) {
+				return strtolower($this->settings['role']) == $c->getRole()->slug;
+			});
+		}
 
 		if (count($this->contributions) == 0)
 			return "";
@@ -110,7 +141,7 @@ class Shortcodes {
 				$contributor = $contribution->getContributor();
 				return '<span class="contributor">'
 				     . ($this->settings['avatars'] == 'yes' ? $contributor->getAvatar(18) . ' ' : '')
-				     . $contributor->publicname
+				     . $this->wrapWithLink($contributor, $contributor->publicname)
 				     . '</span>';
 		}, $this->contributions)) . '</span>';
 	}
@@ -138,7 +169,7 @@ EOD;
 		foreach ($this->contributions as $contribution) {
 			$contributor = $contribution->getContributor();
 			$body .= "<tr>";
-			$body .= "  <td>" . ($this->settings['avatars'] == 'yes' ? $contributor->getAvatar(18) . ' ' : '') . $contributor->publicname . "</td>";
+			$body .= "  <td>" . ($this->settings['avatars'] == 'yes' ? $contributor->getAvatar(18) . ' ' : '') . $this->wrapWithLink($contributor, $contributor->publicname) . "</td>";
 			$body .= "  <td>" . $this->getSocialButtons($contributor) . "</td>";
 			$body .= "  <td>" . $this->getDonationButton($contributor) . "</td>";
 			$body .= "</tr>";
@@ -149,47 +180,8 @@ EOD;
 
 	private function getSocialButtons($contributor)
 	{
-		$services = array(
-			array(
-				'key' => 'publicemail',
-				'url_template' => 'mailto:%s',
-				'title' => 'E-Mail',
-				'icon' => 'podlove-icon-mail'
-			),
-			array(
-				'key' => 'www',
-				'url_template' => '%s',
-				'title' => 'Homepage',
-				'icon' => 'podlove-icon-house'
-			),
-			array(
-				'key' => 'adn',
-				'url_template' => 'http://app.net/%s',
-				'title' => 'ADN',
-				'icon' => 'podlove-icon-appdotnet'
-			),
-			array(
-				'key' => 'twitter',
-				'url_template' => 'http://twitter.com/%s',
-				'title' => 'Twitter',
-				'icon' => 'podlove-icon-twitter'
-			),
-			array(
-				'key' => 'facebook',
-				'url_template' => '%s',
-				'title' => 'Facebook',
-				'icon' => 'podlove-icon-facebook'
-			),
-			array(
-				'key' => 'amazonwishlist',
-				'url_template' => '%s',
-				'title' => 'Wishlist',
-				'icon' => 'podlove-icon-cart'
-			),
-		);
-
 		$html = '';
-		foreach ($services as $service) {
+		foreach ($this->getServices() as $service) {
 			if ($contributor->{$service['key']}) {
 				$html .= sprintf(
 					'<a href="%s" class="contributor-contact %s" title="%s"><i class="%s"></i></a>',
@@ -230,6 +222,55 @@ EOD;
     		    t.parentNode.insertBefore(s, t);
    			 })();
 			/* ]]> */</script>\n";
+	}
+
+	private function getServices() {
+		return array(
+			array(
+				'key' => 'publicemail',
+				'url_template' => 'mailto:%s',
+				'title' => 'E-Mail',
+				'icon' => 'podlove-icon-mail'
+			),
+			array(
+				'key' => 'www',
+				'url_template' => '%s',
+				'title' => 'Homepage',
+				'icon' => 'podlove-icon-house'
+			),
+			array(
+				'key' => 'adn',
+				'url_template' => 'http://app.net/%s',
+				'title' => 'ADN',
+				'icon' => 'podlove-icon-appdotnet'
+			),
+			array(
+				'key' => 'twitter',
+				'url_template' => 'http://twitter.com/%s',
+				'title' => 'Twitter',
+				'icon' => 'podlove-icon-twitter'
+			),
+			array(
+				'key' => 'facebook',
+				'url_template' => '%s',
+				'title' => 'Facebook',
+				'icon' => 'podlove-icon-facebook'
+			),
+			array(
+				'key' => 'amazonwishlist',
+				'url_template' => '%s',
+				'title' => 'Wishlist',
+				'icon' => 'podlove-icon-cart'
+			),
+		);
+	}
+
+	private function getService($service){
+		$filtered = array_filter($this->getServices(), function($s) use ($service) {
+			return $s['key'] == $service;
+		});
+
+		return count($filtered) ? current($filtered) : null;
 	}
 
 }
