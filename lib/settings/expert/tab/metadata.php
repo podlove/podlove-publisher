26afa75260f973af3b9bb88372362d34ff927f2c
chemiker <alex@Skylake.local>
2013-10-18 21:00:09 +0200

    Added license selector

--- lib/settings/expert/tab/metadata.php
+++ lib/settings/expert/tab/metadata.php
@@ -53,6 +53,26 @@ class Metadata extends Tab {
 			/* $section  */ 'podlove_settings_episode'
 		);
 
+		add_settings_field(
+			/* $id       */ 'podlove_setting_episode_license',
+			/* $title    */ sprintf(
+				'<label for="enable_episode_license">%s</label>',
+				__( 'Enable license field.', 'podlove' )
+			),
+			/* $callback */ function () {
+				?>
+				<label>
+					<input name="podlove_metadata[enable_episode_license]" id="enable_episode_license" type="radio" value="1" <?php checked( \Podlove\get_setting( 'metadata', 'enable_episode_license' ), 1 ) ?> /> <?php echo __( 'enable', 'podlove' ) ?>
+				</label>
+				<label>
+					<input name="podlove_metadata[enable_episode_license]" id="enable_episode_license" type="radio" value="0" <?php checked( \Podlove\get_setting( 'metadata', 'enable_episode_license' ), 0 ) ?> /> <?php echo __( 'disable', 'podlove' ) ?>
+				</label>
+				<?php
+			},
+			/* $page     */ Settings::$pagehook,  
+			/* $section  */ 'podlove_settings_episode'
+		);
+
 		register_setting( Settings::$pagehook, 'podlove_metadata' );
 	}
 }
