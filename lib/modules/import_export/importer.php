3d1e4e0dfc84cc5d6ba0ed15f60a0e5dcdf2fabe
Eric Teubert <eric@ericteubert.de>
2013-10-28 14:40:58 +0100

    refactor importer: extract importEpisodes

--- lib/modules/import_export/importer.php
+++ lib/modules/import_export/importer.php
@@ -3,32 +3,34 @@ namespace Podlove\Modules\ImportExport;
 
 class Importer {
 
+	// path to import file
 	private $file;
+
+	// SimpleXML document of import file
+	private $xml;
 
 	public function __construct($file) {
 		$this->file = $file;
 	}
 
 	public function import() {
-		global $wpdb;
 
-		$xml = simplexml_load_file($this->file);
-		$xml->registerXPathNamespace('wpe', \Podlove\Exporter::XML_NAMESPACE);
-
-		$attr = function($element, $attribute) {
-			return (string) $element->xpath('wpe:' . $attribute)[0];
-		};
+		$this->xml = simplexml_load_file($this->file);
+		$this->xml->registerXPathNamespace('wpe', \Podlove\Exporter::XML_NAMESPACE);
 
 		// TODO: clean podlove tables beforehand?
 
-		// Import episodes.
-		// The only trouble here is that the post_id (probably) does not match 
-		// the real post id as it changes during export/import. However, there
-		// is an import_id which equals the previous post_id. This is fortunate
-		// as it enables mapping.
-		$episodes = $xml->xpath('//wpe:episode');
+		$this->importEpisodes();
+	}
+
+	private function importEpisodes()
+	{
+		global $wpdb;
+		
+		$episodes = $this->xml->xpath('//wpe:episode');
 		foreach ($episodes as $episode) {
 			$new_episode = new \Podlove\Model\Episode;
+
 			foreach ($episode->children('wpe', true) as $attribute) {
 				$value = (string) $attribute;
 				$wpdb->escape_by_ref($value);
@@ -41,7 +43,6 @@ class Importer {
 			} else {
 				// no matching post found
 			}
-
 		}
 	}
 
