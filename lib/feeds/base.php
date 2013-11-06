520d58331a1277080f2dd6819dc6d458e46a9a4e
chemiker <alex@Skylake.local>
2013-11-06 15:11:58 +0100

    Changes

--- lib/feeds/base.php
+++ lib/feeds/base.php
@@ -207,8 +207,8 @@ function override_feed_entry( $hook, $podcast, $feed, $format ) {
 		$summary = sprintf( '<itunes:summary>%s</itunes:summary>', $summary );
 		echo apply_filters( 'podlove_feed_itunes_summary', $summary );
 
-		$itunes_explicit = apply_filters( 'podlove_feed_content', strip_tags( $episode->explicit ) );
-		$itunes_explicit = sprintf( '<itunes:explicit>%s</itunes:explicit>', ( $episode->explicit == 2) ? 'clean' : ( ( $episode->explicit ) ? 'yes' : 'no' ) );
+		$itunes_explicit = apply_filters( 'podlove_feed_content', ( $episode->explicit == 2) ? 'clean' : ( ( $episode->explicit ) ? 'yes' : 'no' ) ) );
+		$itunes_explicit = sprintf( '<itunes:explicit>%s</itunes:explicit>', $itunes_explicit );
 		echo apply_filters( 'podlove_feed_itunes_explicit', $itunes_explicit );
 
 		if ( $cover_art_url ) {
