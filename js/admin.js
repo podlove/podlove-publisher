e4b4a05f6614187897c5bd9ab5c72d21f1ebc8ee
Eric Teubert <eric@ericteubert.de>
2013-11-15 16:55:24 +0100

    cleanup ProtectedFeed JS

--- js/admin.js
+++ js/admin.js
@@ -45,6 +45,51 @@ function human_readable_size(size) {
 	return megabytes.toFixed(2) + " MB";
 }
 
+(function($) {
+	PODLOVE.ProtectFeed = function() {
+		var $protection = $("#podlove_feed_protected"),
+			$protection_row = $("tr.row_podlove_feed_protection_type"),
+			$protection_type = $("#podlove_feed_protection_type"),
+			$credentials = $("tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user");
+
+		var protectionIsActive = function() {
+			return $protection.is(":checked");
+		};
+
+		var isCustomLogin = function() {
+			return $protection_type.val() == "0";
+		};
+
+		if (protectionIsActive()) {
+			$protection_row.show();
+		}
+		
+		if (protectionIsActive() && isCustomLogin()) {
+			$credentials.show();
+		}
+
+		$("#podlove_feed_protected").on("change", function() {
+			if (protectionIsActive()) {
+				$protection_row.show();
+				if (isCustomLogin()) {
+					$credentials.show();
+				} 
+			} else {
+				$protection_row.hide();
+				$credentials.hide();
+			}
+		});	
+
+		$protection_type.change(function() {
+			if (protectionIsActive() && isCustomLogin()) {
+				$credentials.show();
+			} else {
+				$credentials.hide();
+			}
+		});
+	}
+}(jQuery));
+
 jQuery(function($) {
 
 	$("#validation").each(function() {
@@ -70,30 +115,3 @@ jQuery(function($) {
 	$(".autogrow").autogrow();
 	
 });
-
-
-PODLOVE.ProtectFeed = function() {
-	if( jQuery("#podlove_feed_protected:checked").val() == "on" ) {
-		jQuery("tr.row_podlove_feed_protection_type").show();
-	}
-	if( jQuery("#podlove_feed_protection_type").val() == "0" && jQuery("#podlove_feed_protected:checked").val() == "on" ) {
-		jQuery("tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user").show();
-	}
-	jQuery("#podlove_feed_protected").change(function() {
-		if( jQuery("#podlove_feed_protected:checked").val() == "on" ) {
-			jQuery("tr.row_podlove_feed_protection_type").show();
-			if( jQuery("#podlove_feed_protection_type").val() == "0" ) {
-				jQuery("tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user").show();
-			} 
-		} else {
-			jQuery("tr.row_podlove_feed_protection_type,tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user").hide();
-		}
-	});	
-	jQuery("#podlove_feed_protection_type").change(function() {
-		if( jQuery("#podlove_feed_protection_type").val() == "0" && jQuery("#podlove_feed_protected:checked").val() == "on" ) {
-			jQuery("tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user").show();
-		} else {
-			jQuery("tr.row_podlove_feed_protection_password,tr.row_podlove_feed_protection_user").hide();
-		}
-	});
-}
