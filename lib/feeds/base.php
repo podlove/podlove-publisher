a7e3e7703736dbe18eb9c2c5f28b3f745c6c7ee0
Eric Teubert <eric@ericteubert.de>
2013-11-24 15:35:09 +0100

    reenable itunes:complete

--- lib/feeds/base.php
+++ lib/feeds/base.php
@@ -166,6 +166,10 @@ function override_feed_head( $hook, $podcast, $feed, $format ) {
         $explicit = sprintf( '<itunes:explicit>%s</itunes:explicit>', ( $podcast->explicit == 2) ? 'clean' : ( ( $podcast->explicit ) ? 'yes' : 'no' ) );
 		echo "\t" . apply_filters( 'podlove_feed_itunes_explicit', $explicit );
 		echo PHP_EOL;
+
+		$complete = sprintf( '<itunes:complete>%s</itunes:complete>', ( $podcast->complete ) ? 'yes' : 'no' );
+		echo "\t" . apply_filters( 'podlove_feed_itunes_complete', $complete );
+		echo PHP_EOL;
 	} );
 }
 
