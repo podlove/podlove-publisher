2645b86451d71da787f397794839f897916ede4f
Eric Teubert <eric@ericteubert.de>
2013-12-20 17:05:47 +0100

    finish episode template wrapper

--- lib/template/episode.php
+++ lib/template/episode.php
@@ -43,26 +43,12 @@ class Episode {
 		return $this->post->post_content;
 	}
 
-	public function chapters() {
-		return array_map(function($chapter) {
-			return new Chapter($chapter);
-		}, $this->episode->get_chapters()->toArray());
-	}
+	public function publicationDate($format = '') {
 
-	public function license() {
-		return new License(
-			new \Podlove\Model\License(
-				"episode",
-				array(
-					'type'                 => $this->episode->license_type,
-					'license_name'         => $this->episode->license_name,
-					'license_url'          => $this->episode->license_url,
-					'allow_modifications'  => $this->episode->license_cc_allow_modifications,
-					'allow_commercial_use' => $this->episode->license_cc_allow_commercial_use,
-					'jurisdiction'         => $this->episode->license_cc_license_jurisdiction,
-				)
-			)
-		);
+		if ($format === '')
+			$format = get_option('date_format');
+
+		return mysql2date($format, $this->post->post_date);
 	}
 
 	/**
@@ -89,11 +75,21 @@ class Episode {
 	 * Episode Duration
 	 *
 	 * Use duration("full") to include milliseconds.
+	 *
+	 * @todo  support custom formatstrings
 	 * 
 	 * @accessor
 	 */
 	public function duration($format = 'HH:MM:SS') {
 		return $this->episode->get_duration($format);
+	}
+
+	public function imageUrl() {
+		return $this->episode->get_cover_art();
+	}
+
+	public function imageUrlWithFallback() {
+		return $this->episode->get_cover_art_with_fallback();
 	}
 
 	/**
@@ -106,6 +102,28 @@ class Episode {
 		return array_map(function($contribution) {
 			return new Contributor($contribution->getContributor(), $contribution);
 		}, \Podlove\Modules\Contributors\Model\EpisodeContribution::find_all_by_episode_id($this->episode->id));
+	}
+
+	public function chapters() {
+		return array_map(function($chapter) {
+			return new Chapter($chapter);
+		}, $this->episode->get_chapters()->toArray());
+	}
+
+	public function license() {
+		return new License(
+			new \Podlove\Model\License(
+				"episode",
+				array(
+					'type'                 => $this->episode->license_type,
+					'license_name'         => $this->episode->license_name,
+					'license_url'          => $this->episode->license_url,
+					'allow_modifications'  => $this->episode->license_cc_allow_modifications,
+					'allow_commercial_use' => $this->episode->license_cc_allow_commercial_use,
+					'jurisdiction'         => $this->episode->license_cc_license_jurisdiction,
+				)
+			)
+		);
 	}
 
 }
