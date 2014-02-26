50d2bd2152a2e60c49dd47fde10a3ad380a7c168
Eric Teubert <eric@ericteubert.de>
2014-02-26 19:42:02 +0530

    found a way to document dynamic accessors \o/

--- bin/documentation.php
+++ bin/documentation.php
@@ -7,10 +7,16 @@ if (!getenv('WPBASE')) {
 	die("You need to set the environment variable WPBASE to your WordPress root\n");
 }
 
+require_once dirname(__FILE__) . '/../lib/helper.php';
 require_once getenv('WPBASE') . '/wp-load.php';
 require_once dirname(__FILE__) . '/../bootstrap/bootstrap.php';
 
 $output_dir = '/Users/ericteubert/code/podlove.github.com/sources/template';
+
+// classes containing dynamic accessors
+$dynamicAccessorClasses = [
+	'\Podlove\Modules\Contributors\Contributors'
+];
 
 $classes = [
 	'\Podlove\Template\Podcast',
@@ -24,8 +30,44 @@ $classes = [
 	'\Podlove\Modules\Contributors\Template\Contributor'
 ];
 
+// first, parse dynamic accessors
+$dynamicAccessors = [];
+foreach ($dynamicAccessorClasses as $class) {
+	$reflectionClass = new ReflectionClass($class);
+	$methods = $reflectionClass->getMethods();
+
+	$accessors = array_filter($methods, function($method) {
+		$comment = $method->getDocComment();
+		return stripos($comment, '@accessor') !== false && stripos($comment, '@dynamicAccessor') !== false;
+	});
+
+	$parsedMethods = array_map(function($method) {
+		$c = new Comment($method->getDocComment());
+		$c->parse();
+
+		$dynamicAccessor = $c->getTag('dynamicAccessor');
+		$callData = explode(".", $dynamicAccessor['description']);
+
+		return [
+			'methodname'  => $callData[1],
+			'title'       => $c->getTitle(),
+			'description' => $c->getDescription(),
+			'tags'        => $c->getTags(),
+			'class' => $callData[0]
+		];
+	}, $accessors);
+
+	foreach ($parsedMethods as $method) {
+		if (!isset($dynamicAccessors[$method['class']]))
+			$dynamicAccessors[$method['class']] = [];
+
+		$dynamicAccessors[$method['class']][$method['methodname']] = $method;
+	}
+}
+
 foreach ($classes as $class) {
 	$reflectionClass = new ReflectionClass($class);
+	$className = $reflectionClass->getShortName();
 	$methods = $reflectionClass->getMethods();
 
 	$accessors = array_filter($methods, function($method) {
@@ -45,28 +87,21 @@ foreach ($classes as $class) {
 		];
 	}, $accessors);
 
+	if (isset($dynamicAccessors[strtolower($className)])) {
+		foreach ($dynamicAccessors[strtolower($className)] as $dynamicMethodName => $dynamicMethod) {
+			$parsedMethods[] = $dynamicMethod;
+		}
+	}
+
 	$classComment = new Comment($reflectionClass->getDocComment());
 	$classComment->parse();
 	$templatetag = $classComment->getTags()[0]['description'];
 	 
 	assert(strlen($templatetag) > 0, 'templatetag must not be empty');
 
-	// Simply list dynamically added methods.
-	// Here we can't parse function doc (or can we?) so we need to find another way to get docs.
-	if (isset($class::$dynamicAccessors[$class::get_class_slug()])) {
-		foreach ($class::$dynamicAccessors[$class::get_class_slug()] as $dynamicAccessor) {
-			$parsedMethods[] = [
-				'methodname'  => $dynamicAccessor,
-				'title'       => '',
-				'description' => '',
-				'tags'        => array()
-			];
-		}
-	}
-
 	$classdoc = [
 		'class' => [
-			'classname' => $reflectionClass->getShortName(),
+			'classname'   => $className,
 			'templatetag' => $templatetag,
 			'description' => $classComment->getDescription()
 		],
