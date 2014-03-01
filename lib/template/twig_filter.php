a550a5cb33b211ff57802ecbd6c58571d5d82c5f
Eric Teubert <eric@ericteubert.de>
2014-03-01 21:03:14 +0530

    remove obsolete twig subtemplating filter

--- lib/template/twig_filter.php
+++ lib/template/twig_filter.php
@@ -38,7 +38,6 @@ class TwigFilter {
 		$loader = new \Twig_Loader_Chain($loaders);
 
 		$twig = new \Twig_Environment($loader, array('autoescape' => false));
-		$twig->addFilter(self::subtemplating_filter($twig));
 		$twig->addExtension(new \Twig_Extensions_Extension_I18n());
 
 		$context = $vars;
@@ -52,37 +51,5 @@ class TwigFilter {
 		}
 
 		return $twig->render($html, $context);
-	}
-
-	/**
-	 * Twig "template" Filter
-	 *
-	 * Enable subtemplating for all wrapper classes.
-	 *
-	 * Example:
-	 *
-	 * {{ episode|template("sub template id") }}
-	 */
-	protected static function subtemplating_filter($twig) {
-		return new \Twig_SimpleFilter('template', function ($context, $wrapperClass, $template_id) use ($twig) {
-
-			$reflectionClass = new \ReflectionClass($wrapperClass);
-			// todo use same comment class as in bin/documentation.php
-			preg_match('/@templatetag\s+(\w+)/', $reflectionClass->getDocComment(), $matches);
-
-			$templatetag = null;
-			if (isset($matches[1]))
-				$templatetag = trim($matches[1]);
-
-			if (!$templatetag)
-				return sprintf( __( 'Podlove Error: No subtemplating possible for wrapper class "%s"', 'podlove' ), $reflectionClass->name );
-
-			$context[$templatetag] = $wrapperClass;
-
-			if (!$template = Model\Template::find_one_by_title($template_id))
-				return sprintf( __( 'Podlove Error: Whoops, there is no template with id "%s"', 'podlove' ), $template_id );
-
-		    return $twig->render($template->content, $context);
-		}, array('needs_context' => true));
 	}
 }
