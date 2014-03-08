c49e0814d814933f8503b8b3d1a10c1d2d0c343b
chemiker <alex@Skylake.local>
2014-03-08 12:18:23 +0100

    is_object -> is_wp_error

--- lib/model/feed.php
+++ lib/model/feed.php
@@ -255,7 +255,7 @@ class Feed extends Base {
 			'_redirection' => ''
 		) );
 
-		if( is_object( $response ) )
+		if( is_wp_error( $response ) )
 			return FALSE; // Return FALSE if Error occured
 
 		return $curl->get_response();
@@ -278,7 +278,7 @@ class Feed extends Base {
 		) );
 		$response = $curl->get_response();
 
-		if( is_object( $response ) )
+		if( is_wp_error( $response ) )
 			return FALSE; // Return FALSE if Error occured
 
 		if( strpos( $response['body'], 'faultcode' ) )
