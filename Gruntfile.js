73e7906400590d028f4ec21ecfe3ffc28504c4f1
Sven Anders <github-commit@sven.anders.im>
2014-03-29 11:52:20 +0100

    Added msgmerge target

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,29 @@ module.exports = function(grunt) {
             expand: true,
         },
     },
+    shell: {
+    options: {
+      failOnError: true
+    },
+
+    msgmerge: {
+      command:  function () {
+          var locale = 'de_DE';
+          var po = "language/podlove-" + locale + ".po";
+          return "if [ -f \"" + po + "\" ]; then\n" +
+              "    echo \"Updating " + po + "\"\n" +
+              "    msgmerge " + po + " language/podlove.pot > .new.po.tmp\n" +
+              "    exitCode=$?\n" +
+              "    if [ $exitCode -ne 0 ]; then\n" +
+              "        echo \"Msgmerge failed with exit code $?\"\n" +
+              "        exit $exitCode\n" +
+              "    fi\n" +
+              "    mv .new.po.tmp " + po + "\n" +
+              "fi\n";
+      
+      }
+    }
+    },
     rsync: {
       dist: {
         src: "./",
@@ -49,10 +72,11 @@ module.exports = function(grunt) {
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks("grunt-rsync");
   grunt.loadNpmTasks('grunt-po2mo'); 
+  grunt.loadNpmTasks('grunt-shell'); 
   grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
   // Default task(s).
-  grunt.registerTask('default', ['makepot','po2mo','rsync']);
+    grunt.registerTask('default', ['makepot','shell:msgmerge','po2mo','rsync']);
 
 
 };
