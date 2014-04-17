603d5ac180313fe418278bb6a7ae8c1927664bdc
chemiker <alexlueken@gmail.com>
2014-04-17 10:35:42 +0200

    Further debug

--- lib/modules/app_dot_net/app_dot_net.php
+++ lib/modules/app_dot_net/app_dot_net.php
@@ -456,6 +456,8 @@ class App_Dot_Net extends \Podlove\Modules\Base {
         	$data['text'] = ( !empty( $episode->subtitle ) ? $episode->subtitle . "\n\n" : '' ) . $episode->summary;
         }
 
+        print_r($data);
+
         $this->broadcast($data);
 		
 		update_post_meta( $post_id, '_podlove_episode_was_published', true );
