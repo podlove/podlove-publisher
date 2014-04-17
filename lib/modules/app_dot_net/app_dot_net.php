a512dabf154f997fa6eaf0ae0e09c496d12f609a
chemiker <alexlueken@gmail.com>
2014-04-17 13:29:52 +0200

    Remove debug

--- lib/modules/app_dot_net/app_dot_net.php
+++ lib/modules/app_dot_net/app_dot_net.php
@@ -452,9 +452,6 @@ class App_Dot_Net extends \Podlove\Modules\Base {
         // Change Announcement text for broadcast
         $data['text'] = ( !empty( $episode->subtitle ) ? $episode->subtitle . "\n\n" : '' ) . $episode->summary;
 
-        print_r($episode);
-        print_r($data);
-
         $this->broadcast( $data, $post_id );
 		
 		update_post_meta( $post_id, '_podlove_episode_was_published', true );
