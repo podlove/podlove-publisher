4f7f18704505ae677590d715a272e5a498ee50fc
Eric Teubert <eric@ericteubert.de>
2014-06-17 16:29:03 +0800

    tracking export prototype

--- lib/modules/import_export/import_export.php
+++ lib/modules/import_export/import_export.php
@@ -10,6 +10,9 @@ class Import_Export extends \Podlove\Modules\Base {
 	protected $module_group = 'system';
 
 	public function load() {
+
+		add_action( 'wp_ajax_podlove-export-tracking', array($this, 'export_tracking') );
+		add_action( 'wp_ajax_podlove-export-tracking-status', array($this, 'export_tracking_status') );
 		
 		// hook into export feature
 		add_action('init', function() {
@@ -23,6 +26,18 @@ class Import_Export extends \Podlove\Modules\Base {
 				exit;
 			}
 
+			if (isset($_GET['podlove_export_tracking']) && $_GET['podlove_export_tracking']) {
+				
+				header( 'Content-Type: application/octet-stream' );
+				header( 'Content-Description: File Transfer' );
+				header( 'Content-Disposition: attachment; filename=tracking.csv.gz' );
+				header( 'Cache-control: private' );
+				header( 'Expires: -1' );
+
+				readfile(\Podlove\Modules\ImportExport\Import_Export::get_tracking_export_file_path());
+				exit;
+			}
+
 		});
 
 		add_action( 'admin_menu', array( $this, 'register_menu' ), 250 );
@@ -30,6 +45,52 @@ class Import_Export extends \Podlove\Modules\Base {
 
 	public function register_menu() {
 		new Settings\Settings( \Podlove\Podcast_Post_Type::SETTINGS_PAGE_HANDLE );
+	}
+
+	public static function get_tracking_export_file_path() {
+		$upload_dir = wp_upload_dir();
+		return $upload_dir['basedir'] . DIRECTORY_SEPARATOR . "tracking.tmp";
+	}
+
+	public function export_tracking() {
+		global $wpdb;
+
+		// only one export at a time
+		if (get_option('podlove_tracking_export_all') !== false)
+			return;
+
+		update_option('podlove_tracking_export_all', $wpdb->get_var("SELECT COUNT(*) FROM " . \Podlove\Model\DownloadIntent::table_name()));
+		update_option('podlove_tracking_export_progress', 0);
+
+		$rowsPerQuery = 1000;
+		$page = 0;
+
+		$fp = gzopen(self::get_tracking_export_file_path(), 'w');
+
+		do {
+			$sql = "SELECT * FROM " . \Podlove\Model\DownloadIntent::table_name() . " di LIMIT " . ($page*$rowsPerQuery) . ", $rowsPerQuery";
+			$rows = $wpdb->get_results($sql, ARRAY_A);
+			foreach ($rows as $row) {
+				gzwrite($fp, implode(",", $row) . "\n");
+			}
+
+			update_option('podlove_tracking_export_progress', $page*$rowsPerQuery);
+			$page++;
+		} while (count($rows) > 0);
+
+		gzclose($fp);
+
+		delete_option('podlove_tracking_export_all');
+		delete_option('podlove_tracking_export_progress');
+		exit;
+	}
+
+	public function export_tracking_status() {
+		echo json_encode(array(
+			'all'      => get_option('podlove_tracking_export_all'),
+			'progress' => get_option('podlove_tracking_export_progress')
+		));
+		exit;
 	}
 
 }
--- lib/modules/import_export/settings/settings.php
+++ lib/modules/import_export/settings/settings.php
@@ -126,7 +126,96 @@ class Settings {
 					</td>
 				</tr>
 			</table>
+
+			<hr>
+			<h2><?php echo __('Tracking Import &amp; Export', 'podlove') ?></h2>
+
+			<table class="form-table">
+				<tr>
+					<th scope="row" valign="top">
+						<h3 style="margin-bottom: 0px"><?php echo __('Export', 'podlove') ?></h3>
+					</th>
+				</tr>
+				<tr>
+					<td>
+						<p>
+							<?php echo __('Im- and export of tracking data is a separate task. After you have completed the steps above, you can ex- and import tracking data.', 'podlove'); ?>
+						</p>
+						<p>
+							<button id="podlove_tracking_export" class="button-primary"><?php echo __('Export Tracking Data', 'podlove') ?></button>
+							<span id="podlove_tracking_export_status_wrapper">
+								<?php echo __('Export', 'podlove') ?>: <span id="podlove_tracking_export_status">starting ...</span>
+							</span>
+						</p>
+					</td>
+				</tr>
+			</table>
+
 		</div>
+
+		<style type="text/css">
+		#podlove_tracking_export_status_wrapper {
+			display: none;
+		}
+		</style>
+
+		<script type="text/javascript">
+		(function($) {
+
+			var timeoutID = null,
+				isExporting = false;
+
+			var podlove_check_export_status = function() {
+
+				if (timeoutID) {
+					window.clearTimeout(timeoutID);
+				}
+
+				$.ajax({
+					url: ajaxurl,
+					data: {action: 'podlove-export-tracking-status'},
+					dataType: 'json',
+					success: function(result) {
+						if (result.all && result.progress) {
+							$("#podlove_tracking_export").attr('disabled', 'disabled');
+							$("#podlove_tracking_export_status").html((Math.round(1000.0 * (result.progress / result.all))/10.0) + "%");
+							$("#podlove_tracking_export_status_wrapper").show();
+
+							isExporting = true;
+							timeoutID = window.setTimeout(podlove_check_export_status, 1000);
+						} else {
+							$("#podlove_tracking_export").attr('disabled', false);
+							if (isExporting) {
+								$("#podlove_tracking_export_status_wrapper").hide();
+								window.location = window.location + "&podlove_export_tracking=1";
+							}
+						}
+					}
+				});
+
+			};
+
+			$("#podlove_tracking_export").on("click", function() {
+
+				$("#podlove_tracking_export").attr('disabled', 'disabled');
+				$("#podlove_tracking_export_status_wrapper").show();
+
+				$.ajax({
+					url: ajaxurl,
+					data: {action: 'podlove-export-tracking'},
+					dataType: 'json',
+					success: function(result) {
+						console.log("tracking export finished");
+					}
+				});
+
+				podlove_check_export_status();
+			});
+
+			// start immediately, in case the user refreshes the page
+			podlove_check_export_status();
+		}(jQuery));
+		</script>
 		<?php
 	}
 }
