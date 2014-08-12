19dabaf1045a86747f6169a6cb4976ebfe206d0a
Eric Teubert <eric@ericteubert.de>
2014-08-12 14:06:33 +0800

    repair button: fix duplicate contributions
    fixes #253

--- lib/repair.php
+++ lib/repair.php
@@ -30,6 +30,7 @@ class Repair {
 		self::clear_podlove_cache();
 		self::flush_rewrite_rules();
 		self::fix_data_inconsistencies();
+		self::fix_duplicate_contributions();
 
 		wp_redirect(admin_url('admin.php?page=' . $_REQUEST['page']));
 		exit;
@@ -94,6 +95,60 @@ class Repair {
 		);
 	}
 
+	private static function fix_duplicate_contributions()
+	{
+		global $wpdb;
+
+		$contributions = self::find_duplicate_episode_contributions();
+
+		if (!is_array($contributions) || empty($contributions)) {
+			self::add_to_repair_log(__('Contributions did not need repair', 'podlove'));
+			return;
+		}
+
+		foreach ($contributions as $contribution) {
+			$sql = "
+				DELETE FROM
+					" . \Podlove\Modules\Contributors\Model\EpisodeContribution::table_name() . "
+				WHERE
+					id != " . $contribution['id'] . "
+					AND `contributor_id` = \"" . $contribution['contributor_id'] . "\"
+					AND `episode_id` = \"" . $contribution['episode_id'] . "\"
+					AND `role_id` = \"" . $contribution['role_id'] . "\"
+					AND `group_id` = \"" . $contribution['group_id'] . "\"
+				";
+			$wpdb->query($sql);
+
+			$ec = \Podlove\Modules\Contributors\Model\EpisodeContribution::find_by_id($contribution['id']);
+			$ec->save(); // recalculates contribution count
+		}
+
+		self::add_to_repair_log(
+			sprintf(
+				_n( 'Deleted 1 duplicate contribution', 'Deleted %s duplicate contributions', count($contributions), 'podlove' ),
+				count($contributions)
+			)
+		);
+	}
+
+	private static function find_duplicate_episode_contributions() {
+		global $wpdb;
+
+		$sql = "
+			SELECT
+				id, contributor_id, episode_id, role_id, group_id, COUNT(*) cnt
+			FROM
+				" . \Podlove\Modules\Contributors\Model\EpisodeContribution::table_name() . "
+			GROUP BY
+				contributor_id, episode_id, role_id, group_id
+			HAVING
+				cnt > 1
+			ORDER BY
+				cnt DESC
+		";
+		return $wpdb->get_results($sql, ARRAY_A);
+	}
+
 	private static function find_duplicate_services() {
 		global $wpdb;
 
@@ -145,7 +200,7 @@ class Repair {
 				</li>
 				<li>
 					<strong>fix data inconsistencies</strong>
-					If you have duplicate data (for example a service appearing twice), this might fix it.
+					If you have duplicate data (for example a service or contribution appearing twice), this might fix it.
 				</li>
 			</ul>
 			<?php echo __('Feel free to press this button as often as you like. Worst case scenario: nothing happens.', 'podlove') ?>
