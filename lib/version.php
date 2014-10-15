130b728c93cc6f02b28452b1d41b72d4dafd1ea2
Chemiker <alexlueken@gmail.com>
2014-10-15 16:50:05 +0200

    New Features for ADN Module
    * Optional Announcement fallback to Podcast Image
    * Show Image in Announcement Preview

--- lib/version.php
+++ lib/version.php
@@ -40,7 +40,7 @@
 namespace Podlove;
 use \Podlove\Model;
 
-define( __NAMESPACE__ . '\DATABASE_VERSION', 85 );
+define( __NAMESPACE__ . '\DATABASE_VERSION', 86 );
 
 add_action( 'admin_init', '\Podlove\maybe_run_database_migrations' );
 add_action( 'admin_init', '\Podlove\run_database_migrations', 5 );
@@ -957,6 +957,13 @@ function run_migrations_for_version( $version ) {
 		break;
 		case 85:
 			add_option('podlove_tracking_delete_head_requests', 1);
+		break;
+		case 86:
+			if (\Podlove\Modules\Base::is_active('app_dot_net')) {
+				$adn = \Podlove\Modules\AppDotNet\App_Dot_Net::instance();
+				if ( $adn->get_module_option( 'adn_auth_key' ) )
+					$adn->update_module_option( 'adn_poster_image_fallback', 'on' );
+			}
 		break;
 	}
 
