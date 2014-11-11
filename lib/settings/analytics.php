353ca27d56cc45b265cd956c45f7afe215bf65fc
Eric Teubert <eric@ericteubert.de>
2014-11-11 23:55:44 +0100

    remove debug method

--- lib/settings/analytics.php
+++ lib/settings/analytics.php
@@ -377,14 +377,6 @@ class Analytics {
 
 
 		<script type="text/javascript">
-		function print_filter(filter){
-			var f=eval(filter);
-			if (typeof(f.length) != "undefined") {}else{}
-			if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
-			if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
-			console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
-		} 
-
 		var assetNames = <?php
 			$assets = Model\EpisodeAsset::all();
 			echo json_encode(
