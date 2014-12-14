f53ae8258821af394ddcc6e77a23a462a4c72167
Eric Teubert <eric@ericteubert.de>
2014-12-14 14:26:20 +0100

    add PHP deprecation warning

---
+++ lib/php_deprecation_warning.php
@@ -0,0 +1,41 @@
+<?php
+namespace Podlove;
+
+class PhpDeprecationWarning {
+
+	public static $target_version = "5.4";
+
+	public static function init() {
+
+		$correct_php_version = version_compare( phpversion(), self::$target_version, ">=" );
+
+		if (!$correct_php_version) {
+			add_action( 'admin_notices', array(__CLASS__, 'show_warning') );
+		}
+	}
+
+	public static function show_warning() {
+		?>
+		<div id="message" class="error">
+			<p>
+				<strong><?php echo __( 'Please upgrade your PHP as soon as possible.', 'podlove' ) ?></strong>
+			</p>
+			<p>
+				<?php echo sprintf(
+					__( 'You are running PHP %s, which is deprecated.', 'podlove' ),
+					phpversion()
+				); ?>
+				<?php echo sprintf(
+					__('Read %sour blogpost%s for further details.'),
+					'<a target="_blank" href="http://podlove.org/2014/08/14/podlove-publisher-2-phasing-out-php-5-3/">',
+					'</a>'
+				); ?>
+			</p>
+			<p>
+				<?php echo __('As long as you opt to not upgrade, do not attempt to update to Podlove Publisher 2.0 and above.', 'podlove') ?>
+			</p>
+		</div>
+		<?php
+	}
+
+}
