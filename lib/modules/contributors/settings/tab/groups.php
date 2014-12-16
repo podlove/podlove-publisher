40516807678e6bf63a26e662758858ae7313a90a
Eric Teubert <eric@ericteubert.de>
2014-12-16 19:48:36 +0100

    simplify tab implementation

--- lib/modules/contributors/settings/tab/groups.php
+++ lib/modules/contributors/settings/tab/groups.php
@@ -18,52 +18,55 @@ class Groups extends Tab {
 	}
 
 	public function getObject() {
-
-		if (!$this->page) {
-			$this->page = new \Podlove\Modules\Contributors\Settings\GenericEntitySettings(
-				'group',
-				'\Podlove\Modules\Contributors\Model\ContributorGroup'
-			);
-
-			$this->page->set_form(function($form_args, $group, $action) {
-
-				\Podlove\Form\build_for( $group, $form_args, function ( $form ) {
-					$wrapper = new \Podlove\Form\Input\TableWrapper( $form );
-
-					$wrapper->string( 'title', array(
-						'label' => __( 'Group Title', 'podlove' ),
-						'html'  => array( 'class' => 'required' )
-					) );
-
-					$wrapper->string( 'slug', array(
-						'label' => __( 'Group Slug', 'podlove' ),
-						'html'  => array( 'class' => 'required' )
-					) );
-
-				} );
-
-			});
-
-			$this->page->enable_tabs('groups');
-			$this->page->set_labels(array(
-				'delete_confirm' => __( 'You selected to delete the group "%s". Please confirm this action.', 'podlove' ),
-				'add_new' => __( 'Add new group', 'podlove' ),
-				'edit' => __( 'Edit group', 'podlove' )
-			));
-
-			add_action('podlove_settings_group_view', function() {
-				echo sprintf(
-					__('Use groups to divide contributors by type of participation. Create a group for teams working together or for a supporting community. Team members can be displayed separately by using the %sappropriate option%s to select a group.', 'podlove'),
-					'<a href="http://docs.podlove.org/ref/template-tags.html#contributors" target="_blank">',
-					'</a>'
-				);
-				$table = new \Podlove\Modules\Contributors\Contributor_Group_List_Table();
-				$table->prepare_items();
-				$table->display();
-			});
-		}
+		
+		if (!$this->page)
+			$this->createObject();
 
 		return $this->page;
+	}
+
+	public function createObject() {
+		$this->page = new \Podlove\Modules\Contributors\Settings\GenericEntitySettings(
+			'group',
+			'\Podlove\Modules\Contributors\Model\ContributorGroup'
+		);
+
+		$this->page->set_form(function($form_args, $group, $action) {
+
+			\Podlove\Form\build_for( $group, $form_args, function ( $form ) {
+				$wrapper = new \Podlove\Form\Input\TableWrapper( $form );
+
+				$wrapper->string( 'title', array(
+					'label' => __( 'Group Title', 'podlove' ),
+					'html'  => array( 'class' => 'required' )
+				) );
+
+				$wrapper->string( 'slug', array(
+					'label' => __( 'Group Slug', 'podlove' ),
+					'html'  => array( 'class' => 'required' )
+				) );
+
+			} );
+
+		});
+
+		$this->page->enable_tabs('groups');
+		$this->page->set_labels(array(
+			'delete_confirm' => __( 'You selected to delete the group "%s". Please confirm this action.', 'podlove' ),
+			'add_new' => __( 'Add new group', 'podlove' ),
+			'edit' => __( 'Edit group', 'podlove' )
+		));
+
+		add_action('podlove_settings_group_view', function() {
+			echo sprintf(
+				__('Use groups to divide contributors by type of participation. Create a group for teams working together or for a supporting community. Team members can be displayed separately by using the %sappropriate option%s to select a group.', 'podlove'),
+				'<a href="http://docs.podlove.org/ref/template-tags.html#contributors" target="_blank">',
+				'</a>'
+			);
+			$table = new \Podlove\Modules\Contributors\Contributor_Group_List_Table();
+			$table->prepare_items();
+			$table->display();
+		});
 	}
 
 }
