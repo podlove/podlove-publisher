c0b5b80bbc36680527bae9266d4c0568d9d73ac4
Eric Teubert <eric@ericteubert.de>
2015-02-09 13:31:11 +0800

    give user agent refresh more time
    It turns out that WordPress does not start a new process for each cron
    event. So even if I schedule a cron for each 500 UAs, they might all be
    executed in the same request and run into timeout.
    
    So I decided to make 500UA-chunks instead and increase the distribution
    time, increasing the chances of random clicks by the admin during that
    time. It’s not absolutely failsafe, but let’s see if it turns out to be
    good enough.

--- includes/user_agent_refresh.php
+++ includes/user_agent_refresh.php
@@ -16,14 +16,14 @@ use \Podlove\Model\UserAgent;
 add_action('podlove_parse_user_agents', 'podlove_refresh_user_agents');
 
 function podlove_init_user_agent_refresh() {
-	foreach (range(0, floor(UserAgent::count()/1000)*1000, 1000) as $start_id) {
-		wp_schedule_single_event( time() + mt_rand(2, 10), 'podlove_parse_user_agents', [$start_id] );
+	foreach (range(0, floor(UserAgent::count()/500)*500, 500) as $start_id) {
+		wp_schedule_single_event( time() + mt_rand(2, 30), 'podlove_parse_user_agents', [$start_id] );
 	}
 }
 
 function podlove_refresh_user_agents($start_id) {
 
-	$agents = UserAgent::find_all_by_where(sprintf("id >= %d ORDER BY id ASC LIMIT 1000", $start_id));
+	$agents = UserAgent::find_all_by_where(sprintf("id >= %d ORDER BY id ASC LIMIT 500", $start_id));
 
 	foreach ($agents as $ua) {
         $ua->parse()->save();
