60c40920ed2ba15e47f103578727e1129a724889
Eric Teubert <eric@ericteubert.de>
2015-02-25 11:22:35 +0800

    Revert "Introduce a filter for episode assets"
    This reverts commit 757856ff210b79f20882e5db1fad166297962c07.

--- lib/settings/episode_asset.php
+++ lib/settings/episode_asset.php
@@ -248,6 +248,11 @@ class EpisodeAsset {
 					}
 				}
 
+				$wrapper->select( 'image', array(
+					'label'   => __( 'Episode Image', 'podlove' ),
+					'options' => $artwork_options
+				) );
+
 				$chapter_file_options = array(
 					'0'      => __( 'None', 'podlove' ),
 					'manual' => __( 'Manual Entry', 'podlove' )
@@ -259,15 +264,6 @@ class EpisodeAsset {
 						$chapter_file_options[ $episode_asset->id ] = sprintf( __( 'Asset: %s', 'podlove' ), $episode_asset->title );
 					}
 				}
-
-				// Offer a filter to provide optional assets to the publisher
-				$artwork_options = apply_filters( 'podlove_artwork_assets', $artwork_options );
-				$chapter_file_options = apply_filters( 'podlove_chapter_assets', $chapter_file_options );
-
-				$wrapper->select( 'image', array(
-					'label'   => __( 'Episode Image', 'podlove' ),
-					'options' => $artwork_options
-				) );
 				$wrapper->select( 'chapters', array(
 					'label'   => __( 'Episode Chapters', 'podlove' ),
 					'options' => $chapter_file_options
