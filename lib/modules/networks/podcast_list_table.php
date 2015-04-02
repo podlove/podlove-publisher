76b1acb85552824509df875429c142c7d1e3ace7
Eric Teubert <eric@ericteubert.de>
2015-04-02 13:56:49 +0800

    handle "empty" podcasts in network list

--- lib/modules/networks/podcast_list_table.php
+++ lib/modules/networks/podcast_list_table.php
@@ -18,7 +18,11 @@ class Podcast_List_Table extends \Podlove\List_Table {
 	public function column_title( $podcast ) {
 		switch_to_blog( $podcast->blog_id );
 		
-		return "<a href='" . admin_url() . "admin.php?page=podlove_settings_handle'>" . $podcast->title . "</a> <br />" . $podcast->subtitle;
+		if ($podcast->title) {
+			return "<a href='" . admin_url() . "admin.php?page=podlove_settings_handle'>" . $podcast->title . "</a> <br />" . $podcast->subtitle;
+		} else {
+			return sprintf(__("No podcast title in blog %s.", 'podlove'), '<a href="' . admin_url() . '">' . get_bloginfo("name") . '</a>');
+		}
 	}
 
 	public function column_logo( $podcast ) {
@@ -38,10 +42,13 @@ class Podcast_List_Table extends \Podlove\List_Table {
 		switch_to_blog( $podcast->get_blog_id() );
 
 		$episodes = array_filter( \Podlove\Model\Episode::find_all_by_time() , function($e) { return $e->is_valid(); });
-		$latest_episode = reset($episodes);
-		$latest_episode_blog_post = get_post( $latest_episode->post_id );
- 		return "<a title='Published on " . date('Y-m-d h:i:s', strtotime( $latest_episode_blog_post->post_date )) ."' href='" . admin_url() . "post.php?post=" . $latest_episode->post_id . "&action=edit'>" . $latest_episode_blog_post->post_title . "</a>"
- 				. "<br />" . \Podlove\relative_time_steps( strtotime( $latest_episode_blog_post->post_date ) );
+		if ($latest_episode = reset($episodes)) {
+			$latest_episode_blog_post = get_post( $latest_episode->post_id );
+	 		return "<a title='Published on " . date('Y-m-d h:i:s', strtotime( $latest_episode_blog_post->post_date )) ."' href='" . admin_url() . "post.php?post=" . $latest_episode->post_id . "&action=edit'>" . $latest_episode_blog_post->post_title . "</a>"
+ 			     . "<br />" . \Podlove\relative_time_steps( strtotime( $latest_episode_blog_post->post_date ) );
+		} else {
+			return "—";
+		}
 	}
 
 	public function get_columns(){
