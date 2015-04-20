podlove/podlove-publisher

commit 98aaef1f353b87f99a5c1597a2c4e6b70fcf1dab

only flush rewrite rules when required
They were accidentally flushed on every admin request. Oops.
Did not create any bugs but probably slowed things down a bit.

lib/settings/settings.php
@@ -26,6 +26,9 @@ class Settings {
 
 		$this->init_page_documentation(self::$pagehook);
 
+		if (filter_input(INPUT_GET, 'page') !== 'podlove_settings_settings_handle')
+			return;
+
 		$tabs = new Tabs( __( 'Expert Settings', 'podlove' ) );
 		$tabs->addTab( new Tab\Website( __( 'Website', 'podlove' ), true ) );
 		$tabs->addTab( new Tab\Metadata( __( 'Metadata', 'podlove' ) ) );

lib/settings/expert/tab/website.php
@@ -5,8 +5,11 @@ use \Podlove\Settings\Expert\Tab;
 
 class Website extends Tab {
 	public function init() {
+		
 		// always flush rewrite rules here for custom_episode_slug setting
-		set_transient( 'podlove_needs_to_flush_rewrite_rules', true );
+		if ($this->is_active()) {
+			set_transient( 'podlove_needs_to_flush_rewrite_rules', true );
+		}
 
 		add_settings_section(
 			/* $id 		 */ 'podlove_settings_general',

commit f7a09891f08da9b1aa494b4e14ce2d5a4bc7ff71

fix expert settings save bug introduced in 98aaef1

lib/settings/settings.php
@@ -26,7 +26,7 @@ class Settings {
 
 		$this->init_page_documentation(self::$pagehook);
 
-		if (filter_input(INPUT_GET, 'page') !== 'podlove_settings_settings_handle')
+		if (filter_input(INPUT_GET, 'page') !== 'podlove_settings_settings_handle' && stripos(filter_input(INPUT_SERVER, 'REQUEST_URI'), 'options.php') === FALSE)
 			return;
 
 		$tabs = new Tabs( __( 'Expert Settings', 'podlove' ) );