ad4ef05d45376e217152e051b8fb2b1405dd5342
eteubert <eric@ericteubert.de>
2015-07-02 15:10:01 +0800

    tests: fix plugin loading
    - plugins_loaded is a better hook than muplugins_loaded
    - also, the plugin has to be activated by hook, not after the whole
    core is loaded

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,16 +11,11 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../podlove.php';
+	echo "Installing Podlove Publisher...\n";
+	\Podlove\activate_for_current_blog();
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter('plugins_loaded', '_manually_load_plugin');
 
 require $_tests_dir . '/includes/bootstrap.php';
-
-// activate_plugin( 'podlove-podcasting-plugin-for-wordpress/podlove.php' );
-
-echo "Installing Podlove Publisher...\n";
-
-// Install Publisher
-\Podlove\activate_for_current_blog();
 
 require_once 'helper/episode_factory.php';
