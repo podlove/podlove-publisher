48f04fce7a96b134061325647182c33f49ffa07b
eteubert <eric@ericteubert.de>
2015-08-30 17:34:13 +0800

    only require admin styles/scripts where needed

--- includes/admin_styles.php
+++ includes/admin_styles.php
@@ -1,18 +1,27 @@
 <?php
 
 add_action( 'admin_print_styles', function () {
-	wp_register_style( 'podlove-admin', \Podlove\PLUGIN_URL . '/css/admin.css', array(), \Podlove\get_plugin_header( 'Version' ) );
-	wp_enqueue_style( 'podlove-admin' );
 
-	wp_register_style( 'podlove-admin-chosen', \Podlove\PLUGIN_URL . '/js/admin/chosen/chosen.min.css', array(), \Podlove\get_plugin_header( 'Version' ) );
-	wp_enqueue_style( 'podlove-admin-chosen' );
+	$screen = get_current_screen();
 
-	wp_register_style( 'podlove-admin-image-chosen', \Podlove\PLUGIN_URL . '/js/admin/chosen/chosenImage.css', array(), \Podlove\get_plugin_header( 'Version' ) );
-	wp_enqueue_style( 'podlove-admin-image-chosen' );
+	$is_podlove_settings_screen = stripos($screen->id, 'podlove') !== false;
+	$is_episode_edit_screen = $screen->base == 'post' && $screen->post_type == 'podcast';
 
-	wp_register_style( 'podlove-admin-font', \Podlove\PLUGIN_URL . '/css/admin-font.css', array(), \Podlove\get_plugin_header( 'Version' ) );
-	wp_enqueue_style( 'podlove-admin-font' );
+	if ($is_podlove_settings_screen || $is_episode_edit_screen) {
+		wp_register_style( 'podlove-admin', \Podlove\PLUGIN_URL . '/css/admin.css', array(), \Podlove\get_plugin_header( 'Version' ) );
+		wp_enqueue_style( 'podlove-admin' );
 
-	wp_register_script( 'podlove-cryptojs', \Podlove\PLUGIN_URL . '/js/admin/cryptojs/md5.js' );
-	wp_enqueue_script( 'podlove-cryptojs' );
+		wp_register_style( 'podlove-admin-chosen', \Podlove\PLUGIN_URL . '/js/admin/chosen/chosen.min.css', array(), \Podlove\get_plugin_header( 'Version' ) );
+		wp_enqueue_style( 'podlove-admin-chosen' );
+
+		wp_register_style( 'podlove-admin-image-chosen', \Podlove\PLUGIN_URL . '/js/admin/chosen/chosenImage.css', array(), \Podlove\get_plugin_header( 'Version' ) );
+		wp_enqueue_style( 'podlove-admin-image-chosen' );
+
+		wp_register_style( 'podlove-admin-font', \Podlove\PLUGIN_URL . '/css/admin-font.css', array(), \Podlove\get_plugin_header( 'Version' ) );
+		wp_enqueue_style( 'podlove-admin-font' );
+
+		wp_register_script( 'podlove-cryptojs', \Podlove\PLUGIN_URL . '/js/admin/cryptojs/md5.js' );
+		wp_enqueue_script( 'podlove-cryptojs' );
+	}
+
 } );
