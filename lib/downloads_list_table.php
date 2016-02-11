2e42cfae1f384605df336c8be4f55dd990a98224
eteubert <eric@ericteubert.de>
2016-02-11 18:26:46 +0800

    new download list table

--- lib/downloads_list_table.php
+++ lib/downloads_list_table.php
@@ -27,19 +27,42 @@ class Downloads_List_Table extends \Podlove\List_Table {
 	public function column_downloads( $episode ) {
 		return self::get_number_or_dash($episode['downloads']);
 	}
-	public function column_downloads_peak( $episode ) {
-		$downloads = self::get_number_or_dash($episode['downloads_peak']['downloads']);
-		$date = $episode['downloads_peak']['theday'];
 
-		if (!$episode['downloads_peak']['downloads']) {
-			return $downloads;
-		} else {
-			return sprintf("%s %son %s%s", $downloads, '<span style="color: #999">', date(get_option('date_format'), strtotime($date)), '</span>');
+	public function column_default($item, $column_name)
+	{
+		$aggregation_columns = self::aggregation_columns();
+
+		$get_prev_column_index = function($col) {
+			$aggregation_columns = self::aggregation_columns();
+			$column_index = array_search($col, $aggregation_columns);
+
+			if ($column_index < 9) {
+				return $aggregation_columns[$column_index + 1];
+			}
+
+			return null;
+		};
+
+		if (in_array($column_name, $aggregation_columns)) {
+
+			$prev_column_index = $get_prev_column_index($column_name);
+
+			$factor = 0;
+			if ($prev_column_index) {
+				$prev = $item[$prev_column_index];
+				if ($prev > 0) {
+					$factor = round($item[$column_name] / $prev * 100 - 100);
+				}
+			}
+
+			$out = self::get_number_or_dash($item[$column_name]);
+
+			if ($factor) {
+				$out .= " <em style=\"color: #999\">+$factor%</em>";
+			}
+
+			return $out;
 		}
-	}
-
-	public function column_downloads_avg( $episode ) {
-		return self::get_number_or_dash($episode['downloads_avg']);
 	}
 
 	public static function get_number_or_dash($value) {
@@ -48,21 +71,42 @@ class Downloads_List_Table extends \Podlove\List_Table {
 
 	public function get_columns(){
 		return array(
-			'episode'        => __( 'Episode', 'podlove-podcasting-plugin-for-wordpress' ),
-			'downloads'      => __( 'Total Downloads', 'podlove-podcasting-plugin-for-wordpress' ),
-			'downloads_peak' => __( 'Downloads on Peak Day', 'podlove-podcasting-plugin-for-wordpress' ),
-			'downloads_avg'  => __( 'Average per Day', 'podlove-podcasting-plugin-for-wordpress' ),
+			'episode'   => __('Episode', 'podlove-podcasting-plugin-for-wordpress'),
+			'downloads' => __('Total', 'podlove-podcasting-plugin-for-wordpress'),
+			'4w' => __('4w', 'podlove-podcasting-plugin-for-wordpress'),
+			'3w' => __('3w', 'podlove-podcasting-plugin-for-wordpress'),
+			'2w' => __('2w', 'podlove-podcasting-plugin-for-wordpress'),
+			'1w' => __('1w', 'podlove-podcasting-plugin-for-wordpress'),
+			'6d' => __('6d', 'podlove-podcasting-plugin-for-wordpress'),
+			'5d' => __('5d', 'podlove-podcasting-plugin-for-wordpress'),
+			'4d' => __('4d', 'podlove-podcasting-plugin-for-wordpress'),
+			'3d' => __('3d', 'podlove-podcasting-plugin-for-wordpress'),
+			'2d' => __('2d', 'podlove-podcasting-plugin-for-wordpress'),
+			'1d' => __('1d', 'podlove-podcasting-plugin-for-wordpress')
 		);
 	}
 
+
 	public function get_sortable_columns() {
-		return array(
-			'episode'        => array('episode', true),
-			'downloads'      => array('downloads', true),
-			'downloads_peak' => array('downloads_peak', true),
-			'downloads_avg'  => array('downloads_avg', true)
-		);
-	}	
+		return [
+			'episode'   => ['episode', true],
+			'downloads' => ['downloads', true],
+			'4w'        => ['4w', true],
+			'3w'        => ['3w', true],
+			'2w'        => ['2w', true],
+			'1w'        => ['1w', true],
+			'6d'        => ['6d', true],
+			'5d'        => ['5d', true],
+			'4d'        => ['4d', true],
+			'3d'        => ['3d', true],
+			'2d'        => ['2d', true],
+			'1d'        => ['1d', true]
+		];
+	}
+
+	public static function aggregation_columns() {
+		return ['4w','3w','2w','1w','6d','5d','4d','3d','2d','1d'];
+	}
 
 	public function prepare_items() {
 		global $wpdb;
@@ -76,47 +120,45 @@ class Downloads_List_Table extends \Podlove\List_Table {
 		$sortable = $this->get_sortable_columns();
 		$this->_column_headers = array( $columns, $hidden, $sortable );
 
-		// $data = \Podlove\Cache\TemplateCache::get_instance()->cache_for('podlove_analytics_downloads_table', function() {
 		global $wpdb;
 
+		$aggregation_columns = self::aggregation_columns();
+
+		$select = array_map(function ($column) {
+			return sprintf('pm_%1$s.meta_value %1$s', $column);
+		}, $aggregation_columns);
+		$select = implode(",\n\t", $select);
+
+		$join = array_map(function ($column) {
+			global $wpdb;
+			return sprintf('LEFT JOIN ' . $wpdb->postmeta . ' pm_%1$s ON pm_%1$s.post_id = p.ID AND pm_%1$s.meta_key = \'_podlove_downloads_%1$s\'', $column);
+		}, $aggregation_columns);
+		$join = implode("\n\t", $join);
+
 		$sql = "
-			SELECT
-				e.id,
-				p.post_title title,
-				p.post_date post_date,
-				COUNT(di.id) downloads
-			FROM
-				" . Model\Episode::table_name() . " e
-				JOIN " . $wpdb->posts . " p ON e.post_id = p.ID
-				JOIN " . Model\MediaFile::table_name() . " mf ON e.id = mf.episode_id
-				LEFT JOIN " . Model\DownloadIntentClean::table_name() . " di ON di.media_file_id = mf.id
-			WHERE
-				p.post_status IN ('publish', 'private')
-			GROUP BY
-				e.id
+SELECT
+	e.id,
+	p.post_title title,
+	p.post_date post_date,
+	COUNT(di.id) downloads,
+	$select
+FROM
+	" . Model\Episode::table_name() . " e
+	JOIN " . $wpdb->posts . " p ON e.post_id = p.ID
+	JOIN " . Model\MediaFile::table_name() . " mf ON e.id = mf.episode_id
+	LEFT JOIN " . Model\DownloadIntentClean::table_name() . " di ON di.media_file_id = mf.id
+	$join
+WHERE
+	p.post_status IN ('publish', 'private')
+GROUP BY
+	e.id
 		";
 
 		$data = $wpdb->get_results($sql, ARRAY_A);
-		// }, HOUR_IN_SECONDS);
-
-		foreach ($data as $row_id => $row) {
-			
-			$days_since_release = (new \DateTime())->diff(new \DateTime($row['post_date']))->days;
-			if ($days_since_release) {
-				$data[$row_id]['downloads_avg'] = round($row['downloads'] / $days_since_release);
-			} else {
-				$data[$row_id]['downloads_avg'] = 0;
-			}
-
-			$peak = Model\DownloadIntentClean::peak_download_by_episode_id($row['id']);
-			$data[$row_id]['downloads_peak'] = $peak;
-		}
 
 		$valid_order_keys = array(
 			'post_date',
-			'downloads',
-			'downloads_peak',
-			'downloads_avg'
+			'downloads'
 		);
 
 		// look for order options
--- lib/settings/tools.php
+++ lib/settings/tools.php
@@ -38,6 +38,9 @@ class Tools {
 			case 'recalculate_useragents':
 				self::recalculate_useragents();
 				break;
+			case 'recalculate_downloads_table':
+				self::recalculate_downloads_table();
+				break;
 			default:
 				# code...
 				break;
@@ -52,8 +55,56 @@ class Tools {
 	}
 
 	public static function recalculate_useragents() {
-		error_log(print_r("recalc user agents", true));
 		podlove_init_user_agent_refresh();
+	}
+
+	public static function recalculate_downloads_table() {
+		global $wpdb;
+
+		$groupings = [
+			'4w' => 24 * 7 * 4,
+			'3w' => 24 * 7 * 3,
+			'2w' => 24 * 7 * 2,
+			'1w' => 24 * 7,
+			'6d' => 24 * 6,
+			'5d' => 24 * 5,
+			'4d' => 24 * 4,
+			'3d' => 24 * 3,
+			'2d' => 24 * 2,
+			'1d' => 24
+		];
+
+		foreach (Model\Podcast::get()->episodes() as $episode) {
+			
+			$max_hsr = $wpdb->get_var(
+				$wpdb->prepare(
+					'SELECT
+					  MAX(hours_since_release)
+					FROM ' . Model\DownloadIntentClean::table_name() . ' di
+					JOIN ' . Model\MediaFile::table_name() . ' mf ON mf.id = di.media_file_id
+					WHERE mf.episode_id = %d',
+					$episode->id
+				)
+			);
+
+			foreach ($groupings as $key => $hours) {
+				if ($max_hsr > $hours) {
+					$sql = $wpdb->prepare(
+						'SELECT
+						  COUNT(*)
+						FROM ' . Model\DownloadIntentClean::table_name() . ' di
+						INNER JOIN ' . Model\MediaFile::table_name() . ' mf ON mf.id = di.media_file_id
+						INNER JOIN ' . Model\Episode::table_name() . ' e ON mf.episode_id = e.id
+						WHERE e.id = %d AND hours_since_release <= %d',
+						$episode->id, $hours
+					);
+					error_log(print_r($sql, true));
+					$downloads = $wpdb->get_var($sql);
+					update_post_meta($episode->post_id, '_podlove_downloads_' . $key, $downloads);
+					error_log(print_r("update_post_meta($episode->post_id, '_podlove_downloads_' . $key, $downloads);", true));
+				}
+			}
+		}
 	}
 
 	public function page() {
@@ -73,6 +124,12 @@ class Tools {
 			<p>
 				<a href="<?php echo admin_url('admin.php?page=' . $_REQUEST['page'] . '&action=recalculate_analytics') ?>" class="button button-primary">
 					<?php echo __( 'Recalculate Analytics', 'podlove-podcasting-plugin-for-wordpress' ) ?>
+				</a>
+			</p>
+
+			<p>
+				<a href="<?php echo admin_url('admin.php?page=' . $_REQUEST['page'] . '&action=recalculate_downloads_table') ?>" class="button button-primary">
+					<?php echo __( 'Recalculate Downloads Table', 'podlove-podcasting-plugin-for-wordpress' ) ?>
 				</a>
 			</p>
 
