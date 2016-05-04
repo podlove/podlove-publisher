cee3d405bb2051bc5f9387b23481b73f327008cd
eteubert <eric@ericteubert.de>
2016-05-04 09:31:46 +0200

    bump version to 2.3.13

--- podlove.php
+++ podlove.php
@@ -3,7 +3,7 @@
  * Plugin Name: Podlove Podcast Publisher
  * Plugin URI:  http://publisher.podlove.org
  * Description: The one and only next generation podcast publishing system. Seriously. It's magical and sparkles a lot.
- * Version:     2.3.12
+ * Version:     2.3.13
  * Author:      Podlove
  * Author URI:  http://podlove.org
  * License:     MIT
