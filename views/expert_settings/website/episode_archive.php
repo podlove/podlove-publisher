2c34216b123c1f164862c20f02986bd0a2679321
eteubert <eric@ericteubert.de>
2016-11-08 15:58:27 +0800

    episode archive option: use site url instead of home url

--- views/expert_settings/website/episode_archive.php
+++ views/expert_settings/website/episode_archive.php
@@ -1,6 +1,6 @@
 <input name="podlove_website[episode_archive]" id="episode_archive" type="checkbox" <?php checked( $enable_episode_archive, 'on' ) ?>> <?php _e( 'Enable episode pages: a complete, paginated list of episodes, sorted by publishing date.', 'podlove-podcasting-plugin-for-wordpress' ); ?>
 <div id="episode_archive_slug_edit"<?php if ( !$enable_episode_archive ) echo ' style="display:none;"' ?>>
-	<code><?php echo get_option('home') . $blog_prefix; ?></code>
+	<code><?php echo get_option('siteurl') . $blog_prefix; ?></code>
 	<input class="podlove-check-input" name="podlove_website[episode_archive_slug]" id="episode_archive_slug" type="text" value="<?php echo $episode_archive_slug ?>">
 </div>
 
