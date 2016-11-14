547426307e8090cfcf6d7b673700ca6e5a0dd011
Simon Kraft <simon@krafit.de>
2016-11-14 14:10:16 +0100

    License Selector i18n-Improvements
    - added strings

--- lib/settings/podcast/tab/license.php
+++ lib/settings/podcast/tab/license.php
@@ -59,7 +59,7 @@ class License extends Tab {
 						<span id="podlove_cc_license_selector_toggle">
 							<span class="_podlove_episode_list_triangle">&#9658;</span>
 							<span class="_podlove_episode_list_triangle_expanded">&#9660;</span>
-							License Selector
+							<?php _e('License Selector', 'podlove-podcasting-plugin-for-wordpress'); ?>
 						</span>
 					</td>
 				</tr>
@@ -67,31 +67,31 @@ class License extends Tab {
 					<th></th>
 					<td>
 						<div>
-							<label for="license_cc_version" class="podlove_cc_license_selector_label">Version</label>
+							<label for="license_cc_version" class="podlove_cc_license_selector_label"><?php _e('Version', 'podlove-podcasting-plugin-for-wordpress'); ?></label>
 							<select id="license_cc_version">
-								<option value="cc0">Public Domain</option>
-								<option value="pdmark">Public Domain Mark</option>
-								<option value="cc3">Creative Commons 3.0 and earlier</option>
-								<option value="cc4">Creative Commons 4.0</option>
+								<option value="cc0"><?php _e('Public Domain', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="pdmark"><?php _e('Public Domain Mark', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="cc3"><?php _e('Creative Commons 3.0 and earlier', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="cc4"><?php _e('Creative Commons 4.0', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
 							</select>
 						</div>
 						<div class="podlove-hide">
-							<label for="license_cc_allow_modifications" class="podlove_cc_license_selector_label">Allow modifications of your work?</label>
+							<label for="license_cc_allow_modifications" class="podlove_cc_license_selector_label"><?php _e('Allow modifications of your work?', 'podlove-podcasting-plugin-for-wordpress'); ?></label>
 							<select id="license_cc_allow_modifications">
-								<option value="yes">Yes</option>
-								<option value="yesbutshare">Yes, as long as others share alike</option>
-								<option value="no">No</option>
+								<option value="yes"><?php _e('Yes', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="yesbutshare"><?php _e('Yes, as long as others share alike', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="no"><?php _e('No', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
 							</select>
 						</div>
 						<div class="podlove-hide">
-							<label for="license_cc_allow_commercial_use" class="podlove_cc_license_selector_label">Allow commercial uses of your work?</label>
+							<label for="license_cc_allow_commercial_use" class="podlove_cc_license_selector_label"><?php _e('Allow commercial uses of your work?', 'podlove-podcasting-plugin-for-wordpress'); ?></label>
 							<select id="license_cc_allow_commercial_use">
-								<option value="yes">Yes</option>
-								<option value="no">No</option>
+								<option value="yes"><?php _e('Yes', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
+								<option value="no"><?php _e('No', 'podlove-podcasting-plugin-for-wordpress'); ?></option>
 							</select>
 						</div>
 						<div class="podlove-hide">
-							<label for="license_cc_license_jurisdiction" class="podlove_cc_license_selector_label">License Jurisdiction</label>
+							<label for="license_cc_license_jurisdiction" class="podlove_cc_license_selector_label"><?php _e('License Jurisdiction', 'podlove-podcasting-plugin-for-wordpress'); ?></label>
 							<select id="license_cc_license_jurisdiction">
 								<?php
 									foreach ( \Podlove\License\locales_cc() as $locale_key => $locale_description) {
@@ -104,13 +104,13 @@ class License extends Tab {
 				</tr>
 				<tr class="row_podlove_podcast_license_preview">
 					<th scope="row" valign="top">
-							<label for="podlove_podcast_subtitle">License Preview</label>
+							<label for="podlove_podcast_subtitle"><?php _e('License Preview', 'podlove-podcasting-plugin-for-wordpress'); ?></label>
 					</th>
 					<td>
 						<p class="podlove_podcast_license_image"></p>
 						<div class="podlove_license">
 							<p>
-								This work is licensed under the 
+								<?php _e('This work is licensed under the', 'podlove-podcasting-plugin-for-wordpress'); ?> 
 								<a class="podlove-license-link" rel="license" href=""></a>.
 							</p>
 						</div>
