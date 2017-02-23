7f01f6d805eac75c5f1e5f67cd3b8a0a59c6f99e
eteubert <eric@ericteubert.de>
2017-02-23 12:14:31 +0800

    notification module prototype

---
+++ lib/modules/notifications/notifications.php
@@ -0,0 +1,93 @@
+<?php 
+namespace Podlove\Modules\Notifications;
+
+use Podlove\Modules\Contributors\Model\EpisodeContribution;
+use Podlove\Model\Episode;
+use Podlove\Modules\Contributors\Model\Contributor;
+
+class Notifications extends \Podlove\Modules\Base {
+	
+	protected $module_name = 'Notifications';
+	protected $module_description = 'Notify contributors via E-Mail when episodes get published.';
+	protected $module_group = 'system';
+
+	public function load() {
+		add_action('publish_podcast', [$this, 'maybe_send_notifications'], 10, 2);
+
+		if (isset($_REQUEST['debug_notification']) && $_REQUEST['debug_notification']) {
+			$this->maybe_send_notifications(4116, get_post(4116));
+		}
+	}
+
+	public function maybe_send_notifications($post_id, $post)
+	{
+		error_log(print_r('maybe_send_notifications', true));
+
+		// if ($this->notifications_sent($post_id))
+		// 	return;
+
+		$this->mark_notifications_sent($post_id);
+
+		error_log(print_r('marker is set, now we send', true));
+		
+		// who? until there is UI, all contributors
+
+		// THOUGHT: maybe ... this should only be a hook that expects emails, names 
+		// or something, so it doesn't directly depend on the contributor module.
+
+		$episode = Episode::find_one_by_property('post_id', (int) $post_id);
+		$contributions = EpisodeContribution::find_all_by_property('episode_id', $episode->id);
+
+		// map contributions to contributors
+		$contributors = array_map(function($c) {
+			return Contributor::find_by_id($c->contributor_id);
+		}, $contributions);
+
+		// stop if there is no one to be notified
+		if (!count($contributors))
+			return;
+
+		foreach ($contributors as $contributor) {
+			
+			if (!$contributor->privateemail) {
+				// todo: log that no email is set
+				error_log(print_r("no email for " . $contributor->getName() . " (id " . $contributor->id . ")", true));
+				continue;
+			}
+
+			// todo: subject should be user configurable template with placeholders
+			$subject = "Episode Published: " . get_the_title($post);
+			$headers = ['Content-Type: text/html; charset=UTF-8'];
+			$message = "Hello " . $contributor->getName() . ",<p>Geht voll ab!</p><p>Greetings, Dein Internet</p>";
+
+			// todo: send using background jobs
+			$success = wp_mail( $contributor->privateemail, $subject, $message, $headers );
+
+			// todo: log is not successful
+
+			error_log(print_r("wp_mail: $success", true));
+		}
+	}
+
+	/**
+	 * Were notifications for this episode sent already?
+	 * 
+	 * @param  int $post_id
+	 * @return bool    True if notifications were sent, otherwise false.
+	 */
+	public function notifications_sent($post_id)
+	{
+		return (bool) get_post_meta($post_id, '_podlove_notifications_sent', true);
+	}
+
+	/**
+	 * Remember that notifications have been sent.
+	 * 
+	 * @param  int $post_id
+	 */
+	public function mark_notifications_sent($post_id)
+	{
+		update_post_meta($post_id, '_podlove_notifications_sent', true);
+	}
+
+}
