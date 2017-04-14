040819818b85279cebe99f78c9e8ca201ef702f7
Alex <chemiker@users.noreply.github.com>
2017-04-14 11:29:48 +0200

    Encode URL scheme only if not already encoded (#915)

--- js/src/admin.js
+++ js/src/admin.js
@@ -162,11 +162,15 @@ function clean_up_input() {
 
 				switch(inputType) {
 					case "url":
-						// Encode the text field into valid URL before we check if it is valid
-						textfield.val( encodeURI( textfield.val() ) );
+						valid_url_regexp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
-						if ( ! textfield.val().match(/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i) ) {
-							ShowInputError('Please enter a valid URL');
+						if ( ! textfield.val().match(valid_url_regexp) ) {
+							// Encode URL only if it is not already encoded
+							if ( ! encodeURI( textfield.val() ).match(valid_url_regexp) ) {
+								ShowInputError('Please enter a valid URL');
+							} else {
+								textfield.val( encodeURI( textfield.val() ) );
+							}							
 						}		 				
 					break;
 					case "avatar":
