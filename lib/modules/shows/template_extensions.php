85dbe23984fbc768511ed477e9b95e2e9ffe5fe1
chemiker <alexlueken@gmail.com>
2017-05-08 20:50:58 +0200

    Fix swapped documentation for template methods

--- lib/modules/shows/template_extensions.php
+++ lib/modules/shows/template_extensions.php
@@ -3,26 +3,6 @@ namespace Podlove\Modules\Shows;
 use \Podlove\Modules\Shows\Model\Show;
 
 class TemplateExtensions {
-
-	/**
-	 * Episode Show
-	 *
-	 * **Examples**
-	 * 
-	 * ```
-	 * This episode is part of the Show: {{ episode.show.title }} which deals with 
-	 * {{ episode.show.summary }}
-	 * ```
-	 *
-	 * @accessor
-	 * @dynamicAccessor episode.show
-	 */
-	public static function accessorPodcastShows($return, $method_name, $episode) {
-		return $episode->with_blog_scope(function() use ($return, $method_name, $episode) {
-			return Show::all();
-		});
-	}
-
 	/**
 	 * List of all Podcast shows
 	 *
@@ -39,6 +19,25 @@ class TemplateExtensions {
 	 *
 	 * @accessor
 	 * @dynamicAccessor podcast.shows
+	 */
+	public static function accessorPodcastShows($return, $method_name, $episode) {
+		return $episode->with_blog_scope(function() use ($return, $method_name, $episode) {
+			return Show::all();
+		});
+	}
+
+	/**
+	 * Episode Show
+	 *
+	 * **Examples**
+	 * 
+	 * ```
+	 * This episode is part of the Show: {{ episode.show.title }} which deals with 
+	 * {{ episode.show.summary }}
+	 * ```
+	 *
+	 * @accessor
+	 * @dynamicAccessor episode.show
 	 */
 	public static function accessorEpisodesShow($return, $method_name, $episode) {
 		return $episode->with_blog_scope(function() use ($return, $method_name, $episode) {
