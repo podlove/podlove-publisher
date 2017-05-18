19712f609ccd34ecec892bcacef6c717a17ea4cb
Sören Wrede <soerenwrede@gmail.com>
2017-05-19 00:03:17 +0200

    Adds translation function to assets settings.

--- lib/settings/episode_asset.php
+++ lib/settings/episode_asset.php
@@ -355,7 +355,7 @@ class EpisodeAsset {
 
 			$f->checkbox( 'downloadable', array(
 				'label'       => __( 'Downloadable', 'podlove-podcasting-plugin-for-wordpress' ),
-				'description' => sprintf( 'Allow downloads for users.', 'podlove-podcasting-plugin-for-wordpress' ),
+				'description' => __( 'Allow downloads for users.', 'podlove-podcasting-plugin-for-wordpress' ),
 				'default' => true
 			) );
 			?>
