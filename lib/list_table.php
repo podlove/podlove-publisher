fa96d4399305f0c42f10913385e977229cfe7f2d
eteubert <eric@ericteubert.de>
2017-06-08 13:08:21 +0200

    fix "no items found" style

--- lib/list_table.php
+++ lib/list_table.php
@@ -31,7 +31,7 @@ class List_Table extends \WP_List_Table {
 		$url  = sprintf( '?page=%s&action=%s', $page, 'new' );
 		$url .= !empty($podlove_tab) ? "&amp;podlove_tab=" . $podlove_tab : ''
 		?>
-		<span class="add-new-h2" style="background: transparent">
+		<span>
 			<?php _e( 'No items found.' ); ?>
 		</span>
 		<a href="<?php echo esc_attr($url) ?>" class="add-new-h2">
