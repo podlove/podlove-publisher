6aec8bff4f2a9f2d18ee46776f399f2ff95a8637
eteubert <eric@ericteubert.de>
2017-08-31 10:00:18 +0200

    fix hide link to sometimes point to nonexisting URLs

--- lib/modules/title_migration/notices.php
+++ lib/modules/title_migration/notices.php
@@ -27,7 +27,7 @@ class Notices {
 				<?php echo __('The Publisher provides a tool to help you update that metadata quickly.', 'podlove-podcasting-plugin-for-wordpress') ?>
 			</p>
 			<p>
-				<a class="button" href="<?php echo admin_url('admin.php?page=podlove_tools_settings_handle#the_tools_section'); ?>"><?php echo __('Take me to the tool', 'podlove-podcasting-plugin-for-wordpress') ?></a> <a href="<?php echo admin_url('admin.php?page=' . $_REQUEST['page'] . '&podlove_set_title_migration_state=' . State::INITIALIZED_HIDDEN); ?>"><?php echo __('hide this message', 'podlove-podcasting-plugin-for-wordpress') ?></a>
+				<a class="button" href="<?php echo admin_url('admin.php?page=podlove_tools_settings_handle#the_tools_section'); ?>"><?php echo __('Take me to the tool', 'podlove-podcasting-plugin-for-wordpress') ?></a> <a href="<?php echo self::hide_message_url(State::INITIALIZED_HIDDEN); ?>"><?php echo __('hide this message', 'podlove-podcasting-plugin-for-wordpress') ?></a>
 			</p>
 		</div>	
 		<?php
@@ -54,10 +54,19 @@ class Notices {
 				<?php echo __('When you are done, you can deactivate the title migration module.' , 'podlove-podcasting-plugin-for-wordpress') ?>
 			</p>
 			<p>
-				<a class="button" href="<?php echo admin_url('admin.php?page=podlove_settings_modules_handle&podlove_disable_title_migration_module=1'); ?>"><?php echo __('Deactivate Title Migration Module', 'podlove-podcasting-plugin-for-wordpress') ?></a> <a href="<?php echo admin_url('admin.php?page=' . $_REQUEST['page'] . '&podlove_set_title_migration_state=' . State::FINISHED_HIDDEN); ?>"><?php echo __('hide this message', 'podlove-podcasting-plugin-for-wordpress') ?></a>
+				<a class="button" href="<?php echo admin_url('admin.php?page=podlove_settings_modules_handle&podlove_disable_title_migration_module=1'); ?>"><?php echo __('Deactivate Title Migration Module', 'podlove-podcasting-plugin-for-wordpress') ?></a> <a href="<?php echo self::hide_message_url(State::FINISHED_HIDDEN); ?>"><?php echo __('hide this message', 'podlove-podcasting-plugin-for-wordpress') ?></a>
 			</p>
 		</div>	
 		<?php
+	}
+
+	public static function hide_message_url($state)
+	{
+		if (isset($_REQUEST['page']) && $_REQUEST['page']) {
+			return admin_url('admin.php?page=' . $_REQUEST['page'] . '&podlove_set_title_migration_state=' . $state);
+		} else {
+			return admin_url('admin.php?page=podlove_tools_settings_handle&podlove_set_title_migration_state=' . $state);
+		}
 	}
 
 }
