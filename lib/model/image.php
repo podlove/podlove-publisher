891b3402bb5c987c305289618a1f244ce3e51b1c
eteubert <eric@ericteubert.de>
2017-11-25 12:58:46 +0100

    Revert "fix contributor rendering in Overcast by disabling srcsets in feeds"
    This reverts commit c32682630de17f5999a5e5e52e40cf4fd60388f5.

--- lib/model/image.php
+++ lib/model/image.php
@@ -288,7 +288,7 @@ class Image {
 
 		$img->setAttribute('src', $this->url());
 
-		if (!is_feed() && $this->retina && $srcset = $this->srcset())
+		if ($this->retina && $srcset = $this->srcset())
 			$img->setAttribute('srcset', $srcset);
 
 		$dom->appendChild($img);
