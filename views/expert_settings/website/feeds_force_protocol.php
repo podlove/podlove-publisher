2073f73c47803ea795f9be9f752c557ee496f5a9
eteubert <eric@ericteubert.de>
2018-03-14 18:24:24 +0100

    update a description
    … iTunes does support lets encrypt now

--- views/expert_settings/website/feeds_force_protocol.php
+++ views/expert_settings/website/feeds_force_protocol.php
@@ -2,7 +2,7 @@
 	<?php echo __('How is your website configured and how do you want your feeds to behave?', 'podlove-podcasting-plugin-for-wordpress'); ?>
 </h4>
 <p>
-<?php echo __( 'If you are using a certificate provider not supported by iTunes (like Let\'s Encrypt), you need to select option 3. Podlove Publisher will then force feed URLs as well as enclosure and image URLs within the feed to be http only.', 'podlove-podcasting-plugin-for-wordpress' ) ?></p>
+<?php echo __( 'If you are using a certificate provider not supported by iTunes, you need to select option 3. Podlove Publisher will then force feed URLs as well as enclosure and image URLs within the feed to be http only. Please verify then that your webserver does not redirect http links back to https, otherwide the feed will not be accessible.', 'podlove-podcasting-plugin-for-wordpress' ) ?></p>
 <h4>
 	<?php echo __('Useful Resources', 'podlove-podcasting-plugin-for-wordpress'); ?>
 </h4>
