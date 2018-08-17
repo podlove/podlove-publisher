31209c459f674556d088f25860b82c9de76a0500
Eric Teubert <eric@ericteubert.de>
2018-08-17 13:01:07 +0200

    use babel when building analytics js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,41 +13,41 @@ let mix = require('laravel-mix');
 
 mix.js('js/src/app.js', 'js/dist/')
 
-.combine(
-    [   
-        'js/admin/chosen/chosen.jquery.min.js',
-        'js/admin/chosen/chosenImage.jquery.js',
-        'js/src/admin/md5.js',
-        'js/src/admin/timeago.jquery.js',
-        'js/src/admin/jquery.autogrow.js',
-        'js/src/admin/jquery.count_characters.js',
-        'js/src/admin/podlove_data_table.js',
-        'js/src/admin/episode.js',
-        'js/src/admin/jobs.js',
-        'js/src/admin/audio_duration_loader.js',
-        'js/src/admin/dashboard_asset_validation.js',
-        'js/src/admin/dashboard_feed_validation.js',
-        'js/src/admin/episode_asset_settings.js',
-        'js/src/admin/episode_duration.js',
-        'js/src/admin/license.js',
-        'js/src/admin/media.js',
-        'js/src/admin/protected_feed.js',
-        'js/src/admin/feed_settings.js',
-        'js/src/admin/post_title_autogenerate.js',
-        'js/src/admin.js'
-    ], 
-    'js/dist/podlove-admin.js'
-)
+    .combine(
+        [
+            'js/admin/chosen/chosen.jquery.min.js',
+            'js/admin/chosen/chosenImage.jquery.js',
+            'js/src/admin/md5.js',
+            'js/src/admin/timeago.jquery.js',
+            'js/src/admin/jquery.autogrow.js',
+            'js/src/admin/jquery.count_characters.js',
+            'js/src/admin/podlove_data_table.js',
+            'js/src/admin/episode.js',
+            'js/src/admin/jobs.js',
+            'js/src/admin/audio_duration_loader.js',
+            'js/src/admin/dashboard_asset_validation.js',
+            'js/src/admin/dashboard_feed_validation.js',
+            'js/src/admin/episode_asset_settings.js',
+            'js/src/admin/episode_duration.js',
+            'js/src/admin/license.js',
+            'js/src/admin/media.js',
+            'js/src/admin/protected_feed.js',
+            'js/src/admin/feed_settings.js',
+            'js/src/admin/post_title_autogenerate.js',
+            'js/src/admin.js'
+        ],
+        'js/dist/podlove-admin.js'
+    )
 
-.combine(
-    [
-        'js/admin/dc.js',
-        'js/src/analytics/common.js',
-        'js/src/analytics/episode.js',
-        'js/src/analytics/totals.js',
-    ], 
-    'js/dist/analytics.js'
-);
+    .babel(
+        [
+            'js/admin/dc.js',
+            'js/src/analytics/common.js',
+            'js/src/analytics/episode.js',
+            'js/src/analytics/totals.js',
+        ],
+        'js/dist/analytics.js'
+    );
 
 
 // Full API
