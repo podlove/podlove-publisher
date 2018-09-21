39c1034c11c80acd8e44dab4ed9b6d7bdf31dee3
Eric Teubert <eric@ericteubert.de>
2018-09-21 16:35:45 +0200

    new social services: Mastodon, Fediverse
    fixes #987

--- lib/version.php
+++ lib/version.php
@@ -41,7 +41,7 @@ namespace Podlove;
 use \Podlove\Model;
 use \Podlove\Jobs\CronJobRunner;
 
-define( __NAMESPACE__ . '\DATABASE_VERSION', 139 );
+define( __NAMESPACE__ . '\DATABASE_VERSION', 140 );
 
 add_action( 'admin_init', '\Podlove\maybe_run_database_migrations' );
 add_action( 'admin_init', '\Podlove\run_database_migrations', 5 );
@@ -1366,6 +1366,11 @@ function run_migrations_for_version( $version ) {
 		case 139:
 			\Podlove\Modules\PodloveWebPlayer\Podlove_Web_Player::instance()->update_module_option('use_cdn', false);
 		break;
+		case 140:
+			if (\Podlove\Modules\Social\Model\Service::table_exists()) {
+				\Podlove\Modules\Social\Social::update_existing_services();
+				\Podlove\Modules\Social\Social::build_missing_services();
+			}
 	}
 
 }
