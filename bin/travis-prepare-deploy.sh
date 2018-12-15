f835f22f0dd03b87e30b77672f85a87591e4fef6
Eric Teubert <eric@ericteubert.de>
2018-12-15 19:39:46 +0100

    wip(travis): change preparation

--- bin/travis-prepare-deploy.sh
+++ bin/travis-prepare-deploy.sh
@@ -2,3 +2,7 @@
  # add build number to version
  sed -i.bak "s/\(Version:.*\)/\1.build$TRAVIS_BUILD_NUMBER/" dist/podlove.php
  rm -f dist/podlove.php.bak
+ mv dist podlove-podcasting-plugin-for-wordpress
+ zip -r latest.zip podlove-podcasting-plugin-for-wordpress
+ mkdir dist
+ mv latest.zip dist
