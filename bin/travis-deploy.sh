d9b9a1c9f5c352ee31334a2a6fe579bc4947f8c7
Eric Teubert <eric@ericteubert.de>
2018-12-28 17:23:10 +0100

    fix file permissions

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -5,5 +5,6 @@ rm -f dist/podlove.php.bak
 mv dist podlove-podcasting-plugin-for-wordpress
 zip -r latest.zip podlove-podcasting-plugin-for-wordpress
 # curl -v --ftp-create-dirs -T latest.zip  sftp://$FTP_USER:$FTP_PASSWORD@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
+chmod 600 ./deploy_key
 ssh -i ./deploy_key $FTP_USER@eric.co.de "mkdir -p /home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/"
 scp -i ./deploy_key latest.zip $FTP_USER@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
