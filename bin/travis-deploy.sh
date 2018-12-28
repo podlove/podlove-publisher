b0bc29c7791d72dbf2d83518a53bafd90b64f6dd
Eric Teubert <eric@ericteubert.de>
2018-12-28 17:35:00 +0100

    Update travis-deploy.sh

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -7,4 +7,4 @@ zip -r latest.zip podlove-podcasting-plugin-for-wordpress
 # curl -v --ftp-create-dirs -T latest.zip  sftp://$FTP_USER:$FTP_PASSWORD@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
 chmod 600 ./deploy_key
 ssh -i ./deploy_key $FTP_USER@eric.co.de "mkdir -p /home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/"
-scp -i ./deploy_key latest.zip $FTP_USER@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest.zip
+scp -i ./deploy_key latest.zip $FTP_USER@eric.co.de:/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest.zip
