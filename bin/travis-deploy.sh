a9547697afac2f9f170f6f8218ca13a7c8a64da7
Eric Teubert <eric@ericteubert.de>
2018-12-28 17:14:49 +0100

    travis: adjust bash script

--- bin/travis-deploy.sh
+++ bin/travis-deploy.sh
@@ -1,7 +1,9 @@
- make build
- # add build number to version
- sed -i.bak "s/\(Version:.*\)/\1.build$TRAVIS_BUILD_NUMBER/" dist/podlove.php
- rm -f dist/podlove.php.bak
- mv dist podlove-podcasting-plugin-for-wordpress
- zip -r latest.zip podlove-podcasting-plugin-for-wordpress
- curl -v --ftp-create-dirs -T latest.zip  sftp://$FTP_USER:$FTP_PASSWORD@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
+make build
+# add build number to version
+sed -i.bak "s/\(Version:.*\)/\1.build$TRAVIS_BUILD_NUMBER/" dist/podlove.php
+rm -f dist/podlove.php.bak
+mv dist podlove-podcasting-plugin-for-wordpress
+zip -r latest.zip podlove-podcasting-plugin-for-wordpress
+# curl -v --ftp-create-dirs -T latest.zip  sftp://$FTP_USER:$FTP_PASSWORD@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
+ssh -i ./deploy_key $FTP_USER@eric.co.de "mkdir -p /home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/"
+scp -i ./deploy_key latest.zip $FTP_USER@eric.co.de/home/podloveftp/files/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/
