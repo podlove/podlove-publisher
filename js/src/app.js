c26d4954315523c13d255687b2261dd6b7370462
Eric Teubert <eric@ericteubert.de>
2019-01-01 19:06:58 +0100

    feat: new "Slacknotes" module
    A module that connects to a Slack channel. From that channel, links
    are extracted and their metadata (website title and source).
    
    The goal of the module is to generate an HTML list of those links to
    be used as show notes.
    
    This final HTML can be copied with a simple click.
    
    To control which links are part of the result, a date range can be
    specified and single links can be deselected.
    
    For convenience, the previously chosen Slack channel is remembered.

--- js/src/app.js
+++ js/src/app.js
@@ -4,11 +4,14 @@ import Chapters from './components/Chapters'
 import Chapter from './components/Chapter'
 import ChapterForm from './components/ChapterForm'
 import JobsDashboard from './components/JobsDashboard'
+import Slacknotes from './components/Slacknotes'
 
 Vue.component('chapters', Chapters);
 Vue.component('chapter', Chapter);
 Vue.component('chapter-form', ChapterForm);
 Vue.component('jobs-dashboard', JobsDashboard);
+Vue.component('jobs-dashboard', JobsDashboard);
+Vue.component('slacknotes', Slacknotes);
 
 if (document.getElementById('podlove-chapters-app')) {
     window.chaptersApp = new Vue({
@@ -21,3 +24,14 @@ if (document.getElementById('podlove-tools-dashboard')) {
         el: '#podlove-tools-dashboard'
     });
 }
+
+if (document.getElementById('slacknotes-app')) {
+    window.slacknotes = new Vue({
+        el: '#slacknotes-app'
+    });
+}
+
+import 'v2-datepicker/lib/index.css'
+import V2Datepicker from 'v2-datepicker'
+
+Vue.use(V2Datepicker)
