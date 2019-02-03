34fc849e1e3fcc337ee3fa3d6c1ce11759924fec
Eric Teubert <eric@ericteubert.de>
2019-02-03 16:48:04 +0800

    create shownotes module

---
+++ lib/modules/shownotes/shownotes.php
@@ -0,0 +1,14 @@
+<?php
+namespace Podlove\Modules\Shownotes;
+
+class Shownotes extends \Podlove\Modules\Base
+{
+    protected $module_name        = 'Shownotes';
+    protected $module_description = 'Generate and manage episode show notes. Helps you provide rich metadata for URLs. Full support for Publisher Templates.';
+    protected $module_group       = 'web publishing';
+
+    public function load()
+    {
+
+    }
+}
