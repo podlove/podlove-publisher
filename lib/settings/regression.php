94a6542d5f4097ebfb0b62d96b25aa68e94fca29
rahra <bf@abenteuerland.at>
2020-01-06 17:31:34 +0100

    display code completely rewritten and is solely based on JS now

--- lib/settings/regression.php
+++ lib/settings/regression.php
@@ -10,8 +10,8 @@
 * <?php require("regression.php"); new PodloveRegression(array()); ?>
 *
 * @auther Bernhard R. Fischer, <bf@abenteuerland.at>
-* @date 2019/12/05
-* @version 1.3
+* @date 2020/01/06
+* @version 2.0
  */
 
 namespace Podlove\Settings;
@@ -193,234 +193,28 @@ class Regression
 }
 
 
-/*! This class is a helper class the generate JS Canvas code from PHP.
- */
-class JSCanvas
-{
-   protected $id_;
-   protected $width_;
-   protected $height_;
-
-   protected $script_;
-
-
-   function __construct($id, $width = 800, $height = 200)
-   {
-      $this->id_ = $id;
-      $this->width_ = $width;
-      $this->height_ = $height;
-   }
-
-
-   function output_canvas_tag()
-   {
-      echo("<div style=\"position:relative;width:$this->width_;height:$this->height_;\">\n");
-      echo("<canvas id=\"$this->id_\" width=\"$this->width_\" height=\"$this->height_\"></canvas>\n");
-      echo("<canvas id=\"{$this->id_}_tip\" width=\"100\" height=\"25\" style=\"background-color:white;border:1px solid blue;position:absolute;left:-200px;top:100px;\"></canvas>\n");
-      echo("</div>");
-   }
-
-
-   function output_script()
-   {
-      echo($this->script_);
-   }
-
-
-   function start_script()
-   {
-      $this->script_ .= "<script>\n";
-   }
-
-   function start_draw()
-   {
-      $this->script_ .= "$this->id_();\nfunction $this->id_()\n{\nvar canvas = document.getElementById('$this->id_');\nif (canvas.getContext)\n{\nvar context = canvas.getContext('2d');\n";
-   }
-
-
-   function end_draw()
-   {
-      $this->script_ .= "}\n}\n";
-   }
-
-
-   function end_script()
-   {
-      $this->script_ .= "</script>\n";
-   }
-
-
-   function add_tooltip($a)
-   {
-      $this->script_ .= "tips.push({x1: {$a['x1']}, y1: {$a['y1']}, x2: {$a['x2']}, y2: {$a['y2']}, t: \"{$a['t']}\"});\n";
-   }
-
-
-   function tooltip_code()
-   {
-      $this->script_ .=
-      "
-      var canvas = document.getElementById('$this->id_');
-      var tip = document.getElementById('{$this->id_}_tip');
-      var tips = [];
-      var canvasCtx = canvas.getContext('2d');
-      var tipCtx = tip.getContext('2d');
-
-      canvas.addEventListener('mousemove', function(e){handleMouseMove(e);});
-
-      function handleMouseMove(e)
-      {
-         var rect = canvas.getBoundingClientRect();
-         mouseX = e.clientX - rect.left;
-         mouseY = e.clientY - rect.top;
-         var hit = false;
-         for (var i = 0; i < tips.length; i++)
-         {
-            var dot = tips[i];
-            if (mouseX >= dot.x1 && mouseY >= dot.y1 && mouseX <= dot.x2 && mouseY <= dot.y2)
-            {
-               var tx = dot.t;
-               tip.style.left = dot.x1 + 10 + 'px';
-               tip.style.top = dot.y1 + 10 + 'px';
-               tip.width = tipCtx.measureText(tx).width + 10;
-               tipCtx.clearRect(0, 0, tip.width, tip.height);
-               tipCtx.fillText(tx, 5, 15);
-               hit = true;
-            }
-         }
-         if (!hit) { tip.style.left = '-400px'; }
-       }
-      ";
-   }
-
-
-   function beginPath()
-   {
-      $this->script_ .= "context.beginPath();\n";
-   }
-
-
-   function moveTo($x, $y)
-   {
-      $this->script_ .= "context.moveTo($x, $y);\n";
-   }
-
-
-   function lineTo($x, $y)
-   {
-      $this->script_ .= "context.lineTo($x, $y);\n";
-   }
-
-
-   function stroke()
-   {
-      $this->script_ .= "context.stroke();\n";
-   }
-
-
-   function fill()
-   {
-      $this->script_ .= "context.fill();\n";
-   }
-
-
-   function rect($x1, $y1, $x2, $y2)
-   {
-      $this->beginPath();
-      $this->script_ .= "context.rect($x1, $y1, $x2, $y2);\n";
-   }
-
-
-   function strokeStyle($s)
-   {
-      $this->script_ .= "context.strokeStyle = '$s';\n";
-   }
-
-
-   function fillStyle($s)
-   {
-      $this->script_ .= "context.fillStyle = '$s';\n";
-   }
-
-
-   function lineWidth($w)
-   {
-      $this->script_ .= "context.lineWidth = $w;\n";
-   }
-
-
-   function fillText($t, $x, $y)
-   {
-      $this->script_ .= "context.fillText('$t', $x, $y);\n";
-   }
-   
-   
-   function setLineDash($a)
-   {
-      $this->script_ .= "context.setLineDash([";
-      for ($i = 0; $i < count($a); $i++)
-      {
-         if ($i)
-            $this->script_ .= ", ";
-         $this->script_ .= $a[$i];
-      }
-      $this->script_ .= "]);\n";
-   }
-}
-
-
-/*! This class creates the HTML canvas and the necessary JS code to generate a
-   * regression diagram.
-   * To use it, first instantiate an object and pass at least a Regression
-   * object to the constructur. Then call the method draw() which will
-   * internally generate all necessary HTML5 code. Finally call the method
-   * output() which will print the full HTML5 code.
- */
-class DrawRegression
+class JSRegression
 {
    //! maximum number of dynamic y-axis grid
    const MAX_GRID_LINES = 12;
+   //! grid line factor
+   const MUL_OF = 5;
 
    //! internal Regression object
    protected $reg_;
-   //! descriptional data of elements
-   protected $desc_;
-   //! JSCanvas object
-   protected $can_;
-
-   //! diagram width
-   protected $width_;
-   //! diagram height
-   protected $height_;
-
    //! first episode (array index) to display on diagram
    protected $start_ = 0;
    //! number of episodes to display
    protected $cnt_;
+   //! distance of y-grid lines
+   protected $ystep_ = JSRegression::MUL_OF;
+   //! descriptional data of elements
+   protected $desc_ = array();
+ 
 
-   //! border pixels (border around diagram within width and height)
-   protected $border_ = 40;
-   //! width of regression line
-   protected $bars_ = 5;
-   //! x-scaling factor for bars
-   protected $xmul_ = 1;
-   //! y-scaling factor for episode downloads
-   protected $ymul_ = 1;
-
-
-   /*! Construct (initialize) DrawRegression object.
-      * @param $reg Regression object.
-      * @param $width Optional diagram width in pixels.
-      * @param $height Optional diagram height in pixels.
-      * @param $vis Number of episodes (elements within regression array) to
-      *        display. If $vis is 0, all elements are display, otherwise it
-      *        displays the last $vis elements.
-    */
-   function __construct($reg, $width = 800, $height = 200, $vis = 0)
+   function __construct($reg, $vis = 0)
    {
       $this->reg_ = $reg;
-      $this->width_ = $width;
-      $this->height_ = $height;
 
       if ($vis <= 0)
       {
@@ -434,14 +228,7 @@ class DrawRegression
          $this->start_ = $this->reg_->count() - $vis;
       }
 
-      $this->can_ = new JSCanvas("reg_can", $width, $height);
-      $this->set_scale_factors();
-   }
-
-
-   function __destruct()
-   {
-      unset($this->can_);
+      $this->ystep_ = round($this->reg_->max() / JSRegression::MAX_GRID_LINES / JSRegression::MUL_OF + 1) * JSRegression::MUL_OF;
    }
 
 
@@ -452,205 +239,68 @@ class DrawRegression
    }
 
 
-   //! Calculate scaling factor xmul_ and ymul_.
-   protected function set_scale_factors()
+   protected function px($i)
    {
-      if ($this->cnt_)
-         $this->xmul_ = ($this->width_ - 2 * $this->border_) / $this->cnt_;
-      if ($this->reg_->max())
-         $this->ymul_ = ($this->height_ - 2 * $this->border_) / $this->reg_->max();
+      $p = $this->reg_->p()[$i] * 100;
+      if ($p >= 10)
+        $p = round($p);
+      else
+        $p = round($p, 1);
+
+      if ($p >= 0)
+         $p = "+$p";
+
+      return "$p%";
    }
 
 
-   //! Set the diagram border.
-   function set_border($border)
+   function html()
    {
-      $this->border_ = $border;
-      $this->set_scale_factors();
+      ?>
+      <div id="reg_cv_container" style="position:relative;width:100%;height:256px;">
+         <canvas id="reg_cv"></canvas>
+         <canvas id="reg_cv_tip" width="100" height="25" style="background-color:#ffffffa0;border:1px solid blue;position:absolute;left:-200px;top:100px;"></canvas>
+      </div>
+      <?php
    }
+ 
 
-
-   //! Generate regression line.
-   function draw_regline()
+   function data()
    {
-      $this->can_->beginPath();
-      $this->can_->moveTo(round($this->border_ + $this->xmul_), round($this->height_ - $this->border_ - ($this->reg_->rval()[$this->start_] * $this->ymul_)));
-      $this->can_->lineTo(round($this->width_ - $this->border_), round($this->height_ - $this->border_ - ($this->reg_->rval()[$this->start_ + $this->cnt_ - 1] * $this->ymul_)));
-      $this->can_->strokeStyle("#ffd700b0");
-      $this->can_->lineWidth($this->bars_);
-      $this->can_->stroke();
-   }
-
-
-   //! Calculate coordinates of an episode bar.
-   private function bar_coords($i)
-   {
-      $c = array();
-
-      $bars = $this->xmul_ * 0.5;
-      $c['x1'] = round($this->border_ + ($i + 1) * $this->xmul_ - $bars / 2);
-      $c['y1'] = round($this->height_ - $this->border_ - $this->reg_->val()[$i + $this->start_] * $this->ymul_);
-      $c['w'] = round($bars);
-      $c['h'] = round($this->reg_->val()[$i + $this->start_] * $this->ymul_);
-      $c['x2'] = $c['x1'] + $c['w'];
-      $c['y2'] = $c['y1'] + $c['h'];
-
-      return $c;
-   }
-
-
-   //! Add data for all tooltips to JS.
-   function tooltip_data()
-   {
-      for ($i = 0; $i < $this->cnt_; $i++)
+      print "var reg_data0_ = [\n";
+      for ($i = 0, $n = $this->start_; $i < $this->cnt_; $i++, $n++)
       {
-         $c = $this->bar_coords($i);
-         $c['t'] = isset($this->desc_[$i + $this->start_]['title']) ? $this->desc_[$i + $this->start_]['title'] : $i + $this->start_ + 1;
-         $this->can_->add_tooltip($c);
+         $j = $n + 1;
+
+         $t = "#{$j}";
+         $t .= isset($this->desc_[$n]) ? ': ' . preg_replace('/\'/', '\\\'', $this->desc_[$n]['title']) : '';
+         $t .= "\\nDownloads: {$this->reg_->val()[$n]}";
+
+         print "{n: {$j}, val: {$this->reg_->val()[$n]}, rval: {$this->reg_->rval()[$n]}, dev: {$this->reg_->dev()[$n]}, p: {$this->reg_->p()[$n]}, px: '{$this->px($n)}', tx: '$t'},\n";
       }
+      print "];\n";
+
+      print "var reg_parm0_ = {b0: {$this->reg_->rparam()['b0']}, b1: {$this->reg_->rparam()['b1']}, r2: {$this->reg_->rparam()['r2']}, max: {$this->reg_->max()}, ystep: {$this->ystep_}};\n";
    }
 
 
-   //! Generate episode bars.
-   function draw_bars()
+   function code()
    {
-      $this->can_->fillStyle("#6a5acde0");
-      for ($i = 0; $i < $this->cnt_; $i++)
-      {
-         $c = $this->bar_coords($i);
-         $this->can_->rect($c['x1'], $c['y1'], $c['w'], $c['h']);
-         $this->can_->fill();
-      }
+      if (($jsc = file_get_contents(dirname(__FILE__) . '/regression.js')) === FALSE)
+         return -1;
+
+      print $jsc;
+      return 0;
    }
 
 
-   //! Generate trend bars of episode.
-   function draw_trend()
-   {
-      $this->can_->fillStyle("#6a5acda0");
-      $bars = $this->xmul_ * 0.2;
-      for ($i = 0; $i < $this->cnt_; $i++)
-      {
-         $this->can_->fillStyle($this->reg_->dev()[$i + $this->start_] >= 0 ? "#98fb98" : "#fa8072a0");
-         $this->can_->rect(round($this->border_ + ($i + 1) * $this->xmul_ - $bars / 2),
-            round($this->height_ - $this->border_ - $this->reg_->dev()[$i + $this->start_] * $this->ymul_),
-            round($bars), round($this->reg_->dev()[$i + $this->start_] * $this->ymul_));
-         $this->can_->fill();
-      }
-   }
-
-
-   //! Generate text of percentage plus/minus of each episode.
-   function draw_ptext()
-   {
-      $this->can_->fillStyle("#202020");
-      for ($i = 0; $i < $this->cnt_; $i++)
-      {
-         $p = round($this->reg_->p()[$i + $this->start_] * 100, 1);
-         if (abs($p) >= 9.5)
-            $p = round($p);
-
-         $s = $p >= 0 ? "+$p%" : "$p%";
-         $this->can_->fillText($s,
-            round($this->border_ + ($i + 1) * $this->xmul_) . "-context.measureText('$s').width/2",
-            round($this->height_ - $this->border_ - $this->reg_->val()[$i + $this->start_] * $this->ymul_));
-      }
-   }
-
-
-   //! Generate text of regression parameters and quality.
-   function draw_rtext()
-   {
-      $fk = sprintf("y = %.1f + %.1f x", round($this->reg_->rparam()['b0'], 1), round($this->reg_->rparam()['b1'], 1));
-      $r2 = sprintf("r² = %.2f", round($this->reg_->rparam()['r2'], 2));
-
-      $this->can_->fillStyle("#202020");
-      $this->can_->fillText($fk, round($this->border_ + 5), round($this->border_));
-      $this->can_->fillText($r2, round($this->border_ + 5), round($this->border_ + 12));
-   }
-
-
-   //! Calculate y-scaling, i.e. dynamic grid. FIXME: should be imroved!
-   private function yscale()
-   {
-      $ys = 5;
-
-      for ($m = $this->reg_->max(); $m / $ys > DrawRegression::MAX_GRID_LINES; $ys *= 2);
-
-      return $ys;
-   }
-
-
-   //! Generate diagram axis, grid, and text.
-   function draw_axis()
-   {
-      $this->can_->strokeStyle("#303030");
-      $this->can_->lineWidth(1);
-
-      $this->can_->beginPath();
-      $this->can_->moveTo(round($this->border_), $this->height_ - $this->border_);
-      $this->can_->lineTo(round($this->width_ - $this->border_ + $this->xmul_ * 0.5), $this->height_ - $this->border_);
-      $this->can_->stroke();
-
-      $this->can_->beginPath();
-      $this->can_->moveTo(round($this->border_), $this->height_ - $this->border_);
-      $this->can_->lineTo(round($this->border_), $this->border_);
-      $this->can_->stroke();
-
-      for ($i = 0; $i < $this->cnt_; $i++)
-      {
-         $ii = $i + 1 + $this->start_;
-         $this->can_->fillText($ii, round($this->border_ + ($i + 1) * $this->xmul_) . "-context.measureText('$ii').width/2", round($this->height_ - $this->border_ * 0.5 ));
-      }
-
-      $this->can_->strokeStyle("#202020");
-      $this->can_->setLineDash(array(8, 2));
-      $ys = $this->yscale();
-      for ($i = $ys; $i < $this->reg_->max(); $i += $ys)
-      {
-         $this->can_->beginPath();
-         $this->can_->moveTo(round($this->border_), round($this->height_ - $this->border_ - $i * $this->ymul_));
-         $this->can_->lineTo(round($this->width_ - $this->border_ + $this->xmul_ * .5), round($this->height_ - $this->border_ - $i * $this->ymul_));
-         $this->can_->stroke();
-
-         if ($i < 1000)
-            $s = $i;
-         else
-            $s = round($i / 1000, 1) . "k";
-
-         $this->can_->fillText($s, $this->border_ . "-context.measureText('$s ').width", round($this->height_ - $this->border_ - $i * $this->ymul_));
-      }
-      $this->can_->setLineDash(array());
-   }
-
-
-   //! Generate complete diagram.
-   function draw()
-   {
-
-      $this->can_->start_script();
-
-      $this->can_->start_draw();
-      $this->draw_axis();
-      $this->draw_bars();
-      $this->draw_regline();
-      $this->draw_trend();
-      $this->draw_ptext();
-      $this->draw_rtext();
-      $this->can_->end_draw();
-
-      $this->can_->tooltip_code();
-      $this->tooltip_data();
-
-      $this->can_->end_script();
-   }
-
-
-   //! Output diagram.
    function output()
    {
-      $this->can_->output_canvas_tag();
-      $this->can_->output_script();
+      $this->html();
+      print "<script>\n";
+      $this->data();
+      $this->code();
+      print "</script>\n";
    }
 }
 
@@ -720,12 +370,11 @@ class PodloveRegression
       $reg->reg_calc();
 
       // generate regression HTML/JS code
-      $dctx = new DrawRegression($reg, 1024, 256, 25);
-      $dctx->set_description($this->desc_);
-      $dctx->draw();
+      $jsr = new JSRegression($reg, 25);
+      $jsr->set_description($this->desc_);
       // output code
-      $dctx->output();
- 
+      $jsr->output();
+
       // HTML closing tags
       ?>
       </div>
@@ -751,15 +400,14 @@ class TestRegression
       $reg = new Regression($this->data_);
       $reg->reg_calc();
 
-      // generate regression HTML/JS code
-      $dctx = new DrawRegression($reg, 1024, 256, 25);
-      $dctx->draw();
-
+      $jsr = new JSRegression($reg, 25);
       ?><!DOCTYPE html>
       <html>
-      <head> <meta charset=utf-8 /> </head>
+      <head> <meta charset="utf-8"/> </head>
       <body>
-      <?php $dctx->output(); ?>
+
+      <?php $jsr->output(); ?>
+
       <pre>
       <?php print_r($reg); ?>
       </pre>
