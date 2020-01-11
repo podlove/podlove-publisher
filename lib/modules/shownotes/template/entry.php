a3b6c8c4c073ea4af4b910ef4c04dccd5d5ce6ec
Eric Teubert <eric@ericteubert.de>
2020-01-11 13:09:28 +0100

    add(twig): entry.typeIcon(size)

--- lib/modules/shownotes/template/entry.php
+++ lib/modules/shownotes/template/entry.php
@@ -56,9 +56,7 @@ class Entry extends Wrapper
      */
     public function url()
     {
-        return $this->entry->affiliate_url
-            ?? $this->entry->url
-            ?? $this->entry->original_url;
+        return $this->entry->affiliate_url ?? $this->entry->url ?? $this->entry->original_url;
     }
 
     /**
@@ -111,6 +109,36 @@ class Entry extends Wrapper
     public function icon()
     {
         return $this->entry->icon;
+    }
+
+    /**
+     * SVG Icon for entry type
+     *
+     * @accessor
+     */
+    public function typeIcon($size = 0)
+    {
+        if ($size && $size > 0) {
+            $size_style = "width: {$size}px; height: {$size}px;";
+        } else {
+            $size_style = "";
+        }
+
+        switch ($this->entry->type) {
+            case 'link':
+                return <<<SVG
+<svg xmlns="http://www.w3.org/2000/svg" style="fill: none !important; $size_style" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>
+SVG;
+                break;
+
+            case 'topic':
+                return <<<SVG
+  <svg xmlns="http://www.w3.org/2000/svg" style="fill: none !important; $size_style" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="4 7 4 4 20 4 20 7"></polyline><line x1="9" y1="20" x2="15" y2="20"></line><line x1="12" y1="4" x2="12" y2="20"></line></svg>
+SVG;
+                break;
+        }
+
+        return null;
     }
 
     /**
