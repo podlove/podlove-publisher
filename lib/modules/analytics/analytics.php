3d6b52338fccdaec5989970ab6093cd98dbf63ef
Dave Keeshan <dkeeshan@vurple.com>
2020-06-08 15:45:08 +0100

    Initial checkin

--- lib/modules/analytics/analytics.php
+++ lib/modules/analytics/analytics.php
@@ -1,24 +1,37 @@
 <?php
-namespace Podlove\Modules\Podtrac;
+namespace Podlove\Modules\Analytics;
 use \Podlove\Model;
 
-class Podtrac extends \Podlove\Modules\Base {
+class Analytics extends \Podlove\Modules\Base {
 
-    protected $module_name = 'Podtrac';
-    protected $module_description = 'Add Podtrac Analytics';
+    protected $module_name = 'Analytics';
+    protected $module_description = 'Add Analytics';
     protected $module_group = 'external services';
     public function load() {
-        //add_action( 'init', array( $this, 'register_hooks' ) );
-        add_filter('podlove_enclosure_url', function ($original_url) {
-            $ext            = pathinfo($original_url, PATHINFO_EXTENSION);
-            $schemeless_url = preg_replace('/^https?:\/\//', '', $original_url);
-            $podtrac_prefix = "https://dts.podtrac.com/redirect.$ext/";
-            return $podtrac_prefix . $schemeless_url;
-        });
+        add_action( 'init', array( $this, 'register_hooks' ) );
+        $this->register_option( 'analytics_prefix', 'string', array(
+                'label'       => __( 'analytics prefix', 'podlove-podcasting-plugin-for-wordpress' ),
+                'description' => __( 'Analytics Prefix', 'podlove-podcasting-plugin-for-wordpress' ),
+                'html'        => array(
+                        'class' => 'regular-text podlove-check-input',
+                        'data-podlove-input-type' => 'text',
+                        'placeholder' => 'https://dts.podtrac.com/redirect.mp3/'
+                )
+        ) );
     }
 
 
     public function register_hooks() {
+	$analytics_prefix = $this->get_module_option( 'analytics_head' );
+	if ( ! $analytics_prefix )
+            return;
+        
+//         add_filter('podlove_enclosure_url', function ($original_url) {
+//             $ext            = pathinfo($original_url, PATHINFO_EXTENSION);
+//             $schemeless_url = preg_replace('/^https?:\/\//', '', $original_url);
+//             $podtrac_prefix = "https://dts.podtrac.com/redirect.$ext/";
+//             return $podtrac_prefix . $schemeless_url;
+//         });
     }
 
 }
