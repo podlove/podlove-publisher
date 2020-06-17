dffb642f94727fb048dbd5fc5aa0c85af8bc982c
Eric Teubert <eric@ericteubert.de>
2020-06-17 22:11:18 +0200

    feat: push show feeds

--- lib/modules/plus/feed_pusher.php
+++ lib/modules/plus/feed_pusher.php
@@ -14,24 +14,37 @@ class FeedPusher
 
     public function init()
     {
-        # push all feeds to PLUS whenever any feed changes
-        # fixme: podcast change is not triggered here because the settings page just sets an option
-        add_action('podlove_model_change', function ($model) {
-            if (in_array($model::name(), ["podlove_feed"])) {
+        add_action('admin_footer', function () {
+            if (get_option('podlove_plus_push_feeds')) {
                 $this->push_all_feeds();
             }
+        });
+
+        # push all feeds to PLUS whenever any feed changes
+        add_action('podlove_model_change', function ($model) {
+            if (in_array($model::name(), ["podlove_feed"])) {
+                update_option('podlove_plus_push_feeds', true);
+            }
+        });
+
+        # push feeds when podcast changes
+        add_action('update_option_podlove_podcast', function () {
+            update_option('podlove_plus_push_feeds', true);
+
         });
 
         # push all feeds to PLUS when the feature is enabled
         add_action('podlove_plus_enable_proxy_changed', function ($new_value) {
             if ($new_value) {
-                $this->push_all_feeds();
+                update_option('podlove_plus_push_feeds', true);
             }
         });
     }
 
     public function push_all_feeds()
     {
+        delete_option('podlove_plus_push_feeds');
+
         // podcasts
         $feeds = array_map(function ($feed) {
             return $feed->get_subscribe_url();
@@ -40,15 +53,15 @@ class FeedPusher
         $this->api->push_feeds($feeds);
 
         // shows
-        // fixme: only when show module is on
-        // fimxe(unrelated): trigger feed refresh after push
-        // $shows = \Podlove\Modules\Shows\Model\Show::all();
-        // foreach ($shows as $show) {
-        //     $feeds = array_map(function ($feed) use ($show) {
-        //         return $feed->get_subscribe_url("shows", $show->id);
-        //     }, \Podlove\Model\Feed::all());
+        if (\Podlove\Modules\Base::is_active('shows')) {
+            $shows = \Podlove\Modules\Shows\Model\Show::all();
+            foreach ($shows as $show) {
+                $feeds = array_map(function ($feed) use ($show) {
+                    return $feed->get_subscribe_url("shows", $show->id);
+                }, \Podlove\Model\Feed::all());
 
-        //     $this->api->push_feeds($feeds);
-        // }
+                $this->api->push_feeds($feeds);
+            }
+        }
     }
 }
