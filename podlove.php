a646f55e36acd074146b116a160352bfdd2ea816
Eric Teubert <eric@ericteubert.de>
2020-06-23 16:02:51 +0200

    chore: remove "v" prefix in releases
    confuses version comparison because stable releases are not prefixed

--- podlove.php
+++ podlove.php
@@ -3,7 +3,7 @@
  * Plugin Name: Podlove Podcast Publisher
  * Plugin URI:  http://publisher.podlove.org
  * Description: The one and only next generation podcast publishing system. Seriously. It's magical and sparkles a lot.
- * Version:     v3.0-beta16
+ * Version:     3.0-beta17
  * Author:      Podlove
  * Author URI:  http://podlove.org
  * License:     MIT
