2cf21dbe2539a87d6c7e0b56046eb01d695f2059
Eric Teubert <eric@ericteubert.de>
2020-07-22 12:30:20 +0200

    chore: update deprecated Twig functions

--- lib/template/twig_filter.php
+++ lib/template/twig_filter.php
@@ -1,186 +1,187 @@
 <?php
 namespace Podlove\Template;
+
 use \Podlove\Model;
 
 /**
  * Apply Twig functionality and podcast/episode accessors to strings/templates
  *
  * Example:
- * 	add_filter('some_filter_for_a_string', array('\Podlove\Template\TwigFilter', 'apply_to_html'));
+ *     add_filter('some_filter_for_a_string', array('\Podlove\Template\TwigFilter', 'apply_to_html'));
  *
  * @param string $html HTML string
  * @param array  $vars optional map of template variables
  */
-class TwigFilter {
+class TwigFilter
+{
+    public static $template_tags = [
+        'is_archive',
+        'is_post_type_archive',
+        'is_attachment',
+        'is_tax',
+        'is_date',
+        'is_day',
+        'is_feed',
+        'is_comment_feed',
+        'is_front_page',
+        'is_home',
+        'is_month',
+        'is_page',
+        'is_paged',
+        'is_preview',
+        'is_search',
+        'is_single',
+        'is_singular',
+        'is_time',
+        'is_year',
+        'is_404',
+        'is_main_query',
+    ];
 
-	public static $template_tags = array(
-		'is_archive',
-		'is_post_type_archive',
-		'is_attachment',
-		'is_tax',
-		'is_date',
-		'is_day',
-		'is_feed',
-		'is_comment_feed',
-		'is_front_page',
-		'is_home',
-		'is_month',
-		'is_page',
-		'is_paged',
-		'is_preview',
-		'is_search',
-		'is_single',
-		'is_singular',
-		'is_time',
-		'is_year',
-		'is_404',
-		'is_main_query'
-	);
+    /**
+     * Apply Twig to given template
+     *
+     * @param  string $html File path or HTML string.
+     * @param  array  $vars optional variables for Twig context
+     * @return string       rendered template string
+     */
+    public static function apply_to_html($html, $vars = [])
+    {
+        $twig = self::getTwigEnv();
 
-	/**
-	 * Apply Twig to given template
-	 * 
-	 * @param  string $html File path or HTML string.
-	 * @param  array  $vars optional variables for Twig context
-	 * @return string       rendered template string
-	 */
-	public static function apply_to_html($html, $vars = array()) {
+        $context = ['option' => $vars];
 
-		$twig = self::getTwigEnv();
+        // add podcast to global context
+        $context = array_merge(
+            $context, ['podcast' => new Podcast(Model\Podcast::get())]
+        );
 
-		$context = ['option' => $vars];
+        // Apply filters to twig templates
+        $context = apply_filters('podlove_templates_global_context', $context);
 
-		// add podcast to global context
-		$context = array_merge(
-			$context, ['podcast' => new Podcast(Model\Podcast::get())]
-		);
+        // add podcast to global context if we are in an episode
+        if ($episode = Model\Episode::find_one_by_property('post_id', get_the_ID())) {
+            $context = array_merge($context, ['episode' => new Episode($episode)]);
+        }
 
-		// Apply filters to twig templates
-		$context = apply_filters( 'podlove_templates_global_context', $context );
+        $result = null;
 
-		// add podcast to global context if we are in an episode
-		if ($episode = Model\Episode::find_one_by_property('post_id', get_the_ID())) {
-			$context = array_merge($context, array('episode' => new Episode($episode)));
-		}
+        if ($twig->getLoader()->exists($html)) {
+            try {
+                $result = $twig->render($html, $context);
+            } catch (\Twig\Error\Error $e) {
+                $message  = $e->getRawMessage();
+                $line     = $e->getTemplateLine();
+                $template = $e->getSourceContext();
 
-		$result = null;
+                \Podlove\Log::get()->addError($message, [
+                    'type'     => 'twig',
+                    'line'     => $line,
+                    'template' => $template,
+                ]);
+            }
+        }
 
-		if ($twig->getLoader()->exists($html)) {
-			try {
-				$result = $twig->render($html, $context);
-			} catch (\Twig\Error\Error $e) {
-				$message  = $e->getRawMessage();
-				$line     = $e->getTemplateLine();
-				$template = $e->getSourceContext();
+        if ($result === null) {
+            try {
+                // simple Twig Env to render plain string
+                $env = new \Twig\Environment(new \Twig\Loader\ArrayLoader([]), ['autoescape' => false]);
 
-				\Podlove\Log::get()->addError($message, [
-					'type'     => 'twig',
-					'line'     => $line,
-					'template' => $template
-				]);
-			}
-		}
+                // no clue yet how this is possible but it happens
+                if (method_exists($env, 'createTemplate')) {
+                    $template = $env->createTemplate($html);
+                    $result   = $template->render($context);
+                } else {
+                    \Podlove\Log::get()->addError("Error when rendering Twig template from string. Missing Twig_Environment::createTemplate method.", [
+                        'type'     => 'twig',
+                        'template' => $html,
+                    ]);
+                }
 
-		if ($result === null) {
-			try {
-				// simple Twig Env to render plain string
-				$env = new \Twig_Environment(new \Twig_Loader_Array([]), ['autoescape' => false]);
+            } catch (\Exception $e) {
+                \Podlove\Log::get()->addError("Error when rendering Twig template from string: " . $e->getMessage(), [
+                    'type'     => 'twig',
+                    'template' => $html,
+                ]);
+            }
+        }
 
-				// no clue yet how this is possible but it happens
-				if (method_exists($env, 'createTemplate')) {
-					$template = $env->createTemplate($html);
-					$result   = $template->render($context);
-				} else {
-					\Podlove\Log::get()->addError("Error when rendering Twig template from string. Missing Twig_Environment::createTemplate method.", [
-						'type'     => 'twig',
-						'template' => $html
-					]);
-				}
+        return $result;
+    }
 
-			} catch (Exception $e) {
-				\Podlove\Log::get()->addError("Error when rendering Twig template from string: " . $e->getMessage(), [
-					'type'     => 'twig',
-					'template' => $html
-				]);
-			}
-		}
+    private static function getTwigLoader()
+    {
+        // file loader for internal use
+        $file_loader = new \Twig\Loader\FilesystemLoader();
+        $file_loader->addPath(implode(DIRECTORY_SEPARATOR, [\Podlove\PLUGIN_DIR, 'templates']), 'core');
 
-		return $result;
-	}
+        // other modules can register their own template directories/namespaces
+        $file_loader = apply_filters('podlove_twig_file_loader', $file_loader);
 
-	private static function getTwigLoader()
-	{
-		// file loader for internal use
-		$file_loader = new \Twig_Loader_Filesystem();
-		$file_loader->addPath(implode(DIRECTORY_SEPARATOR, [\Podlove\PLUGIN_DIR, 'templates']), 'core');
+        // database loader for user templates
+        $db_loader = new TwigLoaderPodloveDatabase;
 
-		// other modules can register their own template directories/namespaces
-		$file_loader = apply_filters('podlove_twig_file_loader', $file_loader);
+        $loaders = [$file_loader, $db_loader];
+        $loaders = apply_filters('podlove_twig_loaders', $loaders);
 
-		// database loader for user templates
-		$db_loader = new TwigLoaderPodloveDatabase;
+        return new \Twig\Loader\ChainLoader($loaders);
+    }
 
-		$loaders = array($file_loader, $db_loader);
-		$loaders = apply_filters('podlove_twig_loaders', $loaders);
+    private static function getTwigEnv()
+    {
+        $twig = new \Twig\Environment(self::getTwigLoader(), ['autoescape' => false]);
+        $twig->addExtension(new \Twig_Extensions_Extension_I18n());
+        $twig->addExtension(new \Twig_Extensions_Extension_Date());
 
-		return new \Twig_Loader_Chain($loaders);		
-	}
+        $formatBytesFilter = new \Twig\TwigFilter('formatBytes', function ($string) {
+            return \Podlove\format_bytes($string, 0);
+        });
 
-	private static function getTwigEnv()
-	{
-		$twig = new \Twig_Environment(self::getTwigLoader(), ['autoescape' => false]);
-		$twig->addExtension(new \Twig_Extensions_Extension_I18n());
-		$twig->addExtension(new \Twig_Extensions_Extension_Date());
+        $padLeftFilter = new \Twig\TwigFilter('padLeft', function ($string, $padChar, $length) {
+            while (strlen($string) < $length) {
+                $string = $padChar . $string;
+            }
+            return $string;
+        });
 
-		$formatBytesFilter = new \Twig_SimpleFilter('formatBytes', function ($string) {
-		    return \Podlove\format_bytes($string, 0);
-		});
+        $wpautopFilter = new \Twig\TwigFilter('wpautop', function ($content) {
+            return \wpautop($content);
+        });
 
-		$padLeftFilter = new \Twig_SimpleFilter('padLeft', function ($string, $padChar, $length) {
-		    while ( strlen($string) < $length ) {
-		    	$string = $padChar . $string;
-		    }
-		    return $string;
-		});
+        $twig->addFilter($formatBytesFilter);
+        $twig->addFilter($padLeftFilter);
+        $twig->addFilter($wpautopFilter);
 
-		$wpautopFilter = new \Twig_SimpleFilter( 'wpautop', function( $content ) {
-			return \wpautop( $content );
-		} );
+        // add functions
+        foreach (self::$template_tags as $tag) {
+            $func = new \Twig\TwigFunction($tag, function () use ($tag) {return $tag();});
+            $twig->addFunction($func);
+        }
 
-		$twig->addFilter($formatBytesFilter);
-		$twig->addFilter($padLeftFilter);
-		$twig->addFilter($wpautopFilter);
+        $func = new \Twig\TwigFunction('get_the_post_thumbnail_url', function ($post = null, $size = 'post-thumbnail') {return get_the_post_thumbnail_url($post, $size);});
+        $twig->addFunction($func);
 
-		// add functions
-		foreach (self::$template_tags as $tag) {
-			$func = new \Twig_SimpleFunction($tag, function() use ($tag) { return $tag(); });
-			$twig->addFunction($func);
-		}
+        // shortcode_exists
+        $func = new \Twig\TwigFunction('shortcode_exists', function ($shortcode) {return \shortcode_exists($shortcode);});
+        $twig->addFunction($func);
 
-		$func = new \Twig_SimpleFunction('get_the_post_thumbnail_url', function ($post = null, $size = 'post-thumbnail') {return get_the_post_thumbnail_url($post, $size);});
-		$twig->addFunction($func);
+        // Translation functions
+        $twig->addFunction(new \Twig\TwigFunction('__', function ($text, $domain = 'default') {
+            return \__($text, $domain);
+        }));
 
-		// shortcode_exists
-		$func = new \Twig_SimpleFunction('shortcode_exists', function($shortcode) { return \shortcode_exists($shortcode); });
-		$twig->addFunction($func);
+        $twig->addFunction(new \Twig\TwigFunction('_x', function ($text, $context, $domain = 'default') {
+            return \_x($text, $context, $domain);
+        }));
 
-		// Translation functions
-		$twig->addFunction( new \Twig_SimpleFunction( '__', function( $text, $domain = 'default' ) {
-			return \__( $text, $domain );
-		} ) );
+        $twig->addFunction(new \Twig\TwigFunction('_n', function ($single, $plural, $number, $domain = 'default') {
+            return \_n($single, $plural, $number, $domain);
+        }));
 
-		$twig->addFunction( new \Twig_SimpleFunction( '_x', function( $text, $context, $domain = 'default' ) {
-			return \_x( $text, $context, $domain );
-		} ) );
+        $twig->addFunction(new \Twig\TwigFunction('_nx', function ($single, $plural, $number, $context, $domain = 'default') {
+            return \_x($single, $plural, $number, $context, $domain);
+        }));
 
-		$twig->addFunction( new \Twig_SimpleFunction( '_n', function( $single, $plural, $number, $domain = 'default' ) {
-			return \_n( $single, $plural, $number, $domain );
-		} ) );
-		
-		$twig->addFunction( new \Twig_SimpleFunction( '_nx', function( $single, $plural, $number, $context, $domain = 'default' ) {
-			return \_x( $single, $plural, $number, $context, $domain );
-		} ) );
-
-		return $twig;		
-	}
+        return $twig;
+    }
 }
