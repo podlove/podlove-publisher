f2e582f8a8ffbcbcde8cf26135436e9e5e015564
Eric Teubert <eric@ericteubert.de>
2020-12-01 21:31:17 +0100

    fix: various cover art url encoding issues
    - do not sanitize episode image url (because it breaks special characters)
    - use esc_attr instead of htmlentities for output sanitization
    - apply same logic for shows image tag
    
    #1179 #1176

--- lib/feeds/base.php
+++ lib/feeds/base.php
@@ -119,7 +119,7 @@ function get_xml_itunesimage_node($url)
     $attr = $doc->createAttribute('href');
 
     // unexpected but true: ampersands are not escaped automatically here
-    $attr->value = htmlentities($url);
+    $attr->value = esc_attr($url);
 
     $node->appendChild($attr);
 
