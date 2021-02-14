2cc9b0c1d619af2b5aae2ce6f761befbbbf0c2a6
Eric Teubert <eric@ericteubert.de>
2021-02-14 21:53:20 +0100

    release 3.2.2-beta10

--- podlove.php
+++ podlove.php
@@ -3,7 +3,7 @@
  * Plugin Name: Podlove Podcast Publisher
  * Plugin URI:  http://publisher.podlove.org
  * Description: The one and only next generation podcast publishing system. Seriously. It's magical and sparkles a lot.
- * Version: 3.2.2-beta9
+ * Version: 3.2.2-beta10
  * Author:      Podlove
  * Author URI:  http://podlove.org
  * License:     MIT
