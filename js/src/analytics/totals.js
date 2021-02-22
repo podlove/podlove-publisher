90e595bfd01c08c1c3a70b51c11eb2f9973cba88
Eric Teubert <eric@ericteubert.de>
2021-02-22 21:44:28 +0100

    wip: dynamic total downloads

--- js/src/analytics/totals.js
+++ js/src/analytics/totals.js
@@ -647,6 +647,12 @@ jQuery(document).ready(function ($) {
 			to = new Date()
 		}
 
+        $.when(
+            $.ajax(ajaxurl + '?action=podlove-analytics-global-total-downloads' + '&date_from=' + from.toISOString() + '&date_to=' + to.toISOString())
+        ).done((downloads) => {
+            console.log({downloads});
+        })
+
 		globalCharts.forEach(chart => {
 			let chartLoading = $(chart.id + " .chart-loading")
 			let chartFailed = $(chart.id + " .chart-failed")
