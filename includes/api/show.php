60210d4c1bcebdcb2634e58e2c3aca262b909e29
Eric Teubert <eric@ericteubert.de>
2021-03-29 22:28:29 +0200

    feat(api): add more data to podcast api

--- includes/api/show.php
+++ includes/api/show.php
@@ -8,6 +8,7 @@ add_action('rest_api_init', __NAMESPACE__.'\\api_init');
 
 function api_init()
 {
+    // FIXME: why is this called "show", not "podcast"? 🤔
     register_rest_route('podlove/v1', 'show', [
         'methods' => 'GET',
         'callback' => __NAMESPACE__.'\\show_api',
@@ -19,12 +20,19 @@ function show_api()
 {
     $podcast = Podcast::get();
 
-    return new \WP_REST_Response([
+    $response = [
         '_version' => 'v1',
         'title' => $podcast->title,
         'subtitle' => $podcast->subtitle,
         'summary' => $podcast->summary,
+        'mnemonic' => $podcast->mnemonic,
+        'itunes_type' => $podcast->itunes_type,
+        'author_name' => $podcast->author_name,
         'poster' => $podcast->cover_art()->setWidth(500)->url(),
         'link' => \Podlove\get_landing_page_url(),
-    ]);
+    ];
+
+    $response = apply_filters('podlove_api_podcast_response', $response);
+
+    return new \WP_REST_Response($response);
 }
