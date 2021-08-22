0ef4064af70c1455b721b9fa34ba20bf91f8f59f
Dirk Schumann <dirk@freunde-aus-berlin.net>
2021-08-22 19:04:00 +0200

    feat(rss): add support for podcast:soundbite (#1224)
    * fix: typing error fixed
    
    * feat(rss): add support for podcast:soundbit
    
    * feat(rss): add support for podcast::soundbite - switch to modules
    
    * feat(rss): add support for podcast::soundbite
    
    Permission check for Episoden API aktiviert.
    
    * chore: adjust labels
    
    * chore: feedback from pull-request #1224
    
    * chore: Switch xml output function from direct output to php XML function
    
    * fix: remove wrong usage of php regex
    
    * chore: podcast:soundbite-item change NormalPlayTime to seconds
    
    Co-authored-by: Eric Teubert <eric@ericteubert.de>

--- includes/scripts_and_styles.php
+++ includes/scripts_and_styles.php
@@ -19,16 +19,34 @@ add_action('admin_print_styles', function () {
     if ($is_episode_edit_screen || in_array($screen->base, $vue_screens)) {
         wp_enqueue_script('podlove-episode-vue-apps', \Podlove\PLUGIN_URL.'/js/dist/app.js', ['underscore', 'jquery'], $version, true);
 
-        wp_localize_script(
-            'podlove-episode-vue-apps',
-            'podlove_vue',
-            [
-                'rest_url' => esc_url_raw(rest_url()),
-                'nonce' => wp_create_nonce('wp_rest'),
-                'post_id' => get_the_ID(),
-                'osf_active' => is_plugin_active('shownotes/shownotes.php'),
-            ]
-        );
+        $episode = Podlove\Model\Episode::find_one_by_post_id(get_the_ID());
+
+        if (!$episode) {
+            wp_localize_script(
+                'podlove-episode-vue-apps',
+                'podlove_vue',
+                [
+                    'rest_url' => esc_url_raw(rest_url()),
+                    'nonce' => wp_create_nonce('wp_rest'),
+                    'post_id' => get_the_ID(),
+                    'episode_id' => 0,
+                    'osf_active' => is_plugin_active('shownotes/shownotes.php'),
+                ]
+            );
+        }
+        else {
+            wp_localize_script(
+                'podlove-episode-vue-apps',
+                'podlove_vue',
+                [
+                    'rest_url' => esc_url_raw(rest_url()),
+                    'nonce' => wp_create_nonce('wp_rest'),
+                    'post_id' => get_the_ID(),
+                    'episode_id' => $episode->id,
+                    'osf_active' => is_plugin_active('shownotes/shownotes.php'),
+                ]
+            );
+        }
     }
 
     if (\Podlove\is_podlove_settings_screen() || $is_episode_edit_screen) {
---
+++ js/src/components/Soundbite.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="container">
+        <div class="soundbite-input">
+            <div>
+                <label>Start</label>
+                <input v-model="start" class="form-input" type="text" placeholder="00:00:00"/>
+                <p v-if="isStartValid">The start timepoint has not the correct format. Please use HH:MM:SS</p>
+            </div>
+            <div class="soundbite-space">
+                <label>Duration</label>
+                <input v-model="duration" class="form-input" type="text"  placeholder="00:00:00"/>
+                <p v-if="isDurationValid">The duration has not the correct format. Please use HH:MM:SS</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  computed: {
+      isStartValid: function() {
+            if (/^\d\d:[0-5]\d:[0-5]\d$/.test(this.start)) {
+                this.startValid = true;
+                this.sendDataToPodlove();
+                return false;
+            }
+            else {
+                this.startValid = false;
+                return true;
+            }
+      },
+      isDurationValid: function() {
+            if (/^\d\d:[0-5]\d:[0-5]\d$/.test(this.duration)) {
+                this.durationValid = true;
+                this.sendDataToPodlove();
+                return false;
+            }
+            else {
+                this.durationValid = false;
+                return true;
+            }
+      }
+  },
+  methods: {
+    sendDataToPodlove: function() {
+        if (this.dataReadFinish === false)
+            return;
+
+        if (this.durationValid === false || this.startValid === false)
+            return;
+            
+        let url = podlove_vue.rest_url + 'podlove/v1/episodes/' + podlove_vue.episode_id; 
+        this.axios
+            .patch(url,
+            {
+                soundbite_start: this.start,
+                soundbite_duration: this.duration
+            }, 
+            {
+            headers: {
+                'X-WP-Nonce': podlove_vue.nonce
+            }
+            })
+            .then(({$data}) => {
+
+            })
+            .catch((error) => {
+                alert("Daten konnten nicht eingetragen werden");
+                console.log(error);
+        });
+    },
+    getDataFromPodlove: async function() {
+        let url = podlove_vue.rest_url + 'podlove/v1/episodes/' + podlove_vue.episode_id;
+        try {
+            const response = await this.$http.get(url);
+            this.start = response.data.soundbite_start;
+            this.duration = response.data.soundbite_duration;
+            this.dataReadFinish = true;
+        }
+        catch (error) {
+            alert(error);
+        }
+    }
+  },
+  data () {
+    return {
+        start: '00:00:00',
+        duration: '00:00:00',
+        startValid: false,
+        durationValid: false,
+        dataReadFinish: false
+    }
+  },
+  created() {
+      this.getDataFromPodlove();
+  }
+}
+</script>
+
+
+<style scoped>
+.soundbite-input {
+    display: flex;
+}
+
+.soundbite-space {
+    margin-left: 20px;
+}
+
+</style>
