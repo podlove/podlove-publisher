c37783dfca724f375fe87e6f01eab381856e1685
Eric Teubert <eric@ericteubert.de>
2022-04-01 22:44:00 +0200

    feat: pretty empty state

--- client/src/modules/auphonic/components/SelectProduction.vue
+++ client/src/modules/auphonic/components/SelectProduction.vue
@@ -1,14 +1,14 @@
 <template>
   <Listbox as="div" v-model="selected">
-    <ListboxLabel class="block text-sm font-medium text-gray-700">
-      Select Existing Production:
+    <ListboxLabel class="block text-sm font-medium text-gray-600 sr-only">
+      Select Existing Production
     </ListboxLabel>
     <div class="mt-1 relative">
       <ListboxButton
         class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
       >
         <span class="w-full inline-flex truncate">
-          <span class="truncate">&mdash;</span>
+          <span class="truncate">Select Existing Production</span>
         </span>
         <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
           <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -21,7 +21,7 @@
         leave-to-class="opacity-0"
       >
         <ListboxOptions
-          class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+          class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-36 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
         >
           <ListboxOption
             as="template"
