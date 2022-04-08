d09aa893a21682f87e9236de722350052a113e75
Eric Teubert <eric@ericteubert.de>
2022-04-08 09:39:15 +0200

    chore: upgrade to tailwind 3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,13 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-  },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
@@ -16,7 +10,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-};
+  plugins: [require('@tailwindcss/forms')],
+}
