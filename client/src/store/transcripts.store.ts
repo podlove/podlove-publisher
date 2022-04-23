955801aee34fbac65944dcbd23d52eb4f178beb3
Dirk Schumann <dirk@freunde-aus-berlin.net>
2022-04-23 15:26:50 +0200

    fix(transcripts) #1295 Add transcript from asset

--- client/src/store/transcripts.store.ts
+++ client/src/store/transcripts.store.ts
@@ -7,6 +7,7 @@ export const SET_TRANSCRIPTS = 'podlove/publisher/transcript/SET_TRANSCRIPTS'
 export const SET_VOICES = 'podlove/publisher/transcript/SET_VOICES'
 export const UPDATE_VOICE = 'podlove/publisher/transcript/UPDATE_VOICE'
 export const IMPORT_TRANSCRIPTS = 'podlove/publisher/transcript/IMPORT_TRANSCRIPTS'
+export const IMPORT_ASSET_TRANSCRIPTS = 'podlove/publisher/transcript/IMPORT_ASSET_TRANSCRIPTS'
 export const DELETE_TRANSCRIPTS = 'podlove/publisher/transcript/DELETE_TRANSCRIPTS'
 
 export const init = createAction<void>(INIT)
@@ -14,6 +15,7 @@ export const setTranscripts = createAction<PodloveTranscript[]>(SET_TRANSCRIPTS)
 export const setVoices = createAction<PodloveTranscriptVoice[]>(SET_VOICES)
 export const updateVoice = createAction<{ voice: string; contributor: string }>(UPDATE_VOICE)
 export const importTranscripts = createAction<string>(IMPORT_TRANSCRIPTS)
+export const importTranscriptFromAsset = createAction<void>(IMPORT_ASSET_TRANSCRIPTS)
 export const deleteTranscripts = createAction<void>(DELETE_TRANSCRIPTS)
 
 export type State = {
