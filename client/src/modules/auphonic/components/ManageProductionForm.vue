c9402958e788a954812cd814593ee15c5eeeb325
Eric Teubert <eric@ericteubert.de>
2022-05-13 16:50:17 +0200

    feat: display production status

--- client/src/modules/auphonic/components/ManageProductionForm.vue
+++ client/src/modules/auphonic/components/ManageProductionForm.vue
@@ -14,6 +14,8 @@
                 class="cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                 >edit in Auphonic</a
               >
+              <br />
+              Status: {{ production.status_string }}
             </p>
           </div>
           <div class="mt-1">
