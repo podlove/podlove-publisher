87adce55c731e41a4450f2c8824dcc7e2ef10fca
Eric Teubert <eric@ericteubert.de>
2022-05-13 13:05:23 +0200

    fix: use correct message field in auphonic api error handler

--- client/src/sagas/auphonic.api.ts
+++ client/src/sagas/auphonic.api.ts
@@ -7,8 +7,8 @@ export function* createApi() {
   const base: string = 'https://auphonic.com/api'
   const bearer: string = yield select(selectors.auphonic.token)
 
-  const errorHandler = function (message: string) {
-    store.dispatch(notify({ type: 'error', message: `Auphonic: ${message}` }))
+  const errorHandler = function (response: any) {
+    store.dispatch(notify({ type: 'error', message: `Auphonic: ${response.error_message}` }))
   }
 
   return auphonic({ base, bearer, errorHandler })
