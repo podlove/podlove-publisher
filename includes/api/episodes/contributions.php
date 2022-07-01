03c09cea71b2a6b731c4688e448560797b6bc4b1
Dirk Schumann <dirk@freunde-aus-berlin.net>
2022-07-01 21:58:26 +0200

    feat(api): add routes to handle episode contributions and social services
    - podlove/v2/social/contributors/{id}
    - podlove/v2/social/contributors/service/{id}
    - podlove/v2/social/podcast
    - podlove/v2/social/podcast/service/{id}
    - podlove/v2/episodes/{id}/contributions
    - podlove/v2/episodes/contributions/{id}

--- includes/api/episodes/contributions.php
+++ includes/api/episodes/contributions.php
@@ -68,6 +68,29 @@ class WP_REST_PodloveEpisodeContributions_Controller extends WP_REST_Controller
                 'permission_callback' => [$this, 'delete_item_permissions_check'],
             ]
         ]);
+        register_rest_route($this->namespace, '/'.$this->rest_base.'/contributions/(?P<id>[\d]+)', [
+            'args' => [
+                'id' => [
+                    'description' => __('Unique identifier for the contribution to an episode.', 'podlove-podcasting-plugin-for-wordpress'),
+                    'type' => 'integer',
+                ],
+            ],
+            [
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => [$this, 'get_contribution'],
+                'permission_callback' => [$this, 'get_item_permissions_check'],
+            ],
+            [
+                'methods' => WP_REST_Server::EDITABLE,
+                'callback' => [$this, 'update_contribution'],
+                'permission_callback' => [$this, 'update_item_permissions_check'],
+            ],
+            [
+                'methods' => WP_REST_Server::DELETABLE,
+                'callback' => [$this, 'delete_contribution'],
+                'permission_callback' => [$this, 'delete_item_permissions_check'],
+            ]
+        ]);
     }
 
     public function get_item($request)
@@ -76,16 +99,37 @@ class WP_REST_PodloveEpisodeContributions_Controller extends WP_REST_Controller
 
         $results = array_map(function ($contributor) {
             return [
+                'id' => $contributor->id,
                 'contributor_id' => $contributor->contributor_id,
                 'role_id' => $contributor->role_id,
                 'group_id' => $contributor->group_id,
+                'position' => $contributor->position,
                 'comment' => $contributor->comment,
             ];
         }, EpisodeContribution::find_all_by_episode_id($id));
 
         return new \Podlove\Api\Response\OkResponse([
             '_version' => 'v2',
-            'contributors' => $results
+            'contribution' => $results
+        ]);
+    }
+
+    public function get_contribution($request)
+    {
+        $id = $request->get_param('id');
+
+        $contribution = EpisodeContribution::find_by_id($id);
+        if (!$contribution) {
+            return new \Podlove\Api\Error\NotFound();
+        }
+
+        return new \Podlove\Api\Response\OkResponse([
+            'id' => $contribution->id,
+            'contributor_id' => $contribution->contributor_id,
+            'role_id' => $contribution->role_id,
+            'group_id' => $contribution->group_id,
+            'position' => $contribution->position,
+            'comment' => $contribution->comment,
         ]);
     }
 
@@ -123,12 +167,46 @@ class WP_REST_PodloveEpisodeContributions_Controller extends WP_REST_Controller
                 if (isset($request['contributors'][$i]['comment'])) {
                     $contrib->comment = $request['contributors'][$i]['comment'];
                 }
+                if (isset($request['contributors'][$i]['position'])) {
+                    $contrib->position = $request['contributors'][$i]['position'];
+                }
                 $contrib->save();
             }
         }
 
         return new \Podlove\Api\Response\OkResponse([
             'status' => 'ok'
+        ]);
+    }
+
+    public function update_contribution($request)
+    {
+        $id = $request->get_param('id');
+
+        $contribution = EpisodeContribution::find_by_id($id);
+        if (!$contribution) {
+            return new \Podlove\Api\Error\NotFound();
+        }
+
+        if (isset($request['contributor_id'])) {
+            $contribution->contributor_id = $request['contributor_id'];
+        }
+        if (isset($request['group_id'])) {
+            $contribution->group = $request['group_id'];
+        }
+        if (isset($request['role_id'])) {
+            $contribution->role = $request['role_id'];
+        }
+        if (isset($request['comment'])) {
+            $contribution->comment = $request['comment'];
+        }
+        if (isset($request['position'])) {
+            $contribution->position = $request['position'];
+        }
+        $contribution->save();
+
+        return new \Podlove\Api\Response\OkResponse([
+            'status' => 'ok',
         ]);
     }
 
@@ -163,6 +241,27 @@ class WP_REST_PodloveEpisodeContributions_Controller extends WP_REST_Controller
         return new \Podlove\Api\Response\OkResponse([
             'status' => 'ok'
         ]);
+    }
+
+    public function delete_contribution($request)
+    {
+        $id = $request->get_param('id');
+        if (!$id) {
+            return;
+        }
+
+        $contribution = EpisodeContribution::find_by_id($id);
+
+        if (!$contribution) {
+            return new \Podlove\Api\Error\NotFound();
+        }
+
+        $contribution->delete();
+
+        return new \Podlove\Api\Response\OkResponse([
+            'status' => 'ok'
+        ]);
+
     }
 
     public function delete_item_permissions_check($request)
