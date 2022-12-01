852ec3d9de982384b2d2f82a6942213f750510b2
Michaela Lehr <michaela.lehr@geildanke.com>
2022-12-01 18:09:49 +0100

    #1340 feat: add related episodes module

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -1,9 +1,11 @@
 import PodloveDescription from './description';
 import PodloveChapters from './chapters';
+import PodloveRelatedEpisodes from './related-episodes';
 import PodloveTranscripts from './transcripts';
 
 export default {
   PodloveDescription,
   PodloveChapters,
+  PodloveRelatedEpisodes,
   PodloveTranscripts
 }
---
+++ client/src/modules/related-episodes/RelatedEpisodes.vue
@@ -0,0 +1,22 @@
+<template>
+  <module
+    name="related-episodes"
+    :title="__('Verknüpfte Episoden')"
+  >
+
+  </module>
+</template>
+
+<script lang="ts">
+import Module from '@components/module/Module.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    Module,
+  },
+})
+</script>
+
+<style>
+</style>
