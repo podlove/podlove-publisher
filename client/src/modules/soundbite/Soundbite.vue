7177edfe03cbc5f441a157a01949b875bdb9c90c
Dirk Schumann <dirk@freunde-aus-berlin.net>
2023-01-08 16:54:38 +0100

    feat(episode ui) - Move soundbite ui to new ui approach

---
+++ client/src/modules/soundbite/Soundbite.vue
@@ -0,0 +1,41 @@
+<template>
+    <module name="soundbite" :title="__('Soundbite')">
+        <template v-slot:actions>
+            <soundbite-clear></soundbite-clear>
+        </template>
+        <soundbite-form></soundbite-form>
+    </module>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import Module from '@components/module/Module.vue'
+import * as episode from '@store/episode.store'
+
+import { injectStore } from 'redux-vuex'
+
+import SoundbiteClear from './components/Clear.vue'
+import SoundbiteForm from './components/Form.vue'
+
+export default defineComponent({
+    components: {
+        Module,
+        SoundbiteForm,
+        SoundbiteClear,
+    },
+
+    setup(): { dispatch: Function } {
+        return {
+            dispatch: injectStore().dispatch,
+        }
+    },
+
+    created() {
+        this.dispatch(episode.init())
+    }
+})
+</script>
+
+<style lang="postcss">
+</style>
---
+++ client/src/modules/soundbite/components/Clear.vue
@@ -0,0 +1,47 @@
+<template>
+    <podlove-button variant="secondary" size="small" @click="clearSoundbite()">Clear</podlove-button>
+</template>
+
+<script lang="ts">
+import { injectStore, mapState } from 'redux-vuex'
+import { defineComponent } from '@vue/runtime-core'
+
+import { update as updateEpisode } from '@store/episode.store'
+
+import PodloveButton from '@components/button/Button.vue'
+
+export default defineComponent({
+  components: {
+    PodloveButton,
+  },
+
+  data() {
+      return {}
+  },
+
+  setup() {
+    return {
+      dispatch: injectStore().dispatch,
+    }
+  },
+
+  methods: {
+    clearSoundbite() {
+      this.dispatch(
+        updateEpisode({prop: 'soundbite_start', value: null })
+      )
+      this.dispatch(
+        updateEpisode({prop: 'soundbite_duration', value: null }),
+      )
+      this.dispatch(
+        updateEpisode({prop: 'soundbite_title', value: null })
+      )
+    }
+  },
+
+})
+
+</script>
+
+<style lang="postcss">
+</style>
