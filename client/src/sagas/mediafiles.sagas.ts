73222b9f13b8535d59416a7126a0ed9a832e2d7b
Eric Teubert <eric@ericteubert.de>
2023-03-18 13:23:03 +0100

    mediafiles: display data as table

--- client/src/sagas/mediafiles.sagas.ts
+++ client/src/sagas/mediafiles.sagas.ts
@@ -1,9 +1,28 @@
+import { PodloveApiClient } from '@lib/api'
+import { selectors } from '@store'
+import { fork, put, select } from 'redux-saga/effects'
 import * as mediafiles from '@store/mediafiles.store'
-import { put } from 'redux-saga/effects'
+import { MediaFile } from '@store/mediafiles.store'
 import { takeFirst } from './helper'
+import { createApi } from './api'
 
 function* mediafilesSaga(): any {
-  console.log('hey')
+  const apiClient: PodloveApiClient = yield createApi()
+  yield fork(initialize, apiClient)
+}
+
+function* initialize(api: PodloveApiClient) {
+  const episodeId: string = yield select(selectors.episode.id)
+  const {
+    result: { results: files },
+  }: { result: { results: MediaFile[] } } = yield api.get(`episodes/${episodeId}/media`)
+
+  console.table(files)
+
+  if (files) {
+    yield put(mediafiles.set(files))
+  }
+
   yield put(mediafiles.initDone())
 }
 
