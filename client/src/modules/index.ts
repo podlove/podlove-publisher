a006b7d2e0169830346f5a3ffb86d78f0d484028
Eric Teubert <eric@ericteubert.de>
2023-03-25 13:16:29 +0100

    chore: remove obsolete code

--- client/src/modules/index.ts
+++ client/src/modules/index.ts
@@ -2,12 +2,12 @@ import PodloveDescription from './description'
 import PodloveChapters from './chapters'
 import PodloveTranscripts from './transcripts'
 import PodloveAuphonic from './auphonic'
-import MediaFiles from './mediafiles'
+import PodloveMediaFiles from './mediafiles'
 
 export default {
   PodloveDescription,
   PodloveChapters,
   PodloveTranscripts,
   PodloveAuphonic,
-  MediaFiles,
+  PodloveMediaFiles,
 }
