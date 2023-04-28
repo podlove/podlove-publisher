a02e3f54e3d87b43848d8579fc5a9954ba6a3c13
Eric Teubert <eric@ericteubert.de>
2023-04-28 14:24:14 +0200

    change: UI cleanup; duration formatting

--- client/src/modules/mediafiles/MediaFiles.vue
+++ client/src/modules/mediafiles/MediaFiles.vue
@@ -8,91 +8,127 @@
         </div>
       </div>
       <div v-else>
-        <!-- filename / slug -->
-        <MediaSlug />
+        <div class="px-6">
+          <div
+            class="mt-10 sm:mt-0 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:pb-0"
+          >
+            <div class="sm:grid sm:grid-cols-[175px_auto_auto] sm:items-start sm:gap-4 sm:py-6">
+              <MediaSlug />
+            </div>
 
-        <!-- media file table -->
-        <table class="min-w-full table-fixed divide-y divide-gray-300">
-          <thead>
-            <tr>
-              <th scope="col" class="py-3.5 pl-3 text-left text-sm font-semibold text-gray-900">
-                Enable
-              </th>
-              <th scope="col" class="py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">
-                Asset
-              </th>
-              <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                File
-              </th>
-              <th
-                scope="col"
-                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              ></th>
-            </tr>
-          </thead>
-          <tbody class="divide-y divide-gray-200 bg-white">
-            <tr v-for="file in files" :key="file.asset_id" :class="{ 'opacity-50': !file.enable }">
-              <td class="relative px-7 sm:w-12 sm:px-6">
-                <!-- <div
+            <div class="sm:grid sm:grid-cols-[175px_auto_auto] sm:items-start sm:gap-4 sm:py-6">
+              <label
+                for="assets"
+                class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+                >Assets</label
+              >
+              <div class="mt-2 sm:col-span-2 sm:mt-0">
+                <table class="min-w-full table-fixed divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        class="py-3.5 pl-3 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Enable
+                      </th>
+                      <th
+                        scope="col"
+                        class="py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Asset
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        File
+                      </th>
+                      <th
+                        scope="col"
+                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      ></th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    <tr
+                      v-for="file in files"
+                      :key="file.asset_id"
+                      :class="{ 'opacity-50': !file.enable }"
+                    >
+                      <td class="relative px-7 sm:w-12 sm:px-6">
+                        <!-- <div
                   class="absolute inset-y-0 left-0 w-0.5"
                   :class="{ 'bg-green-400': file.size > 0, 'bg-red-400': !file.size }"
                 ></div> -->
-                <input
-                  type="checkbox"
-                  class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  :value="file.asset_id"
-                  :checked="file.enable"
-                  @click="handleToggle"
-                />
-              </td>
-              <td>
-                {{ file.asset }}
-              </td>
-              <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                <div class="flex flex-col">
-                  <a :href="file.url" target="_blank" class="text-gray-700 underline">{{
-                    file.url
-                  }}</a>
-                  <span class="flex">
-                    <CheckCircleIcon
-                      v-if="file.size > 0"
-                      class="mr-1.5 h-5 w-5 flex-shrink-0 text-green-400"
-                      aria-hidden="true"
-                    />
-                    <XCircleIcon
-                      v-else
-                      class="mr-1.5 h-5 w-5 flex-shrink-0"
-                      :class="{ 'text-gray-400': !file.enable, 'text-red-400': file.enable }"
-                      aria-hidden="true"
-                    />
-                    <!-- fixme: on load, when an asset is disabled, it shows always "file not found"
+                        <input
+                          type="checkbox"
+                          class="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          :value="file.asset_id"
+                          :checked="file.enable"
+                          @click="handleToggle"
+                        />
+                      </td>
+                      <td class="text-sm">
+                        {{ file.asset }}
+                      </td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <div class="flex flex-col">
+                          <a :href="file.url" target="_blank" class="text-gray-700 underline">{{
+                            file.url
+                          }}</a>
+                          <span class="flex">
+                            <CheckCircleIcon
+                              v-if="file.size > 0"
+                              class="mr-1.5 h-5 w-5 flex-shrink-0 text-green-400"
+                              aria-hidden="true"
+                            />
+                            <XCircleIcon
+                              v-else
+                              class="mr-1.5 h-5 w-5 flex-shrink-0"
+                              :class="{
+                                'text-gray-400': !file.enable,
+                                'text-red-400': file.enable,
+                              }"
+                              aria-hidden="true"
+                            />
+                            <!-- fixme: on load, when an asset is disabled, it shows always "file not found"
 (because Publisher does not know about disabled files). Only on "verify" do we
 get the actual state. => at least show nothing unless we're sure it's "file not
 found" -->
-                    <span
-                      v-if="!file.size"
-                      :class="{ 'text-gray-400': !file.enable, 'text-red-400': file.enable }"
-                      >File not found</span
-                    >
-                    <span v-else>{{ (file.size / 1024 / 1024).toFixed(2) }} MB</span>
-                  </span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                <button
-                  type="button"
-                  class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
-                  @click="() => handleVerify(file.asset_id)"
-                >
-                  Verify
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="m-3">
-        <span class="text-sm text-gray-700">Episode Duration: {{ duration }}</span>
+                            <span
+                              v-if="!file.size"
+                              :class="{
+                                'text-gray-400': !file.enable,
+                                'text-red-400': file.enable,
+                              }"
+                              >File not found</span
+                            >
+                            <span v-else>{{ (file.size / 1024 / 1024).toFixed(2) }} MB</span>
+                          </span>
+                        </div>
+                      </td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <button
+                          type="button"
+                          class="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
+                          @click="() => handleVerify(file.asset_id)"
+                        >
+                          Verify
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <p class="mt-3 text-sm leading-6 text-gray-600">Episode Duration: {{ duration }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- filename / slug -->
+
+        <!-- media file table -->
       </div>
     </div>
   </module>
@@ -104,7 +140,6 @@ import { selectors } from '@store'
 
 import { injectStore, mapState } from 'redux-vuex'
 import * as mediafiles from '@store/mediafiles.store'
-import { update as updateEpisode } from '@store/episode.store'
 import Module from '@components/module/Module.vue'
 
 import { RefreshIcon } from '@heroicons/vue/outline'
@@ -112,6 +147,8 @@ import { RefreshIcon } from '@heroicons/vue/outline'
 import { CheckCircleIcon, XCircleIcon } from '@heroicons/vue/solid'
 
 import MediaSlug from './components/MediaSlug.vue'
+
+import Timestamp from '@lib/timestamp'
 
 export default defineComponent({
   components: {
@@ -159,7 +196,11 @@ export default defineComponent({
       return this.state.files
     },
     duration(): string {
-      return this.state.duration
+      if (!this.state.duration) {
+        return '-'
+      }
+
+      return Timestamp.fromString(this.state.duration).pretty
     },
     assetPrefix(): string {
       return this.state.baseUri?.replace(/https?:\/\//i, '')
