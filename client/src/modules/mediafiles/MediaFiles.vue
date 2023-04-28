4df7a18b456f00ae1a5a7a0443eccfc74e63b2a3
Eric Teubert <eric@ericteubert.de>
2023-04-28 13:18:02 +0200

    feat: move slug input into vue component

--- client/src/modules/mediafiles/MediaFiles.vue
+++ client/src/modules/mediafiles/MediaFiles.vue
@@ -1,6 +1,6 @@
 <template>
   <module name="mediafiles" title="Media Files">
-    <div class="max-w-5xl">
+    <div>
       <div class="w-full flex justify-center m-12 text-center" v-if="isInitializing">
         <div class="animate-pulse mt-4 flex space-x-4">
           <RefreshIcon class="animate-spin h-5 w-5 mr-3" />
@@ -8,6 +8,40 @@
         </div>
       </div>
       <div v-else>
+        <!-- filename / slug -->
+        <div
+          class="ml-3 space-y-8 border-b border-gray-900/10 pt-6 sm:pt-0 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0"
+        >
+          <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+            <label
+              for="filename_slug"
+              class="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5"
+              >Filename / Slug</label
+            >
+            <div class="mt-2 sm:col-span-2 sm:mt-0">
+              <div
+                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+              >
+                <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">{{
+                  assetPrefix
+                }}</span>
+                <input
+                  type="text"
+                  name="filename_slug"
+                  id="filename_slug"
+                  autocomplete="filename_slug"
+                  class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder=""
+                  :value="slug"
+                  @input="updateSlug($event)"
+                />
+                <span class="flex slect-none items-center text-gray-500 sm:text-sm pr-2">.ext</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- media file table -->
         <table class="min-w-full table-fixed divide-y divide-gray-300">
           <thead>
             <tr>
@@ -100,6 +134,7 @@ import { selectors } from '@store'
 
 import { injectStore, mapState } from 'redux-vuex'
 import * as mediafiles from '@store/mediafiles.store'
+import { update as updateEpisode } from '@store/episode.store'
 import Module from '@components/module/Module.vue'
 
 import { RefreshIcon } from '@heroicons/vue/outline'
@@ -120,6 +155,7 @@ export default defineComponent({
         isInitializing: selectors.mediafiles.isInitializing,
         files: selectors.mediafiles.files,
         duration: selectors.episode.duration,
+        slug: selectors.episode.slug,
       }),
       dispatch: injectStore().dispatch,
     }
@@ -140,6 +176,11 @@ export default defineComponent({
     handleVerify(asset_id: number): void {
       this.dispatch(mediafiles.verify(asset_id))
     },
+    updateSlug(event: Event) {
+      this.dispatch(
+        updateEpisode({ prop: 'slug', value: (event.target as HTMLInputElement).value })
+      )
+    },
   },
 
   computed: {
@@ -151,6 +192,12 @@ export default defineComponent({
     },
     duration(): string {
       return this.state.duration
+    },
+    slug(): string {
+      return this.state.slug
+    },
+    assetPrefix(): string {
+      return 'https://files.podlovers.org/'
     },
   },
 
