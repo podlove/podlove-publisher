67aa04a533651262b33861caa17b90b1372b1702
Eric Teubert <eric@ericteubert.de>
2023-06-02 11:44:54 +0200

    update slug after upload

--- client/src/modules/mediafiles/components/MediaUpload.vue
+++ client/src/modules/mediafiles/components/MediaUpload.vue
@@ -6,27 +6,10 @@
   >
   <div class="mt-2 sm:col-span-2 sm:mt-0">
     <div>
-      <button
-        type="button"
-        class="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        @click="uploadIntent"
-      >
-        <svg
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="mx-auto h-12 w-12 text-gray-400"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-          />
-        </svg>
-
-        <span class="mt-2 block text-sm font-semibold text-gray-900">Upload Media File</span>
-      </button>
+      <podlove-button variant="primary" @click="uploadIntent" class="ml-1">
+        <upload-icon class="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+        {{ __('Upload Media File') }}
+      </podlove-button>
     </div>
   </div>
 </template>
@@ -35,10 +18,15 @@
 import { defineComponent } from 'vue'
 import { mapState, injectStore } from 'redux-vuex'
 
-import { selectors } from '@store'
 import { uploadIntent } from '@store/mediafiles.store'
+import PodloveButton from '@components/button/Button.vue'
+import { UploadIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
+  components: {
+    PodloveButton,
+    UploadIcon,
+  },
   setup() {
     return {
       state: mapState({}),
