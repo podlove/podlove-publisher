8147aa52153ff9e15b31b7137fb1b0cc3fb4d47a
Alexander Heimbuch <github@heimbu.ch>
2024-06-26 20:29:29 +0200

    docker: improve database connect timeout

--- bin/docker-entry.sh
+++ bin/docker-entry.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # Waiting for the MySQL server to start
-while ! wp db check --allow-root &>/dev/null; do \
-    sleep 0.1; \
-done; \
+HOST=$(echo $WORDPRESS_DB_HOST | cut -d: -f1)
+PORT=$(echo $WORDPRESS_DB_HOST | cut -d: -f2)
+
+until mysql -h $HOST -P $PORT -D $WORDPRESS_DB_NAME -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD -e '\q'; do
+  >&2 echo "Mysql is unavailable - sleeping..."
+  sleep 2
+done
 
 # Running the WordPress installation process
-wp core install --allow-root --url=http://127.0.0.1:8080 --path=/var/www/html --title="Podlove Publisher E2E Test Environment" --admin_email=admin@example.com --admin_user=admin --admin_password=admin --skip-email
-rm -rf /var/www/html/wp-content/plugins/podlove-podcasting-plugin-for-wordpress
+wp core install --allow-root --url=http://127.0.0.1 --path=/var/www/html --title="Podlove Publisher E2E Test Environment" --admin_email=admin@example.com --admin_user=admin --admin_password=admin --skip-email
 wp plugin install --activate --allow-root --path=/var/www/html /tmp/podlove-podcasting-plugin-for-wordpress.zip
 
 eval setup.sh
