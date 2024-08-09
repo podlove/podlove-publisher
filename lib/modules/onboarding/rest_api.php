0c5d3b0c67f4b0f10d8c994dc048f5921b297c1a
Dirk Schumann <dirk@freunde-aus-berlin.net>
2024-08-09 10:03:43 +0200

    feat(onboarding) - Integrate PR Feedback

--- lib/modules/onboarding/rest_api.php
+++ lib/modules/onboarding/rest_api.php
@@ -21,24 +21,29 @@ class WP_REST_PodloveOnboarding_Controller extends WP_REST_Controller
      */
     public function register_routes()
     {
-        register_rest_route($this->namespace, $this->rest_base, [
+        register_rest_route($this->namespace, $this->rest_base."/setup", [
             [
                 'methods' => \WP_REST_SERVER::EDITABLE,
                 'callback' => [$this, 'update_items'],
                 'permission_callback' => [$this, 'update_permissions_check']
             ]
-
         ]);
     }
 
     public function update_items($request)
     {
+        // activate File-Upload-Module and set default settings
         if (!\Podlove\Modules\Base::is_active('wordpress_file_upload')) {
             \Podlove\Modules\Base::activate('wordpress_file_upload');
-            $upload_modul = Wordpress_File_Upload::instance();
-            $upload_modul->update_module_option('upload_subdir', 'podlove-media');
         }
+        $upload_modul = Wordpress_File_Upload::instance();
+        $upload_modul->update_module_option('upload_subdir', 'podlove-media');
+        // set upload loaction to emty
+        $settings = get_option('podlove_podcast');
+        $settings["media_file_base_uri"] = "";
+        update_option('podlove_podcast', $settings);
     }
+
     public function update_permissions_check($request)
     {
         if (!current_user_can('edit_posts')) {
