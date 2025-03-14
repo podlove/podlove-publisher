25a163dddbe1acce97100b432961cbe7e0b29d7e
Eric Teubert <eric@ericteubert.de>
2025-03-14 16:02:24 +0800

    feat(dev): configurable local dev env

---
+++ client/config.local.template.js
@@ -0,0 +1,7 @@
+// Template for local development configuration
+// Copy this file to config.local.js and customize
+window.devConfig = {
+  user: 'YOUR_USERNAME',
+  applicationPassword: 'YOUR_APPLICATION_PASSWORD',
+  baseUrl: 'http://publisher.local',
+}
