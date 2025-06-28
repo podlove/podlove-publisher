babdc50c9a8b2c1a0ad60ef47ebe85d8a77c7155
Eric Teubert <eric@ericteubert.de>
2025-06-28 17:18:31 +0800

    refactor: use heroicons instead of inline svgs

--- client/src/modules/mediafiles/components/PlusMediaUpload.vue
+++ client/src/modules/mediafiles/components/PlusMediaUpload.vue
@@ -23,20 +23,7 @@
             <!-- File Icon -->
             <div class="flex-shrink-0">
               <div class="w-10 h-10 bg-white rounded-lg flex items-center justify-center">
-                <svg
-                  class="w-6 h-6 text-indigo-500"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                  ></path>
-                </svg>
+                <document-text-icon class="w-6 h-6 text-indigo-500" />
               </div>
             </div>
 
@@ -99,20 +86,7 @@
               class="flex-shrink-0 text-gray-400 hover:text-gray-600 focus:outline-none"
               @click="resetFile()"
             >
-              <svg
-                class="w-4 h-4"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
+              <x-mark-icon class="w-4 h-4" />
             </button>
           </div>
         </div>
@@ -146,7 +120,7 @@ import { mapState, injectStore } from 'redux-vuex'
 
 import { plusUploadIntent } from '@store/mediafiles.store'
 import PodloveButton from '@components/button/Button.vue'
-import { CloudArrowUpIcon as UploadIcon } from '@heroicons/vue/24/outline'
+import { CloudArrowUpIcon as UploadIcon, DocumentTextIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { State, selectors } from '@store'
 import * as mediafiles from '@store/mediafiles.store'
 
@@ -161,6 +135,8 @@ export default defineComponent({
   components: {
     PodloveButton,
     UploadIcon,
+    DocumentTextIcon,
+    XMarkIcon,
   },
   data() {
     return {}
--- client/src/modules/plus_token/PlusToken.vue
+++ client/src/modules/plus_token/PlusToken.vue
@@ -27,21 +27,15 @@
       >
          <template #status>
           <div v-if="apiToken && isTokenValid" class="flex items-center gap-2 text-sm text-gray-600">
-            <svg class="w-4 h-4 text-green-600" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-            </svg>
+            <CheckCircleIcon class="w-4 h-4 text-green-600" />
             <span>{{ __('You are logged in as', 'podlove-podcasting-plugin-for-wordpress') }} <strong>{{ userEmail }}</strong></span>
           </div>
           <div v-else-if="apiToken && !isTokenValid" class="flex items-center gap-2 text-sm text-red-600">
-            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-            </svg>
+            <XCircleIcon class="w-4 h-4" />
             <span>{{ __('Invalid API token', 'podlove-podcasting-plugin-for-wordpress') }}</span>
           </div>
           <div v-else class="flex items-center gap-2 text-sm text-gray-600">
-            <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-            </svg>
+            <InformationCircleIcon class="w-4 h-4" />
             <span>{{ __('No API token configured', 'podlove-podcasting-plugin-for-wordpress') }}</span>
           </div>
         </template>
@@ -53,6 +47,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TokenInput from './TokenInput.vue'
+import { CheckCircleIcon, XCircleIcon, InformationCircleIcon } from '@heroicons/vue/24/solid'
 import { injectStore, mapState } from 'redux-vuex'
 import * as plus from '@store/plus.store'
 import { selectors } from '@store'
@@ -60,6 +55,9 @@ import { selectors } from '@store'
 export default defineComponent({
   components: {
     TokenInput,
+    CheckCircleIcon,
+    XCircleIcon,
+    InformationCircleIcon,
   },
 
   setup() {
